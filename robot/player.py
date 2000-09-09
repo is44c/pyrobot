59fcbf6d4b8751c5fac194054ca24a283d10f074
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2000-09-09 12:20:20 +0000

    Broken Player/Stage: in middle of conversion to Pyro 3.0

--- robot/player.py
+++ robot/player.py
@@ -1,164 +1,390 @@
-from pyro.robot.playerbase import *
+# Defines PlayerRobot, a subclass of robot
 
-# Extension of PlayerBase for a full robot
+from pyro.robot import *
+from math import pi, cos, sin
+from os import getuid
+from pyro.robot.driver.player import *
+import random, time
 
-class PlayerRobot(PlayerBase):
+PIOVER180 = pi / 180.0
+DEG90RADS = 0.5 * pi
+COSDEG90RADS = cos(DEG90RADS) / 1000.0
+SINDEG90RADS = sin(DEG90RADS) / 1000.0
 
-    def _draw(self, options, renderer): # overloaded from robot
-        #self.setLocation(self.senses['robot']['x'], \
-        #                 self.senses['robot']['y'], \
-        #                 self.senses['robot']['z'], \
-        #                 self.senses['robot']['thr'] )
-        renderer.xformPush()
-        renderer.color((1, 0, 0))
-        #print "position: (", self.get('robot', 'x'), ",",  \
-        #      self.get('robot', 'y'), ")"
+from pyro.robot.service import Service, ServiceError
+
+class PlayerService(Service):
+    def __init__(self, dev, type):
+        Service.__init__(self, type)
+        self.dev = dev
+        self.name = type
+        self.startService()
+
+    def startService(self):
+        try:
+            self.dev.start(self.name)
+            time.sleep(.5)
+        except:
+            print "Device not supported: '%s'" % self.name
+            self.dev = 0
+        return self
+
+    def checkService(self):
+        if self.dev == 0:
+            print "Device '%s' not available" % self.name
+            return 0
+        return 1
+
+    def stopService(self):
+        if self.checkService():
+            self.dev.stop(self.name)
+            self.dev.__dict__[self.name] = {}
+
+    def getServiceData(self, pos = 0):
+        if self.checkService():
+            return self.dev.__dict__[self.name][pos]
+
+    def getServiceState(self):
+        if self.checkService():
+            if self.dev.__dict__[self.name] != {}:
+                return "started"
+            else:
+                return "stopped"
+
+    def getPose(self):
+        function = self.dev.__class__.__dict__[ "get_%s_pose" % self.name]
+        if function != None:
+            x, y, th = function(self.dev)
+            return (x / 1000.0, y / 1000.0, th % 360)
+        else:
+            raise ServiceError, "Function 'getPose' is not available for service '%s'" % self.name
+
+
+    def setPose(self, xM, yM, thDeg):
+        """ Move the device. x, y are in meters """
+        function = self.dev.__class__.__dict__[ "set_%s_pose" % self.name]
+        if function != None:
+            return function( self.dev, xM * 1000.0, yM * 1000.0, thDeg % 360)
+        else:
+            raise ServiceError, "Function 'setPose' is not available for service '%s'" % self.name
+
+class PlayerCommService(PlayerService):
+
+    def __init__(self, dev, name):
+        PlayerService.__init__(self, dev, name)
+        self.messages = []
+    
+    def sendMessage(self, message):
+        if self.dev.comms == {}:
+            print "Need to startService('comm') in robot: message not sent"
+            return
+        self.dev.send_message(message)
+
+    def getMessages(self):
+        if not 'comms' in dir(self.dev) or self.dev.comms == {}:
+            raise ServiceError, "Need to startService('comm') in robot"
+        #if self.dev.comms[0] != '':
+        #    self.update() # this is update in robot
+        tmp = self.messages
+        # reset queue:
+        self.messages = []
+        return tmp
+
+    def updateService(self):
+        for i in self.dev.comms:
+            msg = self.dev.get_comms()
+            if msg:
+                self.messages.append( msg )
+
+class PlayerPTZService(PlayerService):
+
+    def __init__(self, dev, name):
+        PlayerService.__init__(self, dev, name)
+        self.origPose = (0, 0, 120)
+
+    def init(self):
+        self.setPose( self.origPose )
+
+    def setPose(self, *args):
+        if len(args) == 3:
+            pan, tilt, zoom = args[0], args[1], args[2]
+        elif len(args[0]) == 3:
+            pan, tilt, zoom = args[0][0], args[0][1], args[0][2]
+        else:
+            raise AttributeError, "setPose takes pan, tilt, and zoom"
+        return self.dev.set_ptz(pan, tilt, zoom)
+
+    def getPose(self):
+        return self.dev.get_ptz()
+
+    def pan(self, numDegrees):
+        ptz = self.getPose()
+        return self.dev.set_ptz(numDegrees, ptz[1], ptz[2])
+
+    def panRel(self, numDegrees):
+        ptz = self.getPose()
+        return self.dev.set_ptz(ptz[0] + numDegrees, ptz[1], ptz[2])
+
+    def tilt(self, numDegrees):
+        ptz = self.getPose()
+        return self.dev.set_ptz(ptz[0], numDegrees, ptz[2])
+
+    def tiltRel(self, numDegrees):
+        ptz = self.getPose()
+        return self.dev.set_ptz(ptz[0], ptz[1] + numDegrees, ptz[2])
+
+    def panTilt(self, panDeg, tiltDeg):
+        ptz = self.getPose()
+        return self.dev.set_ptz(panDeg, tiltDeg, ptz[2])
+
+    def panTiltRel(self, panDeg, tiltDeg):
+        ptz = self.getPose()
+        return self.dev.set_ptz(ptz[0] + panDeg, ptz[1] + tiltDeg, ptz[2])
+
+    def centerCamera(self):
+        return self.setPose( self.origPose )
+
+    def zoom(self, numDegrees):
+        ptz = self.getPose()
+        return self.dev.set_ptz(ptz[0], ptz[1], numDegrees)
+
+    def zoomRel(self, numDegrees):
+        ptz = self.getPose()
+        return self.dev.set_ptz(ptz[0], ptz[1], ptz[2] + numDegrees)
+
+    def getPan(self):
+        ptz = self.getPose()
+        return ptz[0]
         
-        #renderer.xformXlate((self.get('robot', 'x'), \
-        #                     self.get('robot','y'), \
-        #                     self.get('robot','z')))
-        renderer.xformRotate(self.get('robot', 'th'), (0, 0, 1))
+    def getTilt(self):
+        ptz = self.getPose()
+        return ptz[1]
         
-        renderer.xformXlate(( 0, 0, .15))
+    def getZoom(self):
+        ptz = self.getPose()
+        return ptz[2]
+
+    def getRealPan(self):
+        ptz = self.getPose()
+        return ptz[0]
         
-        renderer.box((-.25, .25, 0), \
-                     (.25, .25, 0), \
-                     (.25, -.25, 0), \
-                     (-.25, .25, .35)) # d is over a, CW
+    def getRealTilt(self):
+        ptz = self.getPose()
+        return ptz[1]
         
-        renderer.color((1, 1, 0))
+    def getRealZoom(self):
+        ptz = self.getPose()
+        return ptz[2]
+
+    def canGetRealPanTilt(self):
+        return 1
+
+    def canGetRealZoom(self):
+        return 1
+
+    def getMaxPosPan(self):
+        return 100
+
+    def getMaxNegPan(self):
+        return -100
+
+    def getMaxPosTilt(self):
+        return 30
+
+    def getMaxNegTilt(self):
+        return -30
+
+    def getMaxZoom(self):
+        return 10
+
+    def getMinZoom(self):
+        return 120
+
+class PlayerGripperService(PlayerService):
+    # Gripper functions
+    #these also exist: 'gripper_carry', 'gripper_press', 'gripper_stay',
+
+    def open(self):
+        return self.dev.gripper_open() 
+
+    def close(self):
+        return self.dev.gripper_close() 
+
+    def stopMoving(self):
+        return self.dev.gripper_stop()
+
+    def liftUp(self):
+        return self.dev.gripper_up()
+
+    def liftDown(self):
+        return self.dev.gripper_down()
+
+    def liftStop(self):
+        return self.dev.gripper_stop()
+
+    def store(self):
+        return self.dev.gripper_store() 
+
+    def deploy(self):
+        return self.dev.gripper_deploy()
+
+    def halt(self):
+        return self.dev.gripper_halt()
+
+    def getState(self):
+        return self.dev.is_paddles_closed() # help!
+
+    def getBreakBeamState(self):
+        sum = 0
+        sum += self.dev.is_ibeam_obstructed() * 2 #FIX: which?
+        sum += self.dev.is_obeam_obstructed()
+        return sum
+
+    def isClosed(self): # FIX: add this to aria
+        return self.dev.is_paddles_closed() #ok
+
+    def isMoving(self):
+        return self.dev.is_paddles_moving() #ok
+
+    def isLiftMoving(self):
+        return self.dev.is_lift_moving() # ok
+
+    def isLiftMaxed(self):
+        return self.dev.is_lift_up() # ok
+
+class PlayerRobot(Robot):
+    def __init__(self, name = "Player", port = 6665):
+        Robot.__init__(self) # robot constructor
+        self.devData["simulated"] = 1
+        self.port = port
+        self.inform("Loading Player robot interface...")
+        self.name = name
+        self.connect() # set self.dev to player robot
+        # for some basic services:
+        for device in ["power", "position", "sonar", "laser",
+                       "bumper"]:
+            #is it supported? if so start it up:
+            devList = self.dev.get_device_list()
+            if device in devList:
+                if device == "ptz":
+                    self.devDataFunc[device] = PlayerPTZService(self.dev, "ptz")
+                elif device == "comms":
+                    self.devDataFunc[device] = PlayerCommService(self.dev, "comms")
+                elif device == "gripper":
+                    self.devDataFunc[device] = PlayerGripperService(self.dev, "gripper")
+                elif device == "sonar" or "laser":
+                    self.devDataFunc[device] = PlayerService(self.dev, device)
+                    self.devDataFunc["range"] = self.devDataFunc[device]
+                else:
+                    self.devDataFunc[device] = PlayerService(self.dev, device)
+            #self.supports["blob"] = PlayerService(self.dev, "blobfinder")
+            #self.supports["power"] = PlayerService(self.dev, "power")
+            #self.supports["position"] = PlayerService(self.dev, "position")
+            #self.supports["sonar"] = PlayerService(self.dev, "sonar")
+            #self.supports["laser"] = PlayerService(self.dev, "laser")
+            #self.supports["ptz"] = PlayerPTZService(self.dev, "ptz")
+            #self.supports["gps"] = PlayerService(self.dev, "gps")
+            #self.supports["bumper"] = PlayerService(self.dev, "bumper")
+            #self.supports["truth"] = PlayerService(self.dev, "truth")
+        # default values
+        self.devData["stall"] = 0
+        self.devData["x"] = 0.0
+        self.devData["y"] = 0.0
+        self.devData["th"] = 0.0
+        self.devData["thr"] = 0.0
+        self.devData["noise"] = .05 # 5 % noise
+        self.devData["datestamp"] = time.time()
+        # Can we get these from player?
+        self.devData["radius"] = 0.25
+        self.devData["type"] = "Player"
+        self.devData["subtype"] = 0
+        self.devData["units"] = "METERS"
+        self.devData["name"] = 0
+        #self.devData["simulated"] = self.simulated
+        self.localize(0.0, 0.0, 0.0)
+        self.update()
+    
+    def translate(self, translate_velocity):
+        self.dev.set_speed(translate_velocity * 900.0, None, None)
+
+    def rotate(self, rotate_velocity):
+        self.dev.set_speed(None, None, rotate_velocity * 65.0)
+
+    def move(self, translate_velocity, rotate_velocity):
+        self.dev.set_speed(translate_velocity * 900.0,
+                           0,
+                           rotate_velocity * 65.0)
+
+    # FIX: either sonar values are changing between calls to X, Y
+    # or sin/cos values are not taking into account offset from center
         
-        renderer.box((.13, -.05, .35), \
-                     (.13, .05, .35), \
-                     (.25, .05, .35), \
-                     (.13, -.05, .45)) # d is over a, CW
+    def localX(self, pos):
+        thr = (self.sonarGeometry[pos][2] + 90.0) * PIOVER180
+        dist = self.rawToUnits(self.dev.sonar[0][pos], 'sonar')
+        x = self.rawToUnits(self.sonarGeometry[pos][0], 'sonar')
+        return cos(thr) * dist
+
+    def localY(self, pos):
+        thr = (self.sonarGeometry[pos][2] - 90.0) * PIOVER180
+        dist = self.rawToUnits(self.dev.sonar[0][pos], 'sonar')
+        y = self.rawToUnits(self.sonarGeometry[pos][1], 'sonar') 
+        return sin(thr) * dist
+
+    def update(self):
+        self._update()
+        pos, speeds, stall = self.dev.get_position()
+        # (xpos, ypos, th), (xspeed, yspeed, rotatespeed), stall
+        self.devData["x"] = pos[0] / 1000.0
+        self.devData["y"] = pos[1] / 1000.0
+        self.devData["th"] = pos[2] # degrees
+        self.devData["thr"] = pos[2] * PIOVER180
+        self.devData["stall"] = stall
         
-        renderer.color((.5, .5, .5))
-        
-        # wheel 1
-        renderer.xformPush()
-        renderer.xformXlate((.25, .3, 0))
-        renderer.xformRotate(90, (1, 0, 0))
-        renderer.torus(.06, .12, 12, 24)
-        renderer.xformPop()
-        
-        # wheel 2
-        renderer.xformPush()
-        renderer.xformXlate((-.25, .3, 0))
-        renderer.xformRotate(90, (1, 0, 0))
-        renderer.torus(.06, .12, 12, 24)
-        renderer.xformPop()
-        
-        # wheel 3
-        renderer.xformPush()
-        renderer.xformXlate((.25, -.3, 0))
-        renderer.xformRotate(90, (1, 0, 0))
-        renderer.torus(.06, .12, 12, 24)
-        renderer.xformPop()
-        
-        # wheel 4
-        renderer.xformPush()
-        renderer.xformXlate((-.25, -.3, 0))
-        renderer.xformRotate(90, (1, 0, 0))
-        renderer.torus(.06, .12, 12, 24)
-        renderer.xformPop()        
-        
-        # sonar
-        renderer.xformPush()
-        renderer.color((0, 0, .7))
-        for i in range(self.get('sonar', 'count')):
-            y1, x1, z1 = -self.get('sonar', 'x', i), \
-                         -self.get('sonar', 'y', i), \
-                         self.get('sonar', 'z', i)
-            #y2, x2, z2 = -self.get('sonar', 'ox', i), \
-            #             -self.get('sonar', 'oy', i), \
-            #             self.get('sonar', 'oz', i)
-            # FIXME: what are the actual positions of sensor x,y?
-            x2, y2, z2 = 0, 0, z1
-            arc    = self.get('sonar', 'arc', i) # in radians
-            renderer.ray((x1, y1, z1), (x2, y2, z2), arc)
-            
-        renderer.xformPop()        
-        
-        # end of robot
-        renderer.xformPop()
+    def rawToUnits(self, raw, name, noise = 0.0):
+        raw = raw / 1000.0
+        if noise > 0:
+            if random.random() > .5:
+                raw += (raw * (noise * random.random()))
+            else:
+                raw -= (raw * (noise * random.random()))
+        if name == 'sonar':
+            val = min(max(raw, 0.0), 2.99)
+        elif name == 'laser':
+            val = min(max(raw, 0.0), 8000.0)
+        else:
+            raise TypeError, "Type is invalid"
+        units = self.get('robot/units')
+        if units == "ROBOTS":
+            return val / 0.75 # Pioneer is about .75 meters diameter
+        elif units == "MM":
+            return val * 1000.0
+        elif units == "CM":
+            return (val) * 100.0 # cm
+        elif units == "METERS" or units == "RAW":
+            return (val) 
+        elif units == "SCALED":
+            return val / 2.99
+        else:
+            raise TypeError, "Units are set to invalid type"
+
+    def localize(self, x = 0, y = 0, th = 0):
+        """
+        Set robot's internal pose to x (meters), y (meters),
+        th (radians)
+        """
+        self.dev.set_odometry(x * 1000, y * 1000, th)
+        self.x = x
+        self.y = y
+        self.th = th
+        self.thr = self.th * PIOVER180
 
     def connect(self):
         self.dev = player('localhost', port=self.port)
-        print "Device: ", self.dev
-        self.dev.start('position')
-        self.dev.start('sonar')
-        self.localize(0.0, 0.0, 0.0)
+        #self.localize(0.0, 0.0, 0.0)
         
-        #     def localize(self, x = 0.0, y = 0.0, th = 0.0):
-        #         self.diffX = x - self.dev.getX()
-        #         self.diffY = y - self.dev.getY()
-        #         self.diffTh = th - self.dev.getTh()
-        self.sensorSet = {'all': range(16),
-                          'front': (3, 4),
-                          'front-left' : (1,2,3),
-                          'front-right' : (4, 5, 6),
-                          'front-all' : (1,2, 3, 4, 5, 6),
-                          'left' : (0, 15), 
-                          'right' : (7, 8), 
-                          'left-front' : (0,), 
-                          'right-front' : (7, ),
-                          'left-back' : (15, ),
-                          'right-back' : (8, ),
-                          'back-right' : (9, 10, 11),
-                          'back-left' : (12, 13, 14), 
-                          'back' : (11, 12),
-                          'back-all' : ( 9, 10, 11, 12, 13, 14)}
-        self.z = 0.0
-        self.y = 0.0
-        self.x = 0.0
-        self.senses = {}
-        simulated = self.simulated
-	# robot senses (all are functions):
-        self.senses['robot'] = {}
-        self.senses['robot']['simulator'] = lambda dev, x = simulated: x
-        self.senses['robot']['stall'] = lambda dev: self.stall
-        self.senses['robot']['x'] = self.getX
-        self.senses['robot']['y'] = self.getY
-        self.senses['robot']['z'] = self.getZ
-        self.senses['robot']['radius'] = lambda self: 250.0 # in MM
-        self.senses['robot']['th'] = self.getTh # in degrees
-        self.senses['robot']['thr'] = self.getThr # in radians
-        self.senses['robot']['type'] = lambda dev: 'Player'
-        self.senses['robot']['units'] = lambda dev: 'METERS'
-        self.senses['robot']['name'] = lambda dev, x = self.name: x
-        
-        self.senses['sonar'] = {}
-        self.sonarGeometry = self.dev.get_sonar_geometry()
-        self.sonarAngles = map(lambda lst: lst[2], self.sonarGeometry)
-        self.senses['sonar']['count'] = lambda dev: len(self.sonarGeometry)
-        self.senses['sonar']['type'] = lambda dev: 'range'
-        
-        # location of sensors' hits:
-        self.senses['sonar']['x'] = lambda dev, pos: self.localXDev(dev, pos)
-        self.senses['sonar']['y'] = lambda dev, pos: self.localYDev(dev, pos)
-        self.senses['sonar']['z'] = lambda dev, pos: 0.03 # meters
-        self.senses['sonar']['value'] = lambda dev, pos: self.rawToUnits(dev, self.dev.sonar[0][pos], 'sonar', self.noise)
-        self.senses['sonar']['maxvalue'] = lambda dev: self.rawToUnits(dev, 2990, 'sonar', 0)
-        self.senses['sonar']['units'] = lambda dev: "ROBOTS"
-        
-        # location of origin of sensors:
-        self.senses['sonar']['ox'] = lambda dev, pos:self.sonarGeometry[pos][0]
-        self.senses['sonar']['oy'] = lambda dev, pos:self.sonarGeometry[pos][1]
-        self.senses['sonar']['oz'] = lambda dev, pos: 0.03 # meters
-        self.senses['sonar']['th'] = lambda dev, pos:self.sonarGeometry[pos][2] * PIOVER180 
-        #         # in radians:
-        self.senses['sonar']['arc'] = lambda dev, pos, \
-                                      x = (7.5 * PIOVER180) : x
-        
-        # Make a copy, for default:
-        self.senses['range'] = self.senses['sonar']
-        self.senses['self'] = self.senses['robot']
-
-        self.controls['move'] = self.moveDev
-        self.controls['translate'] = self.translateDev
-        self.controls['rotate'] = self.rotateDev
-        self.controls['update'] = self.updateDev
-        #self.controls['localize'] = self.localizeDev
-        console.log(console.INFO,'Player control drivers loaded')
-
+if __name__ == '__main__':
+    myrobot = PlayerBase()
+    myrobot.update()
+    #myrobot.translate(.2)
+    #myrobot.translate(.0)
+    #myrobot.disconnect()
