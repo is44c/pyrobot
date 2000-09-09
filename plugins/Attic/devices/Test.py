30b451b3b99e2d9c4a409ab7a7559294ea5e223e
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2000-09-09 22:43:28 +0000

    Added "Save Map..." to File menu; renamed Plots Views; Added inspector to Help menu; added Map... to load menu; moved "Move" next to "Load" menu; disabled double-click from file select widgets; added "Clear Messages" to Window menu; renamed Refresh  to Window,

---
+++ plugins/Attic/devices/Test.py
@@ -0,0 +1,13 @@
+# A Simple Map
+
+
+def INIT(engine):
+    # Fix: need start goal positions too
+    grid = [[0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],            
+            [0, 0, 1, 0, 0, 0, 0],            
+            [0, 0, 1, 0, 0, 0, 0],            
+            [0, 0, 1, 0, 0, 0, 0],            
+            [0, 0, 1, 0, 0, 0, 0],            
+            [0, 0, 1, 0, 0, 0, 0]]
+    return grid
