66530ffbe920800cdf336ca0cdec52a12681e4e7
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2000-09-09 22:42:56 +0000

    New examples; added Evan Moses' named area robot function

---
+++ trunk/plugins/brains/BBCapture.py
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<HTML>
+ <HEAD>
+  <TITLE>Index of /~dblank/pyro/plugins/brains</TITLE>
+ </HEAD>
+ <BODY>
+<H1>Index of /~dblank/pyro/plugins/brains</H1>
+<PRE><IMG SRC="/icons/blank.gif" ALT="     "> <A HREF="?N=D">Name</A>                    <A HREF="?M=A">Last modified</A>       <A HREF="?S=A">Size</A>  <A HREF="?D=A">Description</A>
+<HR>
+<IMG SRC="/icons/back.gif" ALT="[DIR]"> <A HREF="/~dblank/pyro/plugins/">Parent Directory</A>        27-Mar-2002 09:32      -  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="BBCapture.py">BBCapture.py</A>            29-Mar-2002 16:57     4k  
+<IMG SRC="/icons/unknown.gif" ALT="[   ]"> <A HREF="BBCapture.pyc">BBCapture.pyc</A>           29-Mar-2002 16:57     6k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="BBGoto.py">BBGoto.py</A>               28-Mar-2002 22:46     2k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="BBOffice.py">BBOffice.py</A>             29-Mar-2002 12:06     7k  
+<IMG SRC="/icons/unknown.gif" ALT="[   ]"> <A HREF="BBOffice.pyc">BBOffice.pyc</A>            28-Mar-2002 17:03    14k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="BBSquare.py">BBSquare.py</A>             28-Mar-2002 15:53     3k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="BBTest.py">BBTest.py</A>               25-Mar-2002 21:57     4k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="BBTest2.py">BBTest2.py</A>              25-Mar-2002 21:57     4k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="BBWander.py">BBWander.py</A>             29-Mar-2002 15:09     2k  
+<IMG SRC="/icons/unknown.gif" ALT="[   ]"> <A HREF="BBWander.pyc">BBWander.pyc</A>            29-Mar-2002 15:09     3k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="BBWanderAndCapture.py">BBWanderAndCapture.py</A>   29-Mar-2002 16:07     3k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="Braitenberg.py">Braitenberg.py</A>          25-Mar-2002 21:57     1k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="Develop.py">Develop.py</A>              25-Mar-2002 21:57     6k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="KLight.py">KLight.py</A>               25-Mar-2002 21:57     1k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="SOMAA.py">SOMAA.py</A>                25-Mar-2002 21:57     4k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="SOMWander.py">SOMWander.py</A>            25-Mar-2002 21:57     4k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="SimpleBrain.py">SimpleBrain.py</A>          29-Mar-2002 16:02     2k  
+<IMG SRC="/icons/unknown.gif" ALT="[   ]"> <A HREF="SimpleBrain.pyc">SimpleBrain.pyc</A>         29-Mar-2002 16:02     2k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="Test.py">Test.py</A>                 25-Mar-2002 21:57     1k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="Wander.py">Wander.py</A>               25-Mar-2002 21:57     1k  
+<IMG SRC="/icons/p.gif" ALT="[   ]"> <A HREF="__init__.py">__init__.py</A>             25-Mar-2002 21:57     0k  
+</PRE><HR>
+<ADDRESS>Apache/1.3.20 Server at emergent.brynmawr.edu Port 80</ADDRESS>
+</BODY></HTML>
