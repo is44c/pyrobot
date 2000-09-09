4694aaa74dc81fab4c3be6d6360949be77274acd
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2000-09-09 12:20:21 +0000

    Have sonar mostly done for Player; still hacking on Player code: laser next

--- robot/player.py
+++ robot/player.py
@@ -4,7 +4,7 @@ from pyro.robot import *
 from math import pi, cos, sin
 from os import getuid
 from pyro.robot.driver.player import *
-import random, time
+import time
 
 PIOVER180 = pi / 180.0
 DEG90RADS = 0.5 * pi
@@ -14,8 +14,9 @@ SINDEG90RADS = sin(DEG90RADS) / 1000.0
 from pyro.robot.service import Service, ServiceError
 
 class PlayerService(Service):
-    def __init__(self, dev, type):
+    def __init__(self, dev, type, groups = {}):
         Service.__init__(self, type)
+        self.groups = groups
         self.dev = dev
         self.name = type
         self.startService()
@@ -67,6 +68,112 @@ class PlayerService(Service):
             return function( self.dev, xM * 1000.0, yM * 1000.0, thDeg % 360)
         else:
             raise ServiceError, "Function 'setPose' is not available for service '%s'" % self.name
+
+class PlayerSonarService(PlayerService):
+    def __init__(self, dev, name):
+        PlayerService.__init__(self, dev, name)
+        self.sonarGeometry = self.dev.get_sonar_geometry()
+        self.groups = {'all': range(16),
+                       'front': (3, 4),
+                       'front-left' : (1,2,3),
+                       'front-right' : (4, 5, 6),
+                       'front-all' : (1,2, 3, 4, 5, 6),
+                       'left' : (0, 15), 
+                       'right' : (7, 8), 
+                       'left-front' : (0,), 
+                       'right-front' : (7, ),
+                       'left-back' : (15, ),
+                       'right-back' : (8, ),
+                       'back-right' : (9, 10, 11),
+                       'back-left' : (12, 13, 14), 
+                       'back' : (11, 12),
+                       'back-all' : ( 9, 10, 11, 12, 13, 14)}
+        # What are the raw units?
+        self.devData["rawunits"] = "MM"
+        # These are fixed in meters: DO NOT CONVERT ----------------
+        self.devData["radius"] = 0.750 # meters
+        # These are fixed in rawunits (above): DO NOT CONVERT:
+        self.devData['maxvalueraw'] = 5000 # in rawunits
+        # ----------------------------------------------------------
+        # All of the rest of the measures are relative to units, given in rawunits:
+        self.devData['units']    = "ROBOTS"
+        self.postSet()
+        self.devData["count"] = len(self.sonarGeometry)
+        self.devData["noise"] = 0.05 # 5 percent
+        # These are per reading:
+        self.devDataFunc['ox']    = lambda pos:self.rawToUnits(self.sonarGeometry[pos][0])
+        self.devDataFunc['oy']    = lambda pos:self.rawToUnits(self.sonarGeometry[pos][1])
+        self.devDataFunc['oz']    = lambda pos: 0.03 # meters
+        self.devDataFunc['th']    = lambda pos:self.sonarGeometry[pos][2] * PIOVER180 # radians
+        self.devDataFunc['arc']   = lambda pos: (7.5 * PIOVER180) # radians
+        self.devDataFunc['x']     = self.getX
+        self.devDataFunc['y']     = self.getY
+	self.devDataFunc['z']     = lambda pos: self.rawToUnits(0.03) # meters
+        self.devDataFunc['value'] = lambda pos: self.rawToUnits(self.dev.sonar[0][pos], self.devData["noise"])
+        self.devDataFunc['pos']   = lambda pos: pos
+        self.devDataFunc['group'] = self.getGroupNames
+
+    def postSet(self):
+        """ Anything that might change after a set """
+        self.devData['maxvalue'] = self.rawToUnits(self.devData['maxvalueraw'])
+
+    def getX(self, pos):
+        thr = (self.sonarGeometry[pos][2] + 90.0) * PIOVER180
+        dist = self.rawToUnits(self.dev.sonar[0][pos])
+        x = self.rawToUnits(self.sonarGeometry[pos][0])
+        return cos(thr) * dist
+
+    def getY(self, pos):
+        thr = (self.sonarGeometry[pos][2] - 90.0) * PIOVER180
+        dist = self.rawToUnits(self.dev.sonar[0][pos])
+        y = self.rawToUnits(self.sonarGeometry[pos][1]) 
+        return sin(thr) * dist
+
+class PlayerLaserService(PlayerService):
+    def __init__(self, dev, name):
+        PlayerService.__init__(self, dev, name)
+        count = int((self.dev.laser[0][0][1] - self.dev.laser[0][0][0]) / self.dev.laser[0][0][2] + 1)
+        part = int(count/8)
+        start = 0
+        posA = part
+        posB = part * 2
+        posC = part * 3
+        posD = part * 4
+        posE = part * 5
+        posF = part * 6
+        posG = part * 7
+        end = count
+        self.groups = {'all': range(count),
+                       'right': range(0, posB),
+                       'left': range(posF, end),
+                       'front': range(posC, posE),
+                       'front-right': range(posB, posD),
+                       'front-left': range(posD, posF),
+                       'front-all': range(posB, posF),
+                       'right-front': range(posA, posB),
+                       'right-back': range(start, posA),
+                       'left-front': range(posF,posG),
+                       'left-back': range(posG,end),
+                       'back-right': [],
+                       'back-left': [],
+                       'back': [],
+                       'back-all': []}
+        self.devData['maxvalueraw'] = 8.000 # meters
+        self.devData['units']    = "ROBOTS"
+        self.devData['maxvalue'] = 8.000
+        self.devData['index'] = self.dev.laser.keys()[0]
+        self.devData["count"] = count
+        self.devDataFunc['ox']    = lambda pos: 0
+        self.devDataFunc['oy']    = lambda pos: 0
+        self.devDataFunc['oz']    = lambda pos: 0
+        self.devDataFunc['th']    = lambda pos: self.dev.laser[0][0][0] + (self.dev.laser[0][0][2] * pos) # in degrees
+        self.devDataFunc['arc']   = lambda pos: self.dev.laser[0][0][2] # in degrees
+        self.devDataFunc['x']     = lambda pos: 0
+        self.devDataFunc['y']     = lambda pos: 0
+	self.devDataFunc['z']     = lambda pos: 0.03 # meters
+        self.devDataFunc['value'] = lambda pos: self.dev.laser[0][1][pos] / 1000.0 # fix, get in units
+        self.devDataFunc['pos']   = lambda pos: pos
+        self.devDataFunc['group']   = self.getGroupNames
 
 class PlayerCommService(PlayerService):
 
@@ -257,24 +364,30 @@ class PlayerRobot(Robot):
         Robot.__init__(self) # robot constructor
         self.devData["simulated"] = 1
         self.port = port
-        self.inform("Loading Player robot interface...")
         self.name = name
         self.connect() # set self.dev to player robot
         # for some basic services:
+        # Make sure sonar is before laser, so if you have
+        # laser, it will be the default 'range' device
         for device in ["power", "position", "sonar", "laser",
                        "bumper"]:
             #is it supported? if so start it up:
             devList = self.dev.get_device_list()
-            if device in devList:
+            # (('fiducial', 0, 6665), ('comms', 0, 6665), ...)
+            devNameList = map(lambda triplet: triplet[0], devList)
+            if device in devNameList:
                 if device == "ptz":
                     self.devDataFunc[device] = PlayerPTZService(self.dev, "ptz")
                 elif device == "comms":
                     self.devDataFunc[device] = PlayerCommService(self.dev, "comms")
                 elif device == "gripper":
                     self.devDataFunc[device] = PlayerGripperService(self.dev, "gripper")
-                elif device == "sonar" or "laser":
-                    self.devDataFunc[device] = PlayerService(self.dev, device)
-                    self.devDataFunc["range"] = self.devDataFunc[device]
+                elif device == "laser":
+                    self.devDataFunc[device] = PlayerLaserService(self.dev, "laser")
+                    self.devDataFunc["range"] = self.devDataFunc["laser"]
+                elif device == "sonar":
+                    self.devDataFunc[device] = PlayerSonarService(self.dev, "sonar")
+                    self.devDataFunc["range"] = self.devDataFunc["sonar"]
                 else:
                     self.devDataFunc[device] = PlayerService(self.dev, device)
             #self.supports["blob"] = PlayerService(self.dev, "blobfinder")
@@ -340,33 +453,6 @@ class PlayerRobot(Robot):
         self.devData["thr"] = pos[2] * PIOVER180
         self.devData["stall"] = stall
         
-    def rawToUnits(self, raw, name, noise = 0.0):
-        raw = raw / 1000.0
-        if noise > 0:
-            if random.random() > .5:
-                raw += (raw * (noise * random.random()))
-            else:
-                raw -= (raw * (noise * random.random()))
-        if name == 'sonar':
-            val = min(max(raw, 0.0), 2.99)
-        elif name == 'laser':
-            val = min(max(raw, 0.0), 8000.0)
-        else:
-            raise TypeError, "Type is invalid"
-        units = self.get('robot/units')
-        if units == "ROBOTS":
-            return val / 0.75 # Pioneer is about .75 meters diameter
-        elif units == "MM":
-            return val * 1000.0
-        elif units == "CM":
-            return (val) * 100.0 # cm
-        elif units == "METERS" or units == "RAW":
-            return (val) 
-        elif units == "SCALED":
-            return val / 2.99
-        else:
-            raise TypeError, "Units are set to invalid type"
-
     def localize(self, x = 0, y = 0, th = 0):
         """
         Set robot's internal pose to x (meters), y (meters),
