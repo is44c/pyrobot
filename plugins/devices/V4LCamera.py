9a1d7df5312e95c6bdc712fb17fa79c9be6df43c
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2000-09-10 11:49:52 +0000

    Made a good many fixes

--- plugins/devices/V4LCamera.py
+++ plugins/devices/V4LCamera.py
@@ -2,5 +2,5 @@ from pyro.camera.v4l import V4LGrabber
 from pyro.vision.cvision import VisionSystem
 
 def INIT(robot):
-    return {"V4LCamera" : V4LGrabber( 384, 240, channel = 0,
-                                      visionSystem = VisionSystem())}
+    return {"camera" : V4LGrabber( 384, 240, channel = 0,
+                                   visionSystem = VisionSystem())}
