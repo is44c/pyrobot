ca99e5343eed7c1314512f6b449f06df240ec541
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2000-09-10 11:49:54 +0000

    Added V4L cameras based on channel

---
+++ trunk/plugins/devices/V4LCamera1.py
@@ -0,0 +1,6 @@
+from pyro.camera.v4l import V4LCamera
+from pyro.vision.cvision import VisionSystem
+
+def INIT(robot):
+    return {"camera" : V4LCamera( 384, 240, channel = 1,
+                                  visionSystem = VisionSystem())}
