2654172370f78be5ef5e9b5031b7a7e56d88d933
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2000-09-11 00:49:33 +0000

    Working vision system!

---
+++ trunk/plugins/brains/VisionTest.py
@@ -0,0 +1,34 @@
+# A bare brain
+
+from pyro.brain import Brain
+from time import sleep
+
+def process(self):
+   self.addFilter('match', 255 , 166 , 120 , )
+   self.addFilter('match', 132 , 52 , 46 , )
+   #self.apply("match", ) 
+   #camera.apply("maxBlobs", 0, 1, 255, "mass") 
+
+class SimpleBrain(Brain):
+
+   def setup(self, **args):
+      print "Loading arg: '%s'" % args.get('my_arg')
+      self.camera = self.getRobot().startService("FakeCamera")[0]
+      self.camera.makeWindow()
+      self.camera.setVisionCallBack( lambda self = self.camera: process(self) )
+      # initialize your vars here!
+      
+   # Only method you have to define is the step method:
+
+   def step(self):
+      print "robot setpping..."
+            
+
+# -------------------------------------------------------
+# This is the interface for calling from the gui engine.
+# Takes one param (an engine), and returns a Brain object:
+# -------------------------------------------------------
+
+def INIT(engine):
+   return SimpleBrain('SimpleBrain', engine, my_arg = "testing")
+      
