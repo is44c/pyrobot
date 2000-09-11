8769225616f1c37eae1dd7daf1263e2da7122ded
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2000-09-11 00:49:34 +0000

    New rewritten V4L interface

--- camera/v4l/V4L.h
+++ camera/v4l/V4L.h
@@ -1,19 +1,67 @@
-#ifndef __V4L_H__
-#define __V4L_H__
+#ifndef __V4L_H
+#define __V4L_H
 
-#include <Python.h>
-#include "v4lcap.h"
 #include "../vision/Vision.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
 
-class V4L : public Vision {
+#include <errno.h>
+#include <sys/time.h>
+#include <asm/types.h>
+#include <linux/videodev.h>	/* change this to "videodev2.h" for v4l2 */
+#include "jpeglib.h"
+// #define GRAB_DEVICE       "/dev/video1"
+// #define GRAB_WIDTH        320
+// #define GRAB_HEIGHT       240
+// #define GRAB_TEXT         "v4lgrab %d.%m.%Y %H:%M:%S"        /* strftime */
+
+#ifdef VIDIOCGCAP
+/* these work for v4l only, not v4l2 */
+//  # define GRAB_SOURCE      1
+  # define GRAB_NORM        VIDEO_MODE_NTSC
+#endif
+
+//extern int errno;
+
+class V4L : public Vision
+{
+ public:
+  V4L ( char *device_name, int wi, int he, int de, int ch);
+  ~V4L();
 
  public:
-  V4L(char *device_name, int wi, int he, int de, int ch);
-  void updateMMap();
 
-  struct image_cap *v4ldev;
-  int channel;
-  char *device;
-};
+  void updateMMap(void);
+  void init(void);
+  void swap_rgb24(char *mem, int n); 
+
+#ifdef VIDIOC_QUERYCAP
+
+  struct v4l2_capability    grab_cap;
+  struct v4l2_format        grab_pix;
+  int                       grab_fd, grab_size;
+  //unsigned char            *grab_data;
 
 #endif
+
+#ifdef VIDIOCGCAP
+
+  struct video_capability   grab_cap;
+  struct video_mmap         grab_buf;
+  struct video_channel	    grab_chan;
+  int                       grab_fd, grab_size;
+  //unsigned char            *grab_data;
+
+#endif
+
+  char device[255];
+  int channel;
+
+};
+
+#endif // __V4L_H
