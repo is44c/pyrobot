53077b36b3a6c235d5dcc3860f33730ffe7a69be
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2000-09-11 00:49:34 +0000

    turn filter on/off

--- camera/v4l/__init__.py
+++ camera/v4l/__init__.py
@@ -1,10 +1,7 @@
-from v4l import *
-from pyro.camera import *
-from UserString import UserString
+from v4l import V4L      # cameraDevice
+from pyro.camera import Camera, CBuffer
 import types, sys, os
-
 import time
-import re
 
 class V4LGrabber(Camera):
    """
@@ -56,51 +53,6 @@ class V4LGrabber(Camera):
          self.cameraDevice.updateMMap()
       except:
          print "v4l: refresh_image failed"
-      try:
+      if self.vision != None:
          self.vision.processAll()
-      except:
-         print "error in vision system?"
-
-if __name__ == "__main__":
-   cam = V4LGrabber(384, 240)
-
-   if 1:
-      cam.makeWindow()
-      while 1:
-         cam.updateWindow()
-
-   if 0:
-      print "Testing frames per/second:",
-      start = time.time()
-      for i in range(100):
-         print ".",
-         sys.stdout.flush()
-         cam.update()
-      stop = time.time()
-      print "done!"
-      print "FPS = ", 100.00/ (stop - start)
-
-   if 0:
-      print "Saving images:"
-      print "test1.tga..."
-      cam.saveAsTGA("./test1.tga")
-      time.sleep(3)
-      print "test1.tga..."
-      cam.saveAsTGA("./test2.tga")
-      cam.update()
-      print "test1.tga..."
-      cam.saveAsTGA("./test3.tga")
-
-      print """
-      If everything worked, then test1.tga and test2.tga should be identical,
-      but test3.tga should have been snapped about a second later.  They are
-      all saved in the current directory.
-      """
-   if 0:
-      print "Testing greyscale..."
-      cam = V4LGrabber(384, 240, 1)
-      cam.save("./testgrey.tga")
-      print "Saved testgrey.tga"
-
-   del cam
 
