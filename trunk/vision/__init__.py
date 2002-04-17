618e23df832a30e9e1193970795d795781e7ec1f
emoses <emoses@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-04-17 15:27:17 +0000

    Placed PyroImage, Histogram, and Bitmap code from blob.py into __init__.py to create a vision library -EMM

---
+++ trunk/vision/__init__.py
@@ -0,0 +1,536 @@
+import struct
+from pyro.system import file_exists
+
+class PyroImage:
+   """
+   A Basic Image class. 
+   """
+   def __init__(self, width = 0, height = 0, depth = 3, init_val = 0):
+      """
+      Constructor. Depth is bytes per pixel.
+      """
+      self.width = width
+      self.height = height
+      self.depth = depth
+      self.data = [init_val] * height * width * depth
+
+   def loadFromFile(self, filename):
+      """
+      Method to load image from file. Currently must be in PBM P6 Format
+      (color binary).
+      """
+      fp = open(filename, "r")
+      type = fp.readline() # P6
+      (width, height) = fp.readline().split(' ')
+      self.width = int(width)
+      self.height = int(height)
+      self.depth = 3
+      self.data = [0] * self.height * self.width * self.depth
+      irange = int(fp.readline())
+      x = 0
+      while (x < self.width * self.height):
+         c = fp.read(1)
+         r = float(struct.unpack('h', c + '\x00')[0]) / float(irange)
+         c = fp.read(1)
+         g = float(struct.unpack('h', c + '\x00')[0]) / float(irange)
+         c = fp.read(1)
+         b = float(struct.unpack('h', c + '\x00')[0]) / float(irange)
+         self.data[x * self.depth + 0] = r
+         self.data[x * self.depth + 1] = g
+         self.data[x * self.depth + 2] = b
+         x += 1
+
+   def saveToFile(self, filename):
+      """
+      Method to save image to a file. Currently will save PBM P5/P6 Format.
+      """
+      fp = open(filename, "w")
+      if self.depth == 3:
+         fp.writelines("P6\n") # P6
+      else:
+         fp.writelines("P5\n") # P5
+      fp.writelines("%d %d\n" % (self.width, self.height))
+      fp.writelines("255\n")
+      x = 0
+      while (x < self.width * self.height):
+         for i in range(self.depth):
+            c = int(self.data[x * self.depth + i] * 255.0)
+            r = struct.pack('h', c)[0]
+            fp.writelines(r)
+         x += 1
+
+   def grayScale(self):
+      """
+      Method to convert depth 3 color into depth 1 grayscale
+      """
+      if self.depth == 1:
+         return
+      data = [0] * self.width * self.height
+      for h in range(self.height):
+         for w in range(self.width):
+            r = self.data[(w + h * self.width) * self.depth + 0]
+            g = self.data[(w + h * self.width) * self.depth + 1]
+            b = self.data[(w + h * self.width) * self.depth + 2]
+            data[w + h * self.width] = (r + g + b) / 3.0
+      self.data = data
+      self.depth = 1
+
+   def display(self):
+      """
+      Display PyroImage in ASCII Art.
+      """
+      if self.depth == 3:
+         line = ''
+         for h in range(self.height):
+            for w in range(self.width):
+               r = self.data[(w + h * self.width) * self.depth + 0]
+               g = self.data[(w + h * self.width) * self.depth + 1]
+               b = self.data[(w + h * self.width) * self.depth + 2]               
+               if int(((r + g + b) / 3 ) * 9):
+                  line += "%d" % int(((r + g + b) / 3 ) * 9)
+               else:
+                  line += '.'
+            print line; line = ''
+         print line; line = ''
+      else:
+         line = ''
+         for h in range(self.height):
+            for w in range(self.width):
+               r = self.data[(w + h * self.width) * self.depth + 0]
+               if int(r * 9):
+                  line += "%d" % int(r * 9)
+               else:
+                  line += '.'
+            print line; line = ''
+         print line; line = ''
+
+   def set(self, x, y, val, offset = 0):
+      """
+      Method to set a pixel to a value. offset is r, g, b (0, 1, 2)
+      """
+      self.data[(x + y * self.width) * self.depth + offset] = val
+
+   def setVal(self, x, y, val):
+      """
+      Method to set the entire RGB value of a pixel.
+      val should be an n-tuple (or length n list), where n is the depth of the
+      image.  For RGB, it should be of the form (r, g, b)
+      """
+      if len(val) != self.depth:
+         print "Error in setVal: val is not the same length as depth"
+         return
+      
+      for offset in range(self.depth):
+         self.data[(x + y * self.width) * self.depth + offset] = val[offset]
+
+   def incr(self, x, y, offset = 0):
+      """
+      Method to increment a pixel value. offset is r, g, b (0, 1, 2)
+      """
+      self.data[(x + y * self.width) * self.depth + offset] += 1
+
+
+   def get(self, x, y, offset = 0):
+      """
+      Get a pixel value. offset is r, g, b = 0, 1, 2.
+      """
+      return self.data[(x + y * self.width) * self.depth + offset]
+
+   def getVal(self, x, y):
+      """
+      Get the entire color value of the pixel in quetion, returned as a tuple.
+      If this is an RGB image, it will be of the form (r, g, b).
+      """
+      start = (x + y * self.width) * self.depth
+      end = start + self.depth
+      return (tuple(self.data[start:end]))
+
+   def reset(self, vector):
+      """
+      Reset an image to a vector.
+      """
+      for v in range(len(vector)):
+         self.data[v] = vector[v]
+
+   def filter(self, r, g, b, threshold):
+      bitmap = Bitmap(self.width, self.height)
+      for i in range( bitmap.width):
+         for j in range( bitmap.height):
+            redDiff = (self.get(i, j, 0) - r) ** 2
+            greenDiff = (self.get(i, j, 1) - g) ** 2
+            blueDiff = (self.get(i, j, 2) - b) ** 2
+            finalval = 0
+            if redDiff < threshold and \
+               greenDiff < threshold and \
+               blueDiff < threshold:
+               finalval = 1
+            bitmap.set(i, j, finalval, 0)
+      return bitmap
+
+   def histogram(self, cols = 20, rows = 20, initvals = 0):
+      """
+      Creates a histogram.
+      """
+      if not initvals:
+         histogram = Histogram(cols, rows)
+      else:
+         histogram = initvals
+      for h in range(self.height):
+         for w in range(self.width):
+            r = self.get(w, h, 0)
+            g = self.get(w, h, 1)
+            b = self.get(w, h, 2)
+            if r == 0: r = 1.0
+            br = min(int(b/float(r) * float(cols - 1)), cols - 1)
+            gr = min(int(g/float(r) * float(rows - 1)), rows - 1)
+            histogram.incr(br, gr)
+      return histogram
+   
+class Histogram(PyroImage):
+   """
+   Histogram class. Based on Image, but has methods for display.
+   """
+   def __init__(self, width, height, init_val = 0):
+      PyroImage.__init__(self, width, height, 1, init_val) # 1 bit depth
+
+   def display(self):
+      """
+      Display bitmap ASCII art.
+      """
+      maxval = 0
+      for h in range(self.height):
+         for w in range(self.width):
+            maxval = max(maxval, self.get(w, h))
+      for h in range(self.height):
+         for w in range(self.width):
+            if maxval:
+               print "%5d" % self.get(w, h),
+            else:
+               print ' ',
+         print ''
+      print ''
+
+class Bitmap(PyroImage):
+   """
+   Bitmap class. Based on Image, but has methods for blobs, etc.
+   """
+   def __init__(self, width, height, init_val = 0):
+      PyroImage.__init__(self, width, height, 1, init_val) # 1 bit depth
+      self.equivList = [0] * 2000
+      for n in range(2000):
+         self.equivList[n] = n
+
+   def display(self):
+      """
+      Display bitmap ASCII art.
+      """
+      for h in range(self.height):
+         for w in range(self.width):
+            if self.data[w + h * self.width]:
+               try:
+                  print self.equivList[self.data[w + h * self.width]],
+               except:
+                  print ">",
+            else:
+               print '.',
+         print ''
+      print ''
+
+   def avgColor(self, img):
+      """
+      Return an n-tuple of the average color of the image where the bitmap is true,
+      where n is the depth of img.  That is, using the bitmap as a mask, find the average color of img.
+      img and self should have the same dimensions.
+      """
+
+      if not (self.width == img.width and self.height == img.height):
+         print "Error in avgColor: Bitmap and Image do not have the same dimensions"
+         return
+      
+      avg = []
+      n = 0
+      for i in range(img.depth):
+         avg.append(0) #an n-array of zeros
+         
+      for x in range(self.width):
+         for y in range(self.height):
+            if (self.get(x, y)): #if the bitmap is on at this point
+               for i in range(img.depth):
+                  avg[i] += img.get(x, y, i)
+                  n += 1
+
+      if n == 0:
+         return tuple(avg)
+      else:
+         for i in range(img.depth):
+            avg[i] /= n
+         return tuple(avg)
+               
+   def blobify(self):
+      """
+      Algorithm for 1 pass blobification. Returns a bitmap.
+      Probably need to return a blob object.
+      """
+      self.equivList = [0] * 2000
+      for n in range(2000):
+         self.equivList[n] = n
+      blob = Bitmap(self.width, self.height)
+      count = 1
+      for w in range(self.width):
+         for h in range(self.height):
+            if self.get(w, h):
+               if h == 0 and w == 0: # if in upper left hand corner
+                  # new blob!
+                  blob.set(w, h, count)
+                  count += 1
+               elif w == 0:  # if in first col 
+                  if self.get(w, h - 1): # if pixel above is on
+                     # old blob
+                     blob.set(w, h, blob.get(w, h - 1))
+                  else: # above is off
+                     # new blob!
+                     blob.set(w, h, count)
+                     count += 1
+               elif h == 0:
+                  if self.get(w - 1, h): # if pixel to left is on
+                     # old blob
+                     blob.set(w, h, blob.get(w - 1, h))
+                  else: # left is off
+                     # new blob!
+                     blob.set(w, h, count)
+                     count += 1
+               elif self.get(w - 1, h)  and self.get(w, h - 1):
+                  # both on!
+                  if blob.get(w - 1, h) == blob.get(w, h - 1):
+                     blob.set(w, h, blob.get(w - 1, h))
+                  else: # intersection of two blobs
+                     minBlobNum = min( self.equivList[blob.get(w - 1, h)],
+                                       self.equivList[blob.get(w, h - 1)])
+                     maxBlobNum = max( self.equivList[blob.get(w - 1, h)],
+                                       self.equivList[blob.get(w, h - 1)])
+                     blob.set(w, h, minBlobNum)
+                     for n in range(2000):
+                        if self.equivList[n] == maxBlobNum:
+                           self.equivList[n] = minBlobNum
+               else:
+                  if self.get(w - 1, h): # if pixel to left is on
+                     # old blob
+                     blob.set(w, h, blob.get(w - 1, h))
+                  elif self.get(w, h - 1): # if pixel above is on
+                     # old blob
+                     blob.set(w, h, blob.get(w, h - 1))
+                  else: # left is off, above is off
+                     # new blob!
+                     blob.set(w, h, count)
+                     count += 1
+      blob.equivList = self.equivList[:]
+      return blob
+
+
+"""
+assume that we are starting our x,y coordinates from the upper-left,
+and starting at (0,0), such that (0,0) represents the upper-left-most
+pixel
+"""
+class Point:
+   def __init__(self, x=0, y=0):
+      self.x = x
+      self.y = y
+   def set(self, x, y):
+      self.x = x
+      self.y = y
+   def setx(self, x):
+      self.x = x
+   def sety(self, y):
+      self.y = y
+   def clear(self):
+      self.x = 0
+      self.y = 0
+
+class Blob:
+   def __init__(self, pixel):
+      self.mass = 1
+      self.ul = Point(pixel.x, pixel.y)
+      self.lr = Point(pixel.x, pixel.y)
+      self.cm = Point(pixel.x, pixel.y)
+      self.next = 0
+
+   def addpixel(self, pixel):
+      if pixel.x < self.ul.x:
+         self.ul.x = pixel.x
+      elif pixel.x > self.lr.x:
+         self.lr.x = pixel.x
+      if pixel.y < self.ul.y:
+         self.ul.y = pixel.y
+      elif pixel.y > self.lr.y:
+         self.lr.y = pixel.y
+      self.cm.x = float(self.mass * self.cm.x + pixel.x)/float(self.mass + 1)
+      self.cm.y = float(self.mass * self.cm.y + pixel.y)/float(self.mass + 1)
+      self.mass += 1
+
+   def joinblob(self, other):
+      if other.ul.x < self.ul.x:
+         self.ul.x = other.ul.x
+      elif other.lr.x > self.lr.x:
+         self.lr.x = other.lr.x
+      if other.ul.y < self.ul.y:
+         self.ul.y = other.ul.y
+      elif other.lr.y > self.lr.y:
+         self.lr.y = other.lr.y
+      self.cm.x = float(self.mass * self.cm.x + other.mass * other.cm.x) \
+                  /float(self.mass + other.mass)
+      self.cm.y = float(self.mass * self.cm.y + other.mass * other.cm.y) \
+                  /float(self.mass + other.mass)
+      self.mass += other.mass   
+         
+   def width(self):
+      return self.lr.x - self.ul.x + 1
+   def height(self):
+      return self.lr.y - self.ul.y + 1
+   def area(self):
+      return self.width() * self.height()
+   def density(self):
+      return float(self.mass)/float(self.area())
+   def display(self):
+      print "mass:", self.mass
+      print "area:", self.area()
+      print "density:", self.density()
+      print "center of mass:", self.cm.x, ",", self.cm.y
+      print "upper-left bound:", self.ul.x, ",", self.ul.y
+      print "lower-right bound:", self.lr.x, ",", self.lr.y
+
+
+
+class Blobdata:
+   def __init__(self, bitmap):
+      self.blobmap = Bitmap(bitmap.width, bitmap.height)
+      self.bloblist = [0] * 2000
+      count = 1
+
+      # build the blobmap and construct unjoined Blob objects
+      for w in range(bitmap.width):
+         for h in range(bitmap.height):
+            if bitmap.get(w, h):
+               if h == 0 and w == 0: # in upper left corner -- new blob
+                  self.bloblist[count] = Blob(Point(w,h))
+                  self.blobmap.set(w, h, count)
+                  count += 1
+               elif w == 0:  # if in first col 
+                  if bitmap.get(w, h - 1): # pixel above is on -- old blob
+                     self.bloblist[self.blobmap.get(w,h-1)].addpixel(Point(w,h))
+                     self.blobmap.set(w, h, self.blobmap.get(w, h - 1))
+                  else: # above is off -- new blob
+                     self.bloblist[count] = Blob(Point(w,h))
+                     self.blobmap.set(w, h, count)
+                     count += 1
+               elif h == 0: # in first row
+                  if bitmap.get(w - 1, h): # pixel to left is on -- old blob
+                     self.bloblist[self.blobmap.get(w-1,h)].addpixel(Point(w,h))
+                     self.blobmap.set(w, h, self.blobmap.get(w - 1, h))
+                  else: # left is off -- new blob
+                     self.bloblist[count] = Blob(Point(w,h))
+                     self.blobmap.set(w, h, count)
+                     count += 1
+               elif bitmap.get(w - 1, h) and bitmap.get(w, h - 1): # both on
+                  if self.blobmap.get(w - 1, h) == self.blobmap.get(w, h - 1):
+                     self.bloblist[self.blobmap.get(w-1,h)].addpixel(Point(w,h))
+                     self.blobmap.set(w, h, self.blobmap.get(w - 1, h))
+                  else: # intersection of two blobs
+                     minBlobNum = min( \
+                        self.blobmap.equivList[self.blobmap.get(w - 1, h)],
+                        self.blobmap.equivList[self.blobmap.get(w, h - 1)])
+                     maxBlobNum = max( \
+                        self.blobmap.equivList[self.blobmap.get(w - 1, h)],
+                        self.blobmap.equivList[self.blobmap.get(w, h - 1)])
+                     self.bloblist[minBlobNum].addpixel(Point(w,h))
+                     self.blobmap.set(w, h, minBlobNum)
+                     for n in range(2000):
+                        if self.blobmap.equivList[n] == maxBlobNum:
+                           self.blobmap.equivList[n] = minBlobNum
+               else:
+                  if bitmap.get(w - 1, h): # left is on -- old blob
+                     self.bloblist[self.blobmap.get(w-1,h)].addpixel(Point(w,h))
+                     self.blobmap.set(w, h, self.blobmap.get(w - 1, h))
+                  elif bitmap.get(w, h - 1): # above is on -- old blob
+                     self.bloblist[self.blobmap.get(w,h-1)].addpixel(Point(w,h))
+                     self.blobmap.set(w, h, self.blobmap.get(w, h - 1))
+                  else: # left is off, above is off -- new blob
+                     self.bloblist[count] = Blob(Point(w,h))
+                     self.blobmap.set(w, h, count)
+                     count += 1
+
+      # count the number of blobs and join the actual blob objects
+      self.nblobs = 0
+      for n in range(1,count):
+         if self.blobmap.equivList[n] == n:
+            for m in range(n+1,count):
+               if self.blobmap.equivList[m] == n:
+                  self.bloblist[n].joinblob(self.bloblist[m])
+                  self.bloblist[m] = 0
+            self.nblobs += 1
+
+      # shift the elements of bloblist[] so that the first Blob is at
+      # bloblist[0] and the rest follow consecutively
+      for n in range(1,count):
+         m = n-1
+         while self.bloblist[m] == 0:
+            self.bloblist[m] = self.bloblist[m+1]
+            self.bloblist[m+1] = 0
+            if m == 0:
+               break
+            m -= 1
+
+
+   # sort based on mass, area, or density
+   def sort(self, mode="mass"):
+      newlist = [0] * self.nblobs
+      if mode == "mass":
+         for i in range(0,self.nblobs):
+            max = 0
+            m = 0
+            for n in range(0,self.nblobs):
+               if self.bloblist[n] == 0:
+                  pass
+               elif self.bloblist[n].mass > max:
+                  max = self.bloblist[n].mass
+                  m = n
+            newlist[i] = self.bloblist[m]
+            self.bloblist[m] = 0
+         self.bloblist = newlist
+      elif mode == "area":
+         for i in range(0,self.nblobs):
+            max = 0
+            m = 0
+            for n in range(0,self.nblobs):
+               if self.bloblist[n] == 0:
+                  pass
+               elif self.bloblist[n].area() > max:
+                  max = self.bloblist[n].area()
+                  m = n
+            newlist[i] = self.bloblist[m]
+            self.bloblist[m] = 0
+         self.bloblist = newlist
+      elif mode == "density":
+         for i in range(0,self.nblobs):
+            max = 0
+            m = 0
+            for n in range(0,self.nblobs):
+               if self.bloblist[n] == 0:
+                  pass
+               elif self.bloblist[n].density() > max:
+                  max = self.bloblist[n].density()
+                  m = n
+            newlist[i] = self.bloblist[m]
+            self.bloblist[m] = 0
+         self.bloblist = newlist
+      else:
+         print "unknown sorting parameter:", mode
+
+   def display(self):
+      self.blobmap.display()
+      print "Total number of blobs:", self.nblobs
+      print ""
+      for n in range(0,self.nblobs):
+         print "Blob", n, ":"
+         self.bloblist[n].display()            
+         print ""
