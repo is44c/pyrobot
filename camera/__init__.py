8195e3e56009ca5ad97a478239db75b2d9068061
emoses <emoses@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-04-17 15:28:42 +0000

    Created the Camera base class.  This should be subclassed to interface directly with specific cameras.

--- camera/__init__.py
+++ camera/__init__.py
@@ -1,9 +1,28 @@
 # Camera class
 
 import os
+from pyro.vision import PyroImage
 
 class Camera:
+"""
+A base class for Camera
+"""
 
-    def snap(self, filename = 'camera.ppm'):
-        print "Capturing image '%s'..." % filename
-        os.system("bttvgrab -G /dev/video0 -f %s -o pgm -N NTSC -Q -l 1 -S 1 -d q -W 32 -w 48" % filename)
+
+    def __init__(self, width, height, depth = 3):
+    """
+    To specify the resolution of a particular camera, overload this
+    constructor with one that initalizes the dimensions itself
+    """
+        self.width = width
+        self.height = height
+        self.depth = depth
+
+    def update(self):
+    """
+    Return a PyroImage from the camera.  This method should be
+    overloaded to interface with the actual camera.
+    """
+        img = PyroImage(self.width, self.height, self.depth, 0)
+        
+        return img
