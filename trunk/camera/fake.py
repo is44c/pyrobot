82286916e7ecfc48925565ced03b1610c4111e0f
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-04-18 00:33:52 +0000

    Fake camera class

---
+++ trunk/camera/fake.py
@@ -0,0 +1,21 @@
+from pyro.camera import *
+
+class FakeCamera(Camera):
+   """
+   A Fake camera class. Simulates live vision. Call update() to get image.
+   """
+   def __init__(self):
+      self.count = 0
+      Camera.__init__(self, 0, 0) # will get info from file
+
+   def _update(self):
+      from os import getenv
+      pyrodir = getenv('PYRO')
+      if not file_exists(pyrodir + "/vision/snaps/som-%d.ppm" % self.count):
+         self.count = 0
+      if not file_exists(pyrodir + "/vision/snaps/som-%d.ppm" % self.count):
+         import sys
+         print "Can't find $PYRO/vision/snaps/ images!"
+         sys.exit(1)
+      self.loadFromFile(pyrodir + "/vision/snaps/som-%d.ppm" % self.count)
+      self.count += 1
