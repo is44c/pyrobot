67ef0dc48b9185fe96ddd5f3d388d30682de7484
emoses <emoses@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-04-20 18:54:21 +0000

    Added the ArmorRobot robot to Pyro.  This is a simple test version.

---
+++ trunk/robot/armor.py
@@ -0,0 +1,40 @@
+#Define ArmorRobot, an interface to the
+#pair of arms
+
+from pyro.robot import *
+
+class SaphiraRobot(Robot):
+   def __init__(self, name = None, simiulator = 1):
+      Robot.__init__(self, name, "Armor")
+
+   def _draw(self, options, renderer):
+      renderer.xformPush()
+      rederer.color(.5, .5, .5)
+
+      renderer.box((0, 0, 0), \
+                   (1, 0, 0), \
+                   (1, 1, 0), \
+                   (1, 1, 1))
+
+      renderer.xformPop()
+
+   def getOptions(self):
+      pass
+
+   def connect(self):
+      pass
+
+   def localize(self, x = 0.0, y = 0.0, z = 0.0):
+      pass
+
+   def disconnect(self):
+      pass
+
+   def loadDrivers(self):
+      pass
+
+   def update(self):
+      pass
+   
+
+       
