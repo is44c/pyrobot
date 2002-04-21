9a6eab5a88b7a7fda9bf791c57ecf7d8d10d560b
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-04-21 22:44:06 +0000

    BT848 to work on Pioneers. Slow.

--- camera/bt848/bt848grab.cpp
+++ camera/bt848/bt848grab.cpp
@@ -1,13 +1,11 @@
 #include "BT848.h"
 
 main(int argc, char *argv[]) {
-  char number[80];
-  strcpy(number, argv[1]);
+  char filename[255];
+  strcpy(filename, argv[1]);
   BT848 *cam = new BT848( "/dev/bt848fg0", ICOLS, IROWS );
   cam->Update();
   cam->Update();
   cam->Update();
-  char buffer[80];
-  sprintf(buffer, "som-%s.ppm", number);
-  cam->saveFile(buffer);
+  cam->saveFile(filename);
 }
