48768941650c173bc8b2101475ebbe35b3d51670
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-06-10 17:46:11 +0000

    Fixed some typos; added test

--- robot/Attic/driver/Attic/video/__init__.py
+++ robot/Attic/driver/Attic/video/__init__.py
@@ -10,29 +10,31 @@ from pyro.robot.driver import *
 import pyro.gui.console as console
 import low_video
 
-
 class VideoSense(Sense):
-    def __init__(self,fd,xres,yres):
+    def __init__(self, fd, xres, yres, depth = 3):
         self.fd = fd
+        self.depth = depth
         self.xres = xres
         self.yres = yres
-        self.size = xres*yres*3
-
+        self.size = xres*yres*depth
+        
     def getValue(self):
-        return fd.read(self.size*3)
+        return self.fd.read(self.size)
     
 def getVideoDriverDefaults():
+    """ A default dictionary of some standard values. """
     vidopts = {}
     vidopts['name'] = 'camera'
-    vidopts['device'] = '/dev/video'
+    vidopts['device'] = '/dev/video0'
     vidopts['xres'] = 160
     vidopts['yres'] = 120
     return vidopts
 
 class VideoDriver(Driver):
-    def __init__(self,options):
+    def __init__(self, options):
         Driver.__init__(self)
         console.log(console.INFO,'loading video driver')
+        # for each camera:
         for vidopt in options:
             try:
                 fd = open(vidopt['device'])
@@ -48,4 +50,8 @@ class VideoDriver(Driver):
                 
             except IOError:
                 console.log(console.WARNING,'Could not open video device. Options specified: '+str(vidopt))
-
+                
+if __name__ == '__main__':
+    # send in a list of cameras:
+    vd = VideoDriver([getVideoDriverDefaults()])
+    print vd.senses['camera'].getValue()
