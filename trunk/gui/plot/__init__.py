b533f41f4e13fd3e5da1d30b9967562a191a85f2
emoses <emoses@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-06-18 15:27:34 +0000

    Implemented scrolling; smoothed everything out; Normal redraws no longer require redrawing every line.  TODO:  Add tick marks, maybe use two canvases to double-buffer for freezing, get rid of little gaps that form on first scroll and on unfreeze.

--- trunk/gui/plot/__init__.py
+++ trunk/gui/plot/__init__.py
@@ -32,25 +32,62 @@ class GeneralPlot:
       self.colors = ['blue', 'red', 'tan', 'yellow', 'orange', \
                      'black', 'azure', 'beige', 'brown', 'coral', \
                      'gold', 'ivory', 'moccasin', 'navy', 'salmon', \
-                     'tan', 'ivory']
+                     'green', 'ivory']
 
       self.width = width
       self.height = height
 
       self.dataWindowSize = self.width/self.stepLength
       if self.dataWindowSize < 1:
-         self.ataWindowSize = 1
+         self.dataWindowSize = 1
       
       self.canvas = Canvas(self.win, width=self.width, height=self.height,
                            scrollregion=(0, 0,
-                                         history * stepLength, self.height))
-#                           xScrollIncrement=self.stepLength
+                                         history * stepLength, self.height),
+                           xscrollincrement=self.stepLength)
       self.canvas.pack()
-      self.dump()
 
-   def dump(self):
-      print self.history
-                         
+      self.frozen = 0
+
+      b1 = Button(self.win, text="<==", command=self.scrollLeft)
+      b1.pack(side=LEFT)
+      self.freezeButton = Button(self.win, text="Freeze", command=self.freezePress)
+      b3 = Button(self.win, text="==>", command=self.scrollRight)
+      b3.pack(side=RIGHT)
+      self.freezeButton.pack(anchor=S)
+
+
+   def scrollLeft(self):
+      self.canvas.xview(SCROLL, -10, UNITS)
+      self.freeze()
+
+   def scrollRight(self):
+      self.canvas.xview(SCROLL, 10, UNITS)
+      self.freeze()
+
+   def freeze(self):
+      self.freezeButton["text"] = "Unfreeze"
+      self.frozen = 1
+                            
+   def freezePress(self):
+      if self.frozen:
+         #unfreeze
+         self.canvas.xview(MOVETO, 1.0)
+         self.canvas.delete('data')
+         for val in range(self.lastHist):
+            for n in range(len(self.sensorList)):
+               self.canvas.create_line((val-1)*self.stepLength,
+                                       self.convertVal(self.history[val-1][n]),
+                                       val*self.stepLength,
+                                       self.convertVal(self.history[val][n]),
+                                       tags = 'data',
+                                       width = 2,
+                                       fill = self.colors[n])
+         self.freezeButton["text"] = "Freeze"
+         self.frozen = 0
+      else:
+         self.freeze()
+         
    
    def convertVal(self, value):
       """
@@ -74,13 +111,20 @@ class GeneralPlot:
          else:
             self.lastHist += 1
             self.history += [[None]*len(self.sensorList)]
-         
+               
 
          for n in range(len(self.sensorList)):
             self.history[self.lastHist][n] = self.robot.get(self.sensorType, 
-                                                       'value', 
-                                                       self.sensorList[n])
-         if self.lastHist < self.dataWindowSize:
+                                                            'value', 
+                                                            self.sensorList[n])
+         if not self.frozen:
+            if self.lastHist < self.dataWindowSize:
+               pass
+            elif self.dataWindowSize <= self.lastHist < self.maxHist - 1:
+               self.canvas.xview(SCROLL, 1, UNITS)
+            else:
+               self.canvas.move('data', -self.stepLength, 0)
+               
             for n in range(len(self.sensorList)):
                self.canvas.create_line((self.lastHist-1)*self.stepLength,
                                        self.convertVal(self.history[self.lastHist-1][n]),
@@ -89,18 +133,5 @@ class GeneralPlot:
                                        tags = 'data',
                                        width = 2,
                                        fill = self.colors[n])
-               
-         else:
-            self.canvas.xview(SCROLL, 1, UNITS)
-            self.canvas.delete('data')
-            for val in range(self.lastHist - self.dataWindowSize, self.lastHist):
-               for n in range(len(self.sensorList)):
-                  self.canvas.create_line((val-1)*self.stepLength,
-                                          self.convertVal(self.history[val-1][n]),
-                                          val*self.stepLength,
-                                          self.convertVal(self.history[val][n]),
-                                          tags = 'data',
-                                          width = 2,
-                                          fill = self.colors[n])
             
                
