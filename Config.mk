76bd84b199adf7669783de73ab24245d0ec2ab8d
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-06-18 02:26:34 +0000

    Version 0.8.3: new makefile format for easy compiling. Everything compiles except for robot/driver/testc

--- Config.mk
+++ Config.mk
@@ -1,5 +1,5 @@
 export PYROVERSION = 0.8.2
-export PYRODEFINE = -DPYRO_VER=\"$(PYRO)\"
+export PYTHON_VERSION=2.1
 export SAPHIRA=/usr/local/saphira/ver62
-export PYTHONINCLUDE=-I/usr/local/include/python2.0
-export PYRO=$(HOME)/pyro
+export PYTHONINCLUDE=-I/usr/local/include/python$(PYTHON_VERSION)
+export PYRODEFINE = -DPYRO_VER=\"$(PYRO)\"
