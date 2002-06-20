957eb2757ed08a996e1198d6412b85012f776746
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-06-20 00:49:53 +0000

    Not needed

--- trunk/Config.mk
+++
@@ -1,5 +0,0 @@
-export PYROVERSION = 0.8.2
-export PYTHON_VERSION=2.1
-export SAPHIRA=/usr/local/saphira/ver62
-export PYTHONINCLUDE=-I/usr/local/include/python$(PYTHON_VERSION)
-export PYRODEFINE = -DPYRO_VER=\"$(PYRO)\"
