6bfe7eb6fdc533cbbe784ff3eac09eb7dfc41ddc
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-06-30 16:26:54 +0000

    *** empty log message ***

--- brain/seqXor.py
+++ brain/seqXor.py
@@ -11,33 +11,46 @@ def sequentialXorSweeps(n, limit):
     totalErr2 = 0
     totalErr3 = 0
     current = int(random.random()*2)
+    # Init context 
+    n.getLayer('context').copyActivations([.5] * n.getLayer('context').size)
     while step <= limit:
-        n.setInputs([[current]])
+        #n.setInputs([[current]])
+        n.getLayer('input').copyActivations( [current] )
         next = int(random.random()*2)
-        n.setOutputs([[next]])
-        (tssError, totalCorrect, totalCount) = n.sweep()
+        #n.setOutputs([[next]])
+        n.getLayer('output').copyTarget( [next] )
+        (tssError, totalCorrect, totalCount) = n.step()
         err1 = tssError
-        n.setInputs([[next]])
+        #n.setInputs([[next]])
+        n.getLayer('input').copyActivations( [next] )
         result = xor(current,next)
-        n.setOutputs([[result]])
-        (tssError, totalCorrect, totalCount) = n.sweep()
+        #n.setOutputs([[result]])
+        n.getLayer('output').copyTarget( [result] )
+        n.getLayer('context').copyActivations(n.getLayer('hidden').activation)
+        (tssError, totalCorrect, totalCount) = n.step()
         err2 = tssError
-        n.setInputs([[result]])
+        #n.setInputs([[result]])
+        n.getLayer('input').copyActivations( [result] )
+        n.getLayer('context').copyActivations(n.getLayer('hidden').activation)
         current = int(random.random()*2)
-        n.setOutputs([[current]])
-        (tssError, totalCorrect, totalCount) = n.sweep()
+        #n.setOutputs([[current]])
+        n.getLayer('output').copyTarget( [current] )
+        (tssError, totalCorrect, totalCount) = n.step()
+        # for next step
+        n.getLayer('context').copyActivations(n.getLayer('hidden').activation)
         err3 = tssError
-        if step % n.reportRate == 0:
-            print "Epoch: #%6d" % step
         totalErr1 = totalErr1 + err1
         totalErr2 = totalErr2 + err2
         totalErr3 = totalErr3 + err3
+        if step % n.reportRate == 0:
+            print "Epoch: #%6d Errors = %.3f %.3f %.3f Avg = %.3f %.3f %.3f" \
+                  % (step, err1, err2, err3, totalErr1 / step, \
+                     totalErr2 / step, totalErr3 / step)
         step = step + 1
     print "Only the second error value is predicatable and should be lower"
     print "Total errors  : %.4f %.4f %.4f" % (totalErr1, totalErr2, totalErr3)
     print "Average errors: %.4f %.4f %.4f" \
           % (totalErr1/step, totalErr2/step, totalErr3/step)
-
     
 if __name__ == '__main__':
     print "Sequential XOR modeled after Elman's experiment ..........."
@@ -46,8 +59,8 @@ if __name__ == '__main__':
     print "the second output is predictable."
     n = SRN()
     n.addSRNLayers(1,2,1)
-    n.setEpsilon(0.25)
-    n.setMomentum(0.7)
+    n.setEpsilon(0.7)
+    n.setMomentum(0.1)
     n.setBatch(0)
     n.setQuickProp(0)
     n.setReportRate(1000)
