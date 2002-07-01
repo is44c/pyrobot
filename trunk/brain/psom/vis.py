d0737043688fb072c139cd851ca34ddc332c1939
emoses <emoses@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-07-01 18:53:10 +0000

    Fixed some bugs, added support for rect topology, better visualizers

--- trunk/brain/psom/vis.py
+++ trunk/brain/psom/vis.py
@@ -8,9 +8,19 @@ GRAY_STEP = 20
 class VisPsom(psom):
    """
    A vizualized psom class.
+   Takes all the same arguments as the psom class, with the following added
+   keyword arguments:
+
+   vis_radius:  The radius (in pixels) of the som cells.  Defaults to 15
+
+   vis_padding: The amount of space (in pixels) on each side of each cell.
+      Defaults to 2
+
+   vis_vectortype:  The type of VisVector vizualier to use to display the
+      model vectors.  Defaults to 'generic'.  See visvector.py.
    """
 
-   def __init__(self, *args, vis_radius=15, **keys):
+   def __init__(self, *args, **keys):
 
       #get Vis-specific keyword arguements out
       if 'vis_radius' in keys.keys():
@@ -27,14 +37,21 @@ class VisPsom(psom):
          self.vectortype = keys['vis_vectortype']
          del keys['vis_vectortype']
       else:
-         self.vectortype = "IR"
+         self.vectortype = "Generic"
 
         
       psom.__init__(self, *args, **keys)
 
       self.win = Tk()
+      
+      cellwidth = (self.vis_padding + self.vis_radius) * 2
+      #offset to set off the rows for a hexagonal topology
+      if self.topol == 'hexa':
+         offset = cellwidth/2
+      else: #topol = 'rect'
+         offset = 0
       self.canvas = Canvas(self.win,
-                           width=self.xdim * (2*self.vis_radius+2*self.vis_padding) + self.vis_radius+self.vis_padding,
+                           width=self.xdim * (2*self.vis_radius+2*self.vis_padding) + offset,
                            height=self.ydim * (2*self.vis_radius+2*self.vis_padding),
                            bg='white')
       self.canvas.bind("<Button-1>", self.canvas_clicked)
@@ -51,15 +68,12 @@ class VisPsom(psom):
       
       self.cells = []
       self.cellhash = {}
-      cellwidth = (self.vis_padding + self.vis_radius) * 2
-
       self.history = {}
 
-      #for now, assume hexagonal
       for y in range(self.ydim):
          self.cells.append([])
          for x in range(self.xdim):
-            x0 = (cellwidth * x) + self.vis_padding + (cellwidth/2 * (y % 2))
+            x0 = (cellwidth * x) + self.vis_padding + (offset * (y % 2))
             y0 = (cellwidth * y) + self.vis_padding
             x1 = x0 + (self.vis_radius * 2)
             y1 = y0 + (self.vis_radius * 2)
@@ -76,7 +90,8 @@ class VisPsom(psom):
       if (box[0] < event.x < box[2]) and (box[1] < event.y < box[3]):
          x, y = self.cellhash[cell]
          vec = self.get_model_vector(point(x, y))
-         VisVectorFactory(self.vectortype, vec, "Vector @ (%d, %d)" % (x, y))
+         visclass = getVisVectorByName(self.vectortype)
+         visclass(vec, "(%d, %d)" % (x, y))
       
 
    def _setcell(self, x, y, level):
@@ -106,8 +121,17 @@ class VisPsom(psom):
          self._updatefill()
       return retval
 
+   def train(self, vector):
+      retval = psom.train(self,vector)
+      if self.followMap.get():
+         #if we're displaying the map
+         pt = retval.point.x, retval.point.y
+         self.history[pt] = ACT_MAX
+         self._updatefill()
+      return retval
+
 if __name__ == "__main__":
-   mysom = VisPsom(file='ex.cod')
+   mysom = VisPsom(file='ex.cod', vis_vectortype="Generic")
    mydataset = dataset(file='ex.dat')
    mysom.init_training(0.02,4.0,5000)
    mysom.train_from_dataset(mydataset)
