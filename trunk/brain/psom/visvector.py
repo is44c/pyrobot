d1410ef3de1e91b3dbf1f73b2214e8ae3ca77878
emoses <emoses@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-07-08 18:50:19 +0000

    Added HintonVisVector

--- trunk/brain/psom/visvector.py
+++ trunk/brain/psom/visvector.py
@@ -8,6 +8,7 @@ from pyro.brain.psom import *
 from Tkinter import *
 import struct
 import ImageTk
+import pyro.gui.plot.hinton
 
 class VisVector:
    """
@@ -95,6 +96,66 @@ class GrayscaleVisVector(VisVector):
       b.pack(side=BOTTOM)
       self.win.mainloop()
 
+class HintonVisVector(VisVector, pyro.gui.plot.hinton.Hinton):
+   """
+   Use the Hinton plot to display the vector
+   """
+   def __init__(self, vector, maxval=None, title=""):
+      if not maxval:
+         maxval = max(vector)
+      pyro.gui.plot.hinton.Hinton.__init__(self, data=vector.get_elts(),
+                                           maxvalue=maxval, title=title)
+      b = Button(self.win, text="Close", command=self.close)
+      b.pack()
+
+class BarGraphVisVector(VisVector):
+   """
+   Display a vector as a series of horizonal bars
+   """
+   def __init__(self, vector, minval=None, maxval=None, title=""):
+      """
+      Min and max can either be scalar minima and maxima for the
+      entire vector, or it can be a list of the same length as the vector,
+      each element corresponding to an element of the vector
+      """
+      self.vector = vector.get_elts()
+      self.length = len(self.vector)
+
+      #if the min and max are given as scalar values,
+      #convert them to vectors of the length of the
+      #vector
+      
+      try:
+         len(minval)
+         self.min = minval
+      except TypeError:
+         if minval == None:
+            self.min = [min(self.vector)] * self.length
+         else:
+            self.min = [minval] * self.length
+
+      try:
+         len(maxval)
+         self.max = maxval
+      except TypeError:
+         if max == None:
+            self.max = [max(self.vector)] * self.length
+         else:
+            self.max = [maxval] * self.length
+
+      self.win = Tk()
+      self.win.title(title)
+      for i in range(len(self.vector)):
+         c = Canvas(self.win, height= 20, width = 100)
+         c.create_text(2, 10, anchor=W, text=str(i))
+         vec_len = abs(int((self.vector[i] / (self.max[i] - self.min[i])) * 85))
+         c.create_rectangle(15, 3, vec_len + 15, 17, fill="red")
+         c.pack()
+         
+      b = Button(self.win, text="Close", command=self.close)
+      b.pack(side=BOTTOM)
+
+      
 def getVisVectorByName(type):
    """
    Given a type of VisVector as a string, create and initialize an
@@ -106,12 +167,16 @@ def getVisVectorByName(type):
       return GenericVisVector
    elif type =="Grayscale" or type =="Greyscale":
       return GrayscaleVisVector
+   elif type == "BarGraph":
+      return BarGraphVisVector
+   elif type =="Hinton":
+      return HintonVisVector
    else:
       raise "VisVector type not supported"
 
-if __name__=="__main__":
-   from pyro.brain.psom import *
-   grayvis = getVisVectorByName("Grayscale")
-   piclist = [float(x)/255.0 for x in range(256)]
-   grayvis(vector(piclist), 16, 16)
-   print "Done"
+#  if __name__=="__main__":
+#     from pyro.brain.psom import *
+#     grayvis = getVisVectorByName("Grayscale")
+#     piclist = [float(x)/255.0 for x in range(256)]
+#     grayvis(vector(piclist), 16, 16)
+#     print "Done"
