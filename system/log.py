fb4f527c4c0cf62ca8dd7d7f299a7ee262f93252
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-07-16 04:07:53 +0000

    Added output plots to Development; cleaned up Log() so that errors don't occur if already closed; prints robot type.

--- system/log.py
+++ system/log.py
@@ -12,17 +12,18 @@ class Log:
       Pass in robot and brain so that we can query them (and maybe make
       copies and query them on occation).
       """
+      self.open = 1
       timestamp = self.timestamp()
       if filename == None:
          filename= timestamp + '.log'
       self.filename = filename
       self.file = open(filename,'w')
       self.writeln("Log opened: " + timestamp)
-      if name:
+      if name != None:
          self.writeln('Experiment name: ' + name)
-      if robot:
+      if robot != None:
          self.writeln('Robot: ' + robot.get('robot','type'))
-      if brain:
+      if brain != None:
          self.writeln('Brain: ' + brain.name)
 
    def timestamp(self):
@@ -31,20 +32,28 @@ class Log:
 
    def write(self, msg):
       """ Write a string to the log """
-      self.file.write(msg)
+      if self.open:
+         self.file.write(msg)
 
    def writeln(self, msg):
       """ Write a line (with newline) to the log """
-      self.file.write(msg + "\n")
+      if self.open:
+         self.file.write(msg + "\n")
 
    def close(self):
       """ Close the log """
-      year,month,day,hour,minute,second,one,two,three=time.localtime()
-      self.writeln("Log closed: " + self.timestamp())
-      self.file.close()
+      if self.open:
+         year,month,day,hour,minute,second,one,two,three=time.localtime()
+         self.writeln("Log closed: " + self.timestamp())
+         self.file.close()
+         self.open = 0
    
 if __name__ == '__main__':
    log = Log()
+   log.write("testing...")
+   log.write("testing...")
+   log.writeln("tested!")
+   log.close()
    log.write("testing...")
    log.write("testing...")
    log.writeln("tested!")
