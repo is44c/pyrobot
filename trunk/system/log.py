18ecbf7e7613bc3a261c9974ad9dff95836ef8b6
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2002-07-18 02:30:50 +0000

    Added echo to log; log records USER and HOSTNAME; Development prints Level 2 data

--- trunk/system/log.py
+++ trunk/system/log.py
@@ -1,16 +1,18 @@
 # A log class
 
-import time
+import time, os
 
 class Log:
    """
    A log class to do automatically what needs to be done for each
    experiment.
    """
-   def __init__(self,filename = None, name = None, robot = None, brain = None):
+   def __init__(self,filename = None, name = None, robot = None, brain = None,
+                echo = 1):
       """
       Pass in robot and brain so that we can query them (and maybe make
       copies and query them on occation).
+      If echo is True, then it will echo the log file to the terminal
       """
       self.open = 1
       timestamp = self.timestamp()
@@ -18,6 +20,7 @@ class Log:
          filename= timestamp + '.log'
       self.filename = filename
       self.file = open(filename,'w')
+      self.echo = echo
       self.writeln("Log opened: " + timestamp)
       if name != None:
          self.writeln('Experiment name: ' + name)
@@ -25,18 +28,25 @@ class Log:
          self.writeln('Robot: ' + robot.get('robot','type'))
       if brain != None:
          self.writeln('Brain: ' + brain.name)
-
+      if os.environ.has_key('HOSTNAME'):
+         self.writeln('Hostname: ' + os.environ['HOSTNAME'])
+      if os.environ.has_key('USER'):
+         self.writeln('User: ' + os.environ['USER'])
    def timestamp(self):
       year,month,day,hour,minute,second,one,two,three=time.localtime()
       return '%4d-%02d-%02d_%02d.%02d.%02d' % (year, month, day, hour, minute, second)
 
    def write(self, msg):
       """ Write a string to the log """
+      if self.echo:
+         print msg,
       if self.open:
          self.file.write(msg)
 
    def writeln(self, msg):
       """ Write a line (with newline) to the log """
+      if self.echo:
+         print msg
       if self.open:
          self.file.write(msg + "\n")
 
