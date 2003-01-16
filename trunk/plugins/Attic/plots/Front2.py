8c5bb69c1524f24accde7415d3bd84da365ec827
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-01-16 05:53:19 +0000

    Removed buggy graphing programs

--- trunk/plugins/Attic/plots/Front2.py
+++
@@ -1,8 +0,0 @@
-from pyro.gui.plot import *
-
-def INIT(robot, brain):
-   sensorList = map(lambda(x): x[0], robot.sensorGroups['front'])
-   return GeneralPlot(robot, brain, sensorList,
-                      name='Front Plot...General',
-                      history = 300)
-                      
--- trunk/plugins/Attic/plots/Left2.py
+++
@@ -1,8 +0,0 @@
-from pyro.gui.plot import *
-
-def INIT(robot, brain):
-   sensorList = map(lambda(x): x[0], robot.sensorGroups['left'])
-   return GeneralPlot(robot, brain, sensorList,
-                      name='Left Plot...General',
-                      history = 300)
-                      
