728c5a18ad55639d11c61ae199dd206f904e380f
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-01-16 05:53:19 +0000

    Removed buggy graphing programs

--- plugins/Attic/plots/Right2.py
+++
@@ -1,8 +0,0 @@
-from pyro.gui.plot import *
-
-def INIT(robot, brain):
-   sensorList = map(lambda(x): x[0], robot.sensorGroups['right'])
-   return GeneralPlot(robot, brain, sensorList,
-                      name='Right Plot...General',
-                      history = 300)
-                      
