0f431601bd189f0e92d7fbdcfbe170d486c60a53
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-03-06 14:38:28 +0000

    Added interface for own images

--- trunk/camera/fake.py
+++ trunk/camera/fake.py
@@ -3,19 +3,31 @@ from pyro.camera import *
 class FakeCamera(Camera):
    """
    A Fake camera class. Simulates live vision. Call update() to get image.
+   file is a parameter that indicates a full or relative file template
+   starting with 0. Leave empty to use the system files.
    """
-   def __init__(self):
-      self.count = 0
+   def __init__(self, file = None, start = 0, incr = 1):
+      self.start = start
+      self.incr = incr
+      self.count = start
+      if file == None:
+         from os import getenv
+         pyrodir = getenv('PYRO')
+         if pyrodir == None:
+            print "WARNING: environment variable PYRO not defined"
+            print "info   : trying /usr/local/pyro..."
+            pyrodir = "/usr/local/pyro"
+         self.file = pyrodir + "/vision/snaps/som-%d.ppm"
+      else:
+         self.file = file 
       Camera.__init__(self, 0, 0) # will get info from file
 
    def _update(self):
-      from os import getenv
-      pyrodir = getenv('PYRO')
-      if not file_exists(pyrodir + "/vision/snaps/som-%d.ppm" % self.count):
-         self.count = 0
-      if not file_exists(pyrodir + "/vision/snaps/som-%d.ppm" % self.count):
+      if not file_exists(self.file % self.count):
+         self.count = self.start
+      if not file_exists(self.file % self.count):
          from sys import exit
-         print "Can't find $PYRO/vision/snaps/ images!"
-         sys.exit(1)
-      self.loadFromFile(pyrodir + "/vision/snaps/som-%d.ppm" % self.count)
-      self.count += 1
+         print "Can't find images:", self.file % self.count
+         exit(1)
+      self.loadFromFile(self.file % self.count)
+      self.count += self.incr
