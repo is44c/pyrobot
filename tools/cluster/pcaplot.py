cb1b0bd3aace307904b2c2733b986d2cdfb99e6d
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-03-28 04:57:39 +0000

    Added debug; added pcaplot.py

---
+++ tools/cluster/pcaplot.py
@@ -0,0 +1,57 @@
+import Gnuplot
+
+class FakeFile:
+    def __init__(self):
+        pass
+    def readline(self):
+        return ""
+    def close(self):
+        pass
+
+class PCAPlot:
+    def __init__(self, eigenfile, namefile = None, debug = 0,
+                 dimensions = 2, title = None, datatitle = None):
+        self.gp = Gnuplot.Gnuplot(debug = debug)
+        # read in eigenvalues, names
+        efp = open(eigenfile, "r")
+        if namefile:
+            nfp = open(namefile, "r")
+        else:
+            nfp = FakeFile()
+        eline = efp.readline()
+        nline = nfp.readline()
+        dataset = []
+        while eline:
+            eline = eline.strip()
+            label = nline.strip()
+            data = eline.split(" ")
+            if label:
+                self.gp('set label "%s" at %f,%f' %
+                        (label, float(data[0]), float(data[1])))
+            dataset.append( (float(data[0]), float(data[1])))
+            eline = efp.readline()
+            nline = nfp.readline()
+        efp.close()
+        nfp.close()
+        self.data = Gnuplot.Data(dataset)
+        self.gp('set data style points')
+        self.gp.title(title)
+        self.data.set_option(title = datatitle)
+
+    def plot(self):
+        self.gp.plot(self.data)
+
+    def replot(self):
+        self.gp.replot()
+
+    def hardcopy(self, output):
+        self.gp.hardcopy(output)
+
+if __name__ == '__main__':
+    pca = PCAPlot("data.pca", "names", title = "Sample PCA Plot")
+    pca.plot()
+    raw_input()
+    pca.data.set_option(title = "Data name")
+    pca.replot()
+    pca.hardcopy("/tmp/output.ps")
+    raw_input()
