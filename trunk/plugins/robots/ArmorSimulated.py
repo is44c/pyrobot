d936d6157e75b27cfc14bc4c299f4b8fd4c7a2f8
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-04-21 15:29:33 +0000

    Version 1.2.4: remove Pioneer/Saphira; fix Makefile for upgrading Khepera simulator; added -g sensitive Stage startup (to run in non-graphics mode)

--- trunk/plugins/robots/ArmorSimulated.py
+++
@@ -1,9 +0,0 @@
-import pyro.robot.armor as armor
-
-class ArmorSimulated(armor.ArmorRobot):
-   def __init__(self):
-      armor.ArmorRobot.__init__(self, "ArmorRobot", 1)
-
-def INIT():
-   return ArmorSimulated()
-
