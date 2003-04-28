c48c1d2ff5842ad1e0a9cd10985e7e9d59e85c93
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-04-28 15:46:49 +0000

    Test to see that update() was needed to get stall detected. Strange.

--- trunk/plugins/brains/SimpleBrain.py
+++ trunk/plugins/brains/SimpleBrain.py
@@ -6,7 +6,14 @@ class SimpleBrain(Brain):
    # Only method you have to define is the step method:
 
    def step(self):
-      self.getRobot().move(.3, .2)
+      robot = self.getRobot()
+      if robot.get('self','stall'):
+         print "stuck--reversing"
+         robot.move(-0.5, 0)
+         sleep(0.5)
+      else:
+         robot.move(0.5, 0)
+      robot.update()
       #self.quit()
 
 # -------------------------------------------------------
