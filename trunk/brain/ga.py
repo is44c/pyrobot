a12ff5ef1cbee690dc85072a6df9ab81eefdb43f
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-05-02 00:22:27 +0000

    Lisa's corrections

--- trunk/brain/ga.py
+++ trunk/brain/ga.py
@@ -1,23 +1,29 @@
-# ------------------------------------------------
+# --------------------------------------------------------------------------
 # A simple Genetic Algorithm in Python
 # (See bottom for examples, and tests)
-# ------------------------------------------------
+# --------------------------------------------------------------------------
 # (c) D.S. Blank, Bryn Mawr College 2003
-# updated 3/21/03 by Lisa Meeden
-# ------------------------------------------------
-
-# Assumptions:
-#   1. Default rates: mutation .05 %, crossover .3 %
-#   2. Maximizes the fitness function
+#
+# Updated 4/29/03 by Lisa Meeden
+# There are a few key differences in this version.
+#
+# 1. There is no longer a GA parameter selectionRate.  The next generation
+#    of individuals is created by selecting two parents at a time from the
+#    current population.  The crossoverRate determines how often those two
+#    parents will be recombined to produce two new children.  For example,
+#    if the crossoverRate is 0.6 that means that 60% of the time the parents
+#    will be crossed and 40% of the time they will simply be copied.  The
+#    mutationRate determines how often each gene is likely to be modified.
+#    For example, if the mutationRate is 0.1 that means that 10% of the
+#    individual genes will be modified.  So in a genotype of length 50, 
+#    approximately five of the genes will be mutated.
+#
+# 2. We are now using a roulette wheel method of selection.  Therefore,
+#    the fitness function must return a non-negative value.  
+# --------------------------------------------------------------------------
 
 import RandomArray, Numeric, math, random, time, sys
 from copy import deepcopy
-
-# Some useful local functions, and variables:
-
-RANDOM = 0
-GOOD = 1
-BAD = 2
 
 def display(v):
     print v,
@@ -28,21 +34,20 @@ def sum(a):
         sum += n
     return sum
 
-def avg(seq):
-    sum = 0.0
-    for i in range(len(seq)):
-        sum += seq[i]
-    return sum / len(seq)
+def flip(probability):
+    """
+    Flip a biased coin
+    """
+    return random.random() <= probability
 
 
 # The classes:
-#  Population - collection of Genes
 #  Gene - specifics of gene representation 
+#  Population - collection of Genes
 #  GA - the parameters of evolution
 
 class Gene:
     def __init__(self, **args):
-        # Possible Change: Initialization is made beween -1, and 1
         self.verbose = 0
         self.genotype = []
         self.fitness = 0.0
@@ -67,8 +72,7 @@ class Gene:
                 self.genotype.append( math.floor(random.random() *
                                                  (self.max - self.min + 1)) + self.min)
             elif self.mode == 'float':
-                self.genotype.append( random.random() *
-                                      (self.max - self.min + 1) + self.min)
+                self.genotype.append( (random.random() * (self.max - self.min)) + self.min)
             else:
                 raise "unknownMode", self.mode
 
@@ -78,25 +82,6 @@ class Gene:
     def __len__(self):
         return len(self.genotype)
 
-    def crossover(self, otherGene, uniform = 0, **args):
-        """
-        Takes another gene, produces a mixture, and returns it.
-        """
-        if (not uniform): # single point
-            crosspoint = int(random.random() * len(self.genotype))
-            if self.verbose == 2:
-                print "at position", crosspoint
-            temp = self.genotype[:crosspoint]
-            temp.extend(otherGene.genotype[crosspoint:])
-        else:
-            if self.verbose == 2:
-                print "uniformily"
-            temp = self.genotype[:] # copy self
-            for i in range(len(self.genotype)):
-                if random.random() < .5: # from other
-                    temp[i] = otherGene.genotype[i]
-        return temp
-
     def display(self):
         if self.mode == 'bit' or self.mode == 'integer': 
             map(lambda v: display(int(v)), self.genotype)
@@ -105,40 +90,12 @@ class Gene:
         else:
             raise "unknownMode", self.mode
 
-    def mutate(self, **args):
-        pos = int(random.random() * len(self.genotype))
-        if self.verbose == 2:
-            print "at position", pos
-        if self.mode == 'bit': 
-            self.genotype[pos] = not self.genotype[pos]
-        elif self.mode == 'integer': 
-            r = random.random()
-            if (r < .5):
-                self.genotype[pos] += 1
-            else:
-                self.genotype[pos] -= 1
-            #else:
-            #    self.genotype[pos] =  math.floor(random.random() * \
-            #                          (self.max - self.min + 1)) + self.min
-        elif self.mode == 'float': 
-            r = random.random()
-            if (r < .5):
-                self.genotype[pos] -= random.random()
-            else:
-                self.genotype[pos] += random.random()
-            #else:
-            #    self.genotype[pos] = ( random.random() * \
-            #                          (self.max - self.min + 1) + self.min)
-        else:
-            raise "unknownMode", self.mode
-        
-
 class Population:
-    """
-    Class to abstract gene-related items.
-    """
     def __init__(self, cnt, geneConstructor, **args):
+        self.sumFitness = 0   
+        self.avgFitness = 0   
         self.individuals = []
+        self.bestMember = -1
         self.size = cnt
         self.geneConstructor = geneConstructor
         self.args = args
@@ -146,7 +103,7 @@ class Population:
         if args.has_key('verbose'):
             self.verbose = args['verbose']
         for i in range(cnt):
-            self.individuals.append( geneConstructor(**args))
+            self.individuals.append(geneConstructor(**args))
 
     def __getitem__(self, val):
         return self.individuals[val]
@@ -154,70 +111,111 @@ class Population:
     def __len__(self):
         return len(self.individuals)
 
-    def mutate(self, rate, **args):
+    def select(self):
         """
-        If mutationRate = .1, then .1 of the pop will get mutated.
+        Select a single individual via the roulette wheel method.
+        Algorithm from Goldberg's book, page 63.  NOTE: fitness
+        function must return positive values to use this method
+        of selection.
         """
-        for i in range(int(rate * self.size)):
-            pos = self.pick(RANDOM)
-            if self.verbose  == 2:
-                print "Mutation in individual", pos,
-            self.individuals[pos].mutate(**args)
+        index = 0
+        partsum = 0.0
+        if self.sumFitness == 0:
+            raise "Population has a total of zero fitness"
+        spin = random.random() * self.sumFitness
+        while index < self.size-1:
+            fitness = self.individuals[index].fitness
+            if fitness < 0:
+                raise "Negative fitness in select", fitness
+            partsum += self.individuals[index].fitness
+            if partsum >= spin:
+                break
+            index += 1
+        if self.verbose > 2:
+            print "selected",
+            self.individuals[index].display(),
+            print "fitness", self.individuals[index].fitness
+        return self.individuals[index]
 
-    def selection(self, rate, **args):
+    def mutate(self, child, mode, mutationRate):
         """
-        If selectionRate = .2, then .2 of bad pop will be replaced with good.
+        Depending on the mutationRate, will mutate particular genes
+        in the child.
         """
-        for i in range(int(rate * self.size)):
-            replace = self.pick(BAD)
-            parent = self.pick(GOOD)
-            self.individuals[replace].genotype = deepcopy(self.individuals[parent].genotype)
-
-    def crossover(self, rate, **args):
+        for i in range(len(child)):
+            if flip(mutationRate):
+                if self.verbose > 2:
+                    print "mutating at position", i
+                if mode == 'bit': 
+                    child[i] = not child[i]
+                elif mode == 'integer': 
+                    r = random.random()
+                    if (r < .5):
+                        child[i] += 1
+                    else:
+                        child[i] -= 1
+                elif mode == 'float': 
+                    r = random.random()
+                    if (r < .5):
+                        child[i] -= random.random()
+                    else:
+                        child[i] += random.random()
+                else:
+                    raise "unknownMode", mode
+    
+    def crossover(self, parent1, parent2, crossoverRate):
         """
-        If crossoverRate = .3, then .3 of pop will be replaced.
+        Depending on the crossoverRate, will return two new children
+        created by crossing over the given parents at a single point,
+        or will return copies of the parents.
         """
-        for i in range(int(rate * self.size)):
-            replace = self.pick(BAD)
-            parent1 = self.pick(GOOD)
-            parent2 = self.pick(GOOD)
-            if self.verbose == 2:
-                print "Crossover with parent", parent1, "and", parent2,
-                print "replacing individuals", replace,
-            self.individuals[replace].genotype = \
-                self.individuals[parent1].crossover(self.individuals[parent2], **args)
-
-    def pick(self, type):
-	p = int(random.random() * len(self.individuals))
-        if (type == BAD):
-            if (random.random() > self.probability(p)):
-                return p
-            else:
-                return self.pick(type)
-        elif (type == GOOD):
-            if (random.random() < self.probability(p)):
-                return p
-            else:
-                return self.pick(type);
+        geneLength = len(parent1.genotype)
+        if flip(crossoverRate):
+            crosspt = (int)((random.random() * (geneLength - 2)) + 1)
+            if self.verbose > 2:
+                print "crossing over at point", crosspt
+            child1 = parent1.genotype[:crosspt]
+            child1.extend(parent2.genotype[crosspt:])
+            child2 = parent2.genotype[:crosspt]
+            child2.extend(parent1.genotype[crosspt:])
+            return child1, child2
         else:
-            return p
+            if self.verbose > 2:
+                print "no crossover"
+            return parent1.genotype[:], parent2.genotype[:]
 
-    def probability(self, pos):
-        return ((len(self.individuals) - pos) + 0.0) / len(self.individuals)
-            
+    def statistics(self):
+        """
+        Maintains important statistics about the current population.
+        It calculates total fitness, average fitness, best fitness,
+        and worst fitness.  Also stores the best individual in
+        the variable self.bestMember.
+        """
+        self.sumFitness = 0
+        best = self.individuals[0]
+        worst= self.individuals[0]
+        for i in range(self.size):
+            current = self.individuals[i]
+            self.sumFitness += current.fitness
+            if current.fitness < worst.fitness:
+                worst = current
+            if current.fitness > best.fitness:
+                best = current
+        self.bestMember = best
+        self.avgFitness = (self.sumFitness * 1.0) / self.size
+        if self.verbose > 0:
+            print "Fitness: Total", "%7.2f" % self.sumFitness, 
+            print "Best", "%5.2f" % best.fitness,
+            print "Worst", "%5.2f" % worst.fitness,
+            print "Average", "%5.2f" % self.avgFitness
+
 class GA:
     """
     Class which defines everything needed to run a GA.
     """
     def __init__(self, population, **args):
-        x = random.random() * 100000 + time.time()
-        self.setSeed(x)
-        self.pop = population
-        self.applyFitnessFunction()
-        self.sort()
-        self.mutationRate = 0.05
-        self.crossoverRate = 0.3
-        self.selectionRate = 0.25
+        self.mutationRate = 0.1
+        self.crossoverRate = 0.6
         self.maxGeneration = 0
         self.generation = 0
         self.verbose = 0
@@ -231,6 +229,16 @@ class GA:
             self.crossoverRate = args['crossoverRate']
         if args.has_key('maxGeneration'):
             self.maxGeneration = args['maxGeneration']
+        x = random.random() * 100000 + time.time()
+        self.setSeed(x)
+        self.pop = population
+        self.applyFitnessFunction()
+        if self.verbose > 0:
+            print "-" * 60
+            print "Initial population"
+        self.pop.statistics()
+        if self.verbose > 1:
+            self.display()
 
     def isDone(self):
         # Override this
@@ -250,32 +258,6 @@ class GA:
         random.seed(self.seed1)
         RandomArray.seed(int(self.seed1), int(self.seed2))
 
-    def swap(self, a, b):
-        self.pop.individuals[a], self.pop.individuals[b] = \
-            self.pop.individuals[b], self.pop.individuals[a]
-
-    def partition(self, first, last):
-        pivot = self.pop.individuals[(first+last)/2].fitness
-        while (first <= last):
-            while (self.pop.individuals[first].fitness > pivot):
-                first += 1
-            while (self.pop.individuals[last].fitness < pivot):
-                last -= 1
-            if (first <= last):
-                self.swap(first, last)
-                first += 1
-                last -= 1
-        return first
-
-    def quickSort(self, first, last):
-        if (first < last ):
-            piv_index = self.partition(first, last )
-            self.quickSort(first, piv_index - 1 )
-            self.quickSort(piv_index, last )
-
-    def sort(self):
-        self.quickSort(0, len(self.pop.individuals) - 1)
-
     def display_one(self, p):
         self.pop.individuals[p].display()
         print "Fitness:", self.pop.individuals[p].fitness
@@ -284,34 +266,52 @@ class GA:
         print "Population:"
         for p in range(len(self.pop.individuals)):
             self.display_one(p)
-            
+
+    def generate(self):
+        """
+        Iteratively creates a new population from the current population.
+        Selects two parents, attempts to cross them, and then attempts to
+        mutate the resulting children.  The probability of these operations
+        occurring is determined by the crossoverRate and the mutationRate.
+        Overwrites the old population with the new population.
+        """
+        mode = self.pop.individuals[0].mode
+        newpop = range(self.pop.size)
+        i = 0
+        while i < self.pop.size - 1:
+            parent1 = self.pop.select()
+            parent2 = self.pop.select()
+            newpop[i], newpop[i+1] = self.pop.crossover(parent1, parent2, self.crossoverRate)
+            self.pop.mutate(newpop[i], mode, self.mutationRate)
+            self.pop.mutate(newpop[i+1], mode, self.mutationRate)
+            i += 2
+        # For odd sized populations, need to create the last child
+        if self.pop.size % 2 == 1:
+            newpop[self.pop.size-1] = self.pop.select().genotype[:]
+            self.pop.mutate(newpop[self.pop.size-1], mode, self.mutationRate)
+        # Copy new generation into population
+        for i in range(self.pop.size):
+            self.pop.individuals[i].genotype = newpop[i][:]
+    
     def evolve(self):
         self.generation = 0
-        self.applyFitnessFunction()
-        self.sort()
-        while (self.generation < self.maxGeneration or self.maxGeneration == 0):
-            print "-" * 30
-            print "Generation #", self.generation, ":"
-            if self.verbose >= 1:
-                print "Best : (position # 0 )"
-                self.display_one(0)
-                print "Median: (position #", len(self.pop.individuals)/2, ")"
-                self.display_one(len(self.pop.individuals)/2)
-                print "Worst: (position #", len(self.pop.individuals) - 1, ")"
-                self.display_one(len(self.pop.individuals) - 1)
-            if self.verbose == 2:
+        while self.generation < self.maxGeneration or self.maxGeneration == 0:
+            self.generation += 1
+            if self.verbose > 0:
+                print "-" * 60
+                print "Generation", self.generation
+            self.generate()
+            self.applyFitnessFunction()
+            self.pop.statistics()
+            if self.verbose > 1:
                 self.display()
             if self.isDone():
-                print "Completed!"
-                return
-            self.pop.selection(self.selectionRate)
-            self.pop.crossover(self.crossoverRate)
-            self.pop.mutate(self.mutationRate)
-            self.applyFitnessFunction()
-            if self.verbose == 2:
-                self.display()
-            self.sort()
-            self.generation += 1
+                break
+        print "-" * 60
+        print "Done evolving at generation", self.generation
+        print "Current best individual"
+        self.pop.bestMember.display()
+        print "Fitness", self.pop.bestMember.fitness
 
 if __name__ == '__main__':
     # Here is a test to evolve a list of integers to maximize their sum:
@@ -320,15 +320,14 @@ if __name__ == '__main__':
         def fitnessFunction(self, i):
             return sum(self.pop.individuals[i].genotype)
         def isDone(self):
-            return self.fitnessFunction(0) > 30
+            return self.pop.bestMember.fitness > 30
 
     print "Do you want to evolve a list of integers to maximize their sum? ",
     if sys.stdin.readline().lower()[0] == 'y':
         print
-        ga = MaxSumGA(Population(50, Gene, size = 10, mode = 'integer'),
-                      mutationRate = .2, crossoverRate = .5,
-                      selectionRate = .25,
-                      maxGeneration = 50, verbose = 1)
+        ga = MaxSumGA(Population(15, Gene, size=10, mode='integer', verbose=1),
+                      mutationRate=0.1, crossoverRate=0.5, verbose=1,
+                      maxGeneration=50)
         ga.evolve()
     print 
 
@@ -356,14 +355,14 @@ if __name__ == '__main__':
             n.setTolerance(.4)
             g = n.arrayify()
             self.network = n
-            GA.__init__(self, Population( cnt, Gene, size = len(g)),
-                        mutationRate = 0.5, crossoverRate = 0.25,
-                        selectionRate = 0.25, maxGeneration = 400,
-                        verbose = 1, min = -2, max = 2)
+            GA.__init__(self,
+                        Population(cnt, Gene, size=len(g), verbose=1, min=-1, max=1),
+                        mutationRate=0.5, crossoverRate=0.25,
+                        maxGeneration=400, verbose=1)
         def fitnessFunction(self, genePos):
             self.network.unArrayify(self.pop.individuals[genePos].genotype)
             error, correct, count = self.network.sweep()
-            return -error
+            return 4 - error
         def isDone(self):
             self.network.unArrayify(self.pop.individuals[0].genotype)
             error, correct, count = self.network.sweep()
@@ -372,7 +371,7 @@ if __name__ == '__main__':
 
     print "Do you want to evolve a neural network that can do XOR? ",
     if sys.stdin.readline().lower()[0] == 'y':
-        ga = NNGA(300)
+        ga = NNGA(50)
         ga.evolve()
         ga.network.unArrayify(ga.pop.individuals[0].genotype)
         ga.network.setInteractive(1)
