3460a3f075fa1f8cdc7e94efb1e87ed1d3ec0c16
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-06-07 23:47:57 +0000

    Fixed update loop so it doesn't update unless it has to. Fixed loading of simulator

--- gui/__init__.py
+++ gui/__init__.py
@@ -231,6 +231,12 @@ class gui(Drawable):
       f = self.fileloaddialog("simulators","*")
       self.redraw()
       if f != '':
+         if worldfile == '':
+            if f[-5:] == 'Stage':
+               worldfile = self.fileloaddialog("worlds","*", "/usr/local/stage/worlds")
+            elif f[-4:] == 'Aria':
+               worldfile = self.fileloaddialog("worlds","*", "/usr/local/Aria/worlds")
+         self.engine.worldfile = worldfile
          import os
          os.system(f + " " + worldfile + " &")
          self.redraw()
