f6ef33dc7494a9d8841dcf5f40ddbd8db2441a13
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-06-09 02:15:54 +0000

    Moved Brain view to brain; planning to add map visualization tools

--- trunk/brain/behaviors/__init__.py
+++ trunk/brain/behaviors/__init__.py
@@ -114,6 +114,43 @@ class BehaviorBasedBrain (Brain):
       for c in self.controls.keys():
          # set that controller to act with a value
          self.controls[c](0)
+   def redrawPie(self, pie, percentSoFar, piececnt, controller,
+                 percent, name):
+      # FIX: behavior specific. How to put in Behavior-based code?
+      xoffset = 5
+      yoffset = 20
+      width = 100
+      row = (pie - 1) * (width * 1.5)
+      colors = ['blue', 'red', 'tan', 'yellow', 'orange', 'black', 'azure', 'beige', 'brown', 'coral', 'gold', 'ivory', 'moccasin', 'navy', 'salmon', 'tan', 'ivory']
+      self.canvas.create_text(xoffset + 60,row + 10, tags='pie',fill='black', text = controller) 
+      self.canvas.create_arc(xoffset + 10,row + yoffset,width + xoffset + 10,row + width + yoffset,start = percentSoFar * 360.0, extent = percent * 360.0 - .001, tags='pie',fill=colors[(piececnt - 1) % 17])
+      self.canvas.create_text(xoffset + 300,row + 10 + piececnt * 20, tags='pie',fill=colors[(piececnt - 1) % 17], text = name)
+
+   def redraw(self):
+      if len(self.pie) != 0:
+         self.canvas.delete('pie')
+         piecnt = 0
+         for control in self.controls:
+            piecnt += 1
+            percentSoFar = 0
+            piececnt = 0
+            for d in self.pie:
+               if control == d[0]:
+                  piececnt += 1
+                  portion = d[2]
+                  try:
+                     self.redrawPie(piecnt, percentSoFar, \
+                                    piececnt, \
+                                    "%s effects: %.2f" % \
+                                    (d[0], self.history[0][d[0]]),
+                                    portion, \
+                                    "(%.2f) %s IF %.2f THEN %.2f = %.2f" % \
+                                    (d[1], d[5], d[2], d[3], d[4]))
+                  except:
+                     pass
+                  percentSoFar += portion
+      else:
+         self.canvas.create_text(200,130, tags='pie',fill='black', text = "Ready...")
 
 class Behavior:
    """
@@ -215,3 +252,4 @@ class State:
 
    def getEngine(self):
       return self.behaviorEngine.getEngine()
+
