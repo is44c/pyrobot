2586fd32a9b94cbfeb5909201ba56a55d556a049
meeden <meeden@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-06-16 21:09:19 +0000

    Renamed x and y based variables to column and row based variables.

--- trunk/map/occupancyGrid.py
+++ trunk/map/occupancyGrid.py
@@ -1,6 +1,21 @@
 import Tkinter
 
 class occupancyGrid(Tkinter.Tk):
+   """
+   GUI for visualizing an occupancy grid style map.
+   
+   The mouse can be used to change occupancy values:
+   Press the left button for 1.0
+   Press the middle button for 0.5
+   Press the right button  for 0.0
+
+   Certain keys provide other funtionality:
+   Press 'S' to set the start cell of the path.
+   Press 'G' to set the goal cell of the path.
+   Press 'P' to plan the path.
+   Press '2' to double the size of the occupancy grid.
+   Press 'Q' to quit.
+   """
    def __init__(self, grid):
       Tkinter.Tk.__init__(self)
       self.title("Occupancy Grid")
@@ -8,14 +23,17 @@ class occupancyGrid(Tkinter.Tk):
       self.grid = grid
       self.lastMatrix = self.grid
       self.lastPath = None
-      self.xScale = 50.0
-      self.yScale = 50.0
+      self.colScale = 50.0
+      self.rowScale = 50.0
       self.start = (0, 0)
       self.goal = (6, 2)
       self.rows = len(grid)
       self.cols = len(grid[0])
-      self.width = self.cols * self.xScale
-      self.height = self.rows * self.yScale
+      self.width = self.cols * self.colScale
+      self.height = self.rows * self.rowScale
+      self.infinity = 1e5000
+      self.value= [[self.infinity for col in range(self.cols)]
+                   for row in range(self.rows)]
       self.canvas = Tkinter.Canvas(self,width=self.width,height=self.height)
       self.bind("<Configure>", self.changeSize)
       self.canvas.bind("<B1-Motion>", self.increaseCell)
@@ -30,36 +48,33 @@ class occupancyGrid(Tkinter.Tk):
       self.canvas.bind("<KeyPress-2>", self.setDouble)
       self.canvas.bind("<KeyPress-q>", self.close)
       self.canvas.pack()
-      self.infinity = 1e5000
-      self.value= [[self.infinity for col in range(self.cols)]
-                   for row in range(self.rows)]
-      self.protocol('WM_DELETE_WINDOW',self.close)
+      self.protocol('WM_DELETE_WINDOW', self.close)
       self.update_idletasks()
 
    def increaseCell(self, event):
-      y = event.x
-      x = event.y
-      self.grid[int(round(x / self.xScale))][int(round(y / self.yScale))] = 1.0
+      cellCol = int(round(event.x/self.colScale))
+      cellRow = int(round(event.y/self.rowScale))
+      self.grid[cellRow][cellCol] = 1.0
       self.redraw( self.grid, None)
 
    def middleCell(self, event):
-      y = event.x
-      x = event.y
-      self.grid[int(round(x / self.xScale))][int(round(y / self.yScale))] = 0.5
+      cellCol = int(round(event.x/self.colScale))
+      cellRow = int(round(event.y/self.rowScale))
+      self.grid[cellRow][cellCol] = 0.5
       self.redraw( self.grid, None)
 
    def decreaseCell(self, event):
-      y = event.x
-      x = event.y
-      self.grid[int(round(x / self.xScale))][int(round(y / self.yScale))] = 0.0
+      cellCol = int(round(event.x/self.colScale))
+      cellRow = int(round(event.y/self.rowScale))
+      self.grid[cellRow][cellCol] = 0.0
       self.redraw( self.grid, None)
 
    def changeSize(self, event):
       self.width = self.winfo_width() - 2
       self.height = self.winfo_height() - 2
       self.canvas.configure(width = self.width, height = self.height)
-      self.xScale = int(round(self.width / self.cols))
-      self.yScale = int(round(self.height / self.rows))
+      self.colScale = int(round(self.width / self.cols))
+      self.rowScale = int(round(self.height / self.rows))
       self.redraw( self.lastMatrix, self.lastPath)
 
    def color(self, value, maxvalue):
@@ -76,9 +91,9 @@ class occupancyGrid(Tkinter.Tk):
 
    def setDouble(self, event):
       self.rows *= 2
-      self.yScale = int(round(self.height / self.rows))
+      self.rowScale = int(round(self.height / self.rows))
       self.cols *= 2
-      self.xScale = int(round(self.width / self.cols))
+      self.colScale = int(round(self.width / self.cols))
       self.value= [[self.infinity for col in range(self.cols)]
                    for row in range(self.rows)]
       self.grid= [[0.0 for col in range(self.cols)]
@@ -86,18 +101,17 @@ class occupancyGrid(Tkinter.Tk):
       self.redraw(self.grid)
 
    def setGoal(self, event):
-      self.goal = int(round(event.x/self.xScale)), int(round(event.y/self.yScale))
+      self.goal = int(round(event.x/self.colScale)), int(round(event.y/self.rowScale))
       self.redraw(self.grid)
 
    def setStart(self, event):
-      self.start = int(round(event.x/self.xScale)), int(round(event.y/self.yScale))
+      self.start = int(round(event.x/self.colScale)), int(round(event.y/self.rowScale))
       self.redraw(self.grid)
 
    def findPath(self, event):
       path = self.planPath(self.start, self.goal, 50)
       if path:
          self.redraw(g.value, path)
-
 
    def redraw(self, matrix, path = None):
       self.lastMatrix = matrix
@@ -111,28 +125,28 @@ class occupancyGrid(Tkinter.Tk):
       self.canvas.delete("cell")
       for i in range(self.rows):
          for j in range(self.cols):
-            self.canvas.create_rectangle(j * self.xScale,
-                                         i * self.yScale,
-                                         (j + 1) * self.xScale,
-                                         (i + 1) * self.yScale,
+            self.canvas.create_rectangle(j * self.colScale,
+                                         i * self.rowScale,
+                                         (j + 1) * self.colScale,
+                                         (i + 1) * self.rowScale,
                                          width = 0,
                                          fill=self.color(matrix[i][j], maxval),
                                          tag = "cell")
             if path and path[i][j] == 1:
-               self.canvas.create_oval(j * self.xScale,
-                                       i * self.yScale,
-                                       (j + .25) * self.xScale,
-                                       (i + .25) * self.yScale,
+               self.canvas.create_oval(j * self.colScale,
+                                       i * self.rowScale,
+                                       (j + .25) * self.colScale,
+                                       (i + .25) * self.rowScale,
                                        width = 0,
                                        fill = "blue",
                                        tag = "cell")
 
-      self.canvas.create_text((self.start[0] + .5) * self.xScale,
-                              (self.start[1] + .5) * self.yScale,
+      self.canvas.create_text((self.start[0] + .5) * self.colScale,
+                              (self.start[1] + .5) * self.rowScale,
                               tag = 'cell',
                               text="Start", fill='green')
-      self.canvas.create_text((self.goal[0] + .5) * self.xScale,
-                              (self.goal[1] + .5) * self.yScale,
+      self.canvas.create_text((self.goal[0] + .5) * self.colScale,
+                              (self.goal[1] + .5) * self.rowScale,
                               tag = 'cell',
                               text="Goal", fill='green')
 
@@ -143,19 +157,23 @@ class occupancyGrid(Tkinter.Tk):
          print
       print "-------------------------------------------------"
 
-      # Made several changes to algorithm given by Thrun in the chapter
-      # "Map learning and high-speed navigation in Rhino" from the book
-      # "Artificial Intelligence and Mobile Robots" edited by Kortenkamp,
-      # Bonasso, and Murphy.
-      
-      # 1. When an occupancy probability is above some threshold, assume
-      # that the cell is occupied and set its value for search to
-      # infinity.
-      
-      # 2. When iterating over all cells to update the search values, add
-      # in the distance from the current cell to its neighbor.
-
    def planPath(self, start, goal, iterations):
+      """
+      Path planning algorithm is based on one given by Thrun in the
+      chapter 'Map learning and high-speed navigation in Rhino' from
+      the book 'Artificial Intelligence and Mobile Robots' edited by
+      Kortenkamp, Bonasso, and Murphy.
+
+      Made two key changes to the algorithm given.
+      1. When an occupancy probability is above some threshold, assume
+         that the cell is occupied and set its value for search to
+         infinity.
+      2. When iterating over all cells to update the search values, add
+         in the distance from the current cell to its neighbor.  Cels
+         which are horizontal or vertical from the current cell are
+         considered to be a distance of 1, while cells which are diagonal
+         from the current cell are considered to be a distance of 1.41.
+      """
       startCol, startRow = start
       goalCol, goalRow = goal
       self.value= [[self.infinity for col in range(self.cols)] for row in range(self.rows)]
@@ -177,19 +195,18 @@ class occupancyGrid(Tkinter.Tk):
                               d = 1
                            adj = self.value[row+i][col+j] + self.grid[row+i][col+j] + d
                            self.value[row][col] = min(self.value[row][col], adj)
-      #self.printMatrix(self.value)
       return self.getPath(startRow, startCol)
 
    def getPath(self, startRow, startCol):
       path = [[0 for col in range(self.cols)] for row in range(self.rows)]
       row = startRow
       col = startCol
-      nextRow = -1
-      nextCol = -1
       steps = 0
       while not (self.value[row][col] == 0.0):
          path[row][col] = 1
          min = self.infinity
+         nextRow = -1
+         nextCol = -1
          for i in [-1,0,1]:
             for j in [-1,0,1]:
                if not (i == 0 and j == 0) and self.inRange(row+i, col+j):
@@ -197,15 +214,14 @@ class occupancyGrid(Tkinter.Tk):
                      min = self.value[row+i][col+j]
                      nextRow = row+i
                      nextCol = col+j
-                     steps += 1
          if nextRow == -1:
             print "No such path!"
             self.redraw(self.value)
             return None
+         steps += 1
          row = nextRow
          col = nextCol
       path[row][col] = 1
-      #self.printMatrix(path)
       print "Path is %d steps" % steps
       return path
 
@@ -217,9 +233,9 @@ if __name__ == '__main__':
    map = [[0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
           [0.5, 0.5, 0.5, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0, 0.5],
           [1.0, 1.0, 0.5, 0.0, 0.5, 1.0, 0.5, 0.5, 1.0, 0.5],
-          [1.0, 1.0, 0.5, 0.0, 0.5, 1.0, 0.5, 0.5, 1.5, 0.5],
-          [0.5, 0.5, 0.5, 0.0, 0.5, 0.7, 0.5, 0.0, 1.0, 0.0],
-          [0.0, 0.0, 0.0, 0.0, 0.5, 1.5, 0.5, 0.0, 1.0, 0.0],
+          [1.0, 1.0, 0.5, 0.0, 0.5, 1.0, 0.5, 0.5, 0.5, 0.5],
+          [0.5, 0.5, 0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 1.0, 0.5],
+          [0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 1.0, 0.5],
           ]
    g = occupancyGrid(map)
    # Find a path from position 0,0 to a point on the other side of
@@ -227,5 +243,3 @@ if __name__ == '__main__':
    g.redraw(map)
    g.canvas.focus_set()
    g.mainloop()
-   #raw_input("Press ENTER to Plan Path\n")
-   #raw_input("Press ENTER when done\n")
