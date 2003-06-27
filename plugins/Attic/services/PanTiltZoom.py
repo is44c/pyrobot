bed895fcfd7d8065a0ed3dded5dc3f8e786934f6
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-06-27 05:35:38 +0000

    New services format

---
+++ plugins/Attic/services/PanTiltZoom.py
@@ -0,0 +1,4 @@
+""" This device signals the robot to load its PTZ """
+
+def INIT(robot):
+    return ["ptz"]
---
+++ plugins/Attic/services/Truth.py
@@ -0,0 +1,4 @@
+""" This device signals the robot to load its truth interface (god) """
+
+def INIT(robot):
+    return ["truth"]
