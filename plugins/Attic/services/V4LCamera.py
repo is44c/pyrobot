6283df1a263c3dc5882dd544c38bdf269274358a
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-06-30 19:16:33 +0000

    Fixed TTY GUI bug; made v4l auto select the channel based on robot type

--- plugins/Attic/services/V4LCamera.py
+++ plugins/Attic/services/V4LCamera.py
@@ -1,4 +1,14 @@
+""" A simple loader for a Video for Linux (V4L) frame grabber """
+
 from pyro.camera.v4l import *
 
 def INIT(robot):
-    return {"V4LCamera" : V4LGrabber(384, 240)}
+    if robot.name == "khepera":
+        # For framegrabbers:
+        # Channel -  0: television; 1: composite; 2: S-Video
+        ch = 1 # channel
+    else:
+        # Pioneers. You may have to set channel by hand to one that works
+        ch = 0 # channel
+    return {"V4LCamera" : V4LGrabber( 160, 120, channel = ch)}
+    #384, 240, channel = ch)}
