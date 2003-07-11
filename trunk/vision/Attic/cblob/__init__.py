45b8d58c6604b441de1734270fab0a2069374421
emoses <emoses@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-07-11 16:28:52 +0000

    Wrappers for the blob module, mostly to handle memory managment and provide a consisdent interface.  Currently only the CBitmap class is defined.

---
+++ trunk/vision/Attic/cblob/__init__.py
@@ -0,0 +1,30 @@
+import blob
+from pyro.vision import *
+
+class CBitmap:
+"""
+Wrapper with constructor and destructor for blob.bitmap struct
+"""
+   def __init__(self, width, height):
+      self.bitmap = blob.bitmap()
+      blob.Bitmap_init(self.bitmap, width, height)
+
+   def __del__(self):
+      blob.Bitmap_del(self.bitmap)
+
+   def set(self, x, y, data):
+   """
+   data must be able to be cast to an unsigned char (single byte)
+   """
+      blob.Bitmap_set(self.bitmap, x, y, data)
+
+   def get(self, x, y):
+      return blob.Bitmap_get(self.bitmap, x, y)
+   
+   def width(self):
+      return self.bitmap.width
+
+   def height(self):
+      return self.bitmap.height
+   
+   
