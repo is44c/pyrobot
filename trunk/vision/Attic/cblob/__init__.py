0d67119a3c28b547e5b10bb87c5486484a65f7fb
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-07-14 03:47:35 +0000

    Edited indets to make Python happy.

--- trunk/vision/Attic/cblob/__init__.py
+++ trunk/vision/Attic/cblob/__init__.py
@@ -2,29 +2,29 @@ import blob
 from pyro.vision import *
 
 class CBitmap:
-"""
-Wrapper with constructor and destructor for blob.bitmap struct
-"""
-   def __init__(self, width, height):
-      self.bitmap = blob.bitmap()
-      blob.Bitmap_init(self.bitmap, width, height)
+    """
+    Wrapper with constructor and destructor for blob.bitmap struct
+    """
+    def __init__(self, width, height):
+        self.bitmap = blob.bitmap()
+        blob.Bitmap_init(self.bitmap, width, height)
 
-   def __del__(self):
-      blob.Bitmap_del(self.bitmap)
+    def __del__(self):
+        blob.Bitmap_del(self.bitmap)
 
-   def set(self, x, y, data):
-   """
-   data must be able to be cast to an unsigned char (single byte)
-   """
-      blob.Bitmap_set(self.bitmap, x, y, data)
+    def set(self, x, y, data):
+        """
+        data must be able to be cast to an unsigned char (single byte)
+        """
+        blob.Bitmap_set(self.bitmap, x, y, data)
 
-   def get(self, x, y):
-      return blob.Bitmap_get(self.bitmap, x, y)
+    def get(self, x, y):
+        return blob.Bitmap_get(self.bitmap, x, y)
    
-   def width(self):
-      return self.bitmap.width
+    def width(self):
+        return self.bitmap.width
 
-   def height(self):
-      return self.bitmap.height
+    def height(self):
+        return self.bitmap.height
    
    
