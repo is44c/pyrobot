c0fdf4f8fdbcc167a3de42311aae5cd64d0bea17
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-08-07 02:43:44 +0000

    Made joystick gui just save requested movement; brain will get trans/rotate from there. This way, Pyro GUI still runs correctly.

--- tools/joystick.py
+++ tools/joystick.py
@@ -2,46 +2,52 @@ from Tkinter import *
 
 class Joystick:
 
-   def __init__(self, robot):
+   def __init__(self):
       self.app = Tk()
       self.app.wm_state('withdrawn')
       self.win = Toplevel()
       self.win.wm_title('Joystick')
 
-      self.robot = robot
-      self.canvas = Canvas(self.win,
+      self.frame = Frame(self.win)
+      self.canvas = Canvas(self.frame,
                            width = 360,
                            height = 220,
                            bg = 'white')
-
+      
       self.canvas.bind("<ButtonRelease-1>", self.canvas_clicked_up)
       self.canvas.bind("<Button-1>", self.canvas_clicked_down)
       self.canvas.bind("<B1-Motion>", self.canvas_moved)
-      self.canvas.pack(side=TOP)
+      self.canvas.pack(side=BOTTOM)
 
       self.circle_dim = (150, 10, 350, 210)
       self.circle = self.canvas.create_oval(self.circle_dim, fill = 'white')
       self.canvas.create_oval(245, 105, 255, 115, fill='black')
 
-      self.panicButton = Button(self.win, text="STOP!!", command=self.stop)
-      self.win.pack(side = LEFT)
-      self.app.mainloop()
+      self.panicButton = Button(self.frame, text="STOP!!", command=self.stop)
+      self.panicButton.pack(side = TOP)
+      self.frame.pack()
+      self.translate = 0
+      self.rotate = 0
+
+   def move(self, translate, rotate):
+      self.translate = translate
+      self.rotate = rotate
 
    def canvas_clicked_up(self, event):
-      self.robot.move(0, 0)
+      self.move(0, 0)
 
    def canvas_clicked_down(self, event):
       if self.in_circle(event.x, event.y):
          trans, rotate = self.calc_tr(event.x, event.y)
-         self.robot.move(trans, rotate)
+         self.move(trans, rotate)
 
    def canvas_moved(self, event):
       if self.in_circle(event.x, event.y):
          trans, rotate = self.calc_tr(event.x, event.y)
-         self.robot.move(trans, rotate)
+         self.move(trans, rotate)
 
-   def stop(self, event):
-      self.robot.move(0, 0)
+   def stop(self):
+      self.move(0, 0)
 
    def in_circle(self, x, y):
       r2 = ((self.circle_dim[2] - self.circle_dim[0])/2)**2
