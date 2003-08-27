e288904c3f0babbe4c79749928807add0e3948e3
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-08-27 13:41:53 +0000

    Changes to replicate Ioana's goal/path-planning work

---
+++ trunk/Attic/experiments/ffprepare.py
@@ -0,0 +1,56 @@
+from pyro.brain.psom import vis
+import pyro.brain.psom as psom
+
+inputfile = open("ffinputs.dat", "w")
+targetfile = open("fftargets.dat", "w")
+
+camera = open("camera.dat", "r")
+sonar = open("sonar.dat", "r")
+motors = open("motors.dat", "r")
+
+# to get rid of vector length
+sonar.readline()
+camera.readline()
+
+somSonar  = vis.VisPsom(file = "sonar-20passes-20x15.cod")
+somCamera = vis.VisPsom(file = "camera-20passes-20x15.cod")
+
+def saveListToFile(ls,file):
+    for i in range(len(ls)):
+        file.write("%.8f"%ls[i]+" ")
+    file.write("\n")
+
+def makeVector(pos, len):
+   retval = [0.0] * len
+   retval[pos] = 1.0
+   return retval
+   
+sonarData  = map( float, sonar.readline().split() )
+cameraData = map( float, camera.readline().split() )
+modelSonar  = somSonar.map(psom.vector( sonarData ))
+modelCamera = somCamera.map(psom.vector(cameraData ))
+
+goalVector = makeVector(modelSonar.point.x, 20) + \
+             makeVector(modelSonar.point.y, 15) + \
+             makeVector(modelCamera.point.x, 20) + \
+             makeVector(modelCamera.point.y, 15)
+
+for i in xrange(4999):
+   currVector = goalVector[:]
+   currMotor  = map( float, motors.readline().split() )
+
+   sonarData  = map( float, sonar.readline().split() )
+   cameraData = map( float, camera.readline().split() )
+   modelSonar  = somSonar.map(psom.vector( sonarData ))
+   modelCamera = somCamera.map(psom.vector(cameraData ))
+   
+   goalVector = makeVector(modelSonar.point.x, 20) + \
+                makeVector(modelSonar.point.y, 15) + \
+                makeVector(modelCamera.point.x, 20) + \
+                makeVector(modelCamera.point.y, 15)
+
+   saveListToFile(currVector + goalVector, inputfile)
+   translate = (5 * (currMotor[0] - .5) + .5)
+   saveListToFile([translate, currMotor[1] ], targetfile)
+   assert( 0.0 <= translate <= 1.0)
+
--- trunk/Attic/experiments/train.py
+++ trunk/Attic/experiments/train.py
@@ -6,12 +6,13 @@ Names of the file need to be set
 from pyro.brain import psom
 from pyro.brain.psom import vis
 from pyro.brain.psom import *
+from pyro.gui.plot.hinton import *
 
-mydataset = dataset(file = "collect.dat")
-mysom = vis.VisPsom(20,15,data = mydataset)
+mydataset = dataset(file = "camera.dat")
+mysom = vis.VisPsom(20,15,data = mydataset, vis_vectortype = 'Matrix', opts=(60,15))
 
-mysom.init_training(0.02, 5, 5000000)
+mysom.init_training(0.02, 5, 100000)
 mysom.train_from_dataset(mydataset)
-mysom.save_to_file("som20x15.cod")
+mysom.save_to_file("camera-20passes-20x15.cod")
 print "Done!!!!"
 mysom.win.mainloop()
