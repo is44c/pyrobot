8f6547475a1cfcc3195d0f37860a23bc53c4e512
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-08-29 12:23:55 +0000

    Updates

--- Attic/experiments/ffprepare.py
+++ Attic/experiments/ffprepare.py
@@ -1,6 +1,7 @@
-from pyro.brain.psom import vis
 import pyro.brain.psom as psom
 
+dataPoints = 5000
+measure = 10
 inputfile = open("ffinputs.dat", "w")
 targetfile = open("fftargets.dat", "w")
 
@@ -12,8 +13,8 @@ motors = open("motors.dat", "r")
 sonar.readline()
 camera.readline()
 
-somSonar  = vis.VisPsom(file = "sonar-20passes-20x15.cod")
-somCamera = vis.VisPsom(file = "camera-20passes-20x15.cod")
+somSonar  = psom.psom(file = "sonar-200passes-20x15.cod")
+somCamera = psom.psom(file = "camera-200passes-20x15.cod")
 
 def saveListToFile(ls,file):
     for i in range(len(ls)):
@@ -24,33 +25,25 @@ def makeVector(pos, len):
    retval = [0.0] * len
    retval[pos] = 1.0
    return retval
-   
-sonarData  = map( float, sonar.readline().split() )
-cameraData = map( float, camera.readline().split() )
-modelSonar  = somSonar.map(psom.vector( sonarData ))
-modelCamera = somCamera.map(psom.vector(cameraData ))
 
-goalVector = makeVector(modelSonar.point.x, 20) + \
-             makeVector(modelSonar.point.y, 15) + \
-             makeVector(modelCamera.point.x, 20) + \
-             makeVector(modelCamera.point.y, 15)
-
-for i in xrange(4999):
-   currVector = goalVector[:]
-   currMotor  = map( float, motors.readline().split() )
-
-   sonarData  = map( float, sonar.readline().split() )
-   cameraData = map( float, camera.readline().split() )
-   modelSonar  = somSonar.map(psom.vector( sonarData ))
-   modelCamera = somCamera.map(psom.vector(cameraData ))
-   
-   goalVector = makeVector(modelSonar.point.x, 20) + \
-                makeVector(modelSonar.point.y, 15) + \
-                makeVector(modelCamera.point.x, 20) + \
-                makeVector(modelCamera.point.y, 15)
-
-   saveListToFile(currVector + goalVector, inputfile)
-   exaggeratedTrans = (5 * (currMotor[0] - .5) + .5)
-   saveListToFile([exaggeratedTrans, currMotor[1] ], targetfile)
-   assert( 0.0 <= translate <= 1.0)
-
+motorVector = [0] * measure
+sensoryVector = [0] * measure
+for i in xrange(dataPoints / measure):
+    for m in range(measure):
+        sonarData  = map( float, sonar.readline().split() )
+        cameraData = map( float, camera.readline().split() )
+        modelSonar  = somSonar.map(psom.vector( sonarData ))
+        modelCamera = somCamera.map(psom.vector(cameraData ))
+        motorVector[m]  = map( float, motors.readline().split() )
+        sensoryVector[m] = makeVector(modelSonar.point.x, 20) + \
+                           makeVector(modelSonar.point.y, 15) + \
+                           makeVector(modelCamera.point.x, 20) + \
+                           makeVector(modelCamera.point.y, 15)
+    goalVector = sensoryVector[measure - 1][35:]
+    for m in range(measure - 1):
+        saveListToFile(sensoryVector[m] + goalVector, inputfile)
+        exaggeratedTrans = (5 * (motorVector[m][0] - .5) + .5)
+        saveListToFile([exaggeratedTrans, motorVector[m][1] ], targetfile)
+        assert( 0.0 <= exaggeratedTrans <= 1.0)
+    saveListToFile(sensoryVector[measure - 1] + goalVector, inputfile)
+    saveListToFile([0, 0], targetfile)
