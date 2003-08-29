92418f98dbf15cd82fef980ff8993f1fdfb0c6ba
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-08-29 19:17:42 +0000

    removed visualization bits

--- trunk/Attic/experiments/somtrain.py
+++ trunk/Attic/experiments/somtrain.py
@@ -9,14 +9,14 @@ from pyro.brain.psom import *
 from pyro.gui.plot.hinton import *
 
 cameraDataSet = dataset(file = "camera.dat")
-cameraSom = psom(20,15,data = cameraDataSet, vis_vectortype = 'Matrix', opts=(60,15))
+cameraSom = psom(20,15,data = cameraDataSet)
 cameraSom.init_training(0.2, 5, 1000000)
 cameraSom.train_from_dataset(cameraDataSet)
 cameraSom.save_to_file("camera-200passes-20x15.cod")
 print "Camera Done!!!!"
 
 sonarDataSet = dataset(file = "sonar.dat")
-sonarSom = psom(20,15,data = sonarDataSet, vis_vectortype = 'Matrix', opts=(60,15))
+sonarSom = psom(20,15,data = sonarDataSet)
 sonarSom.init_training(0.2, 5, 1000000)
 sonarSom.train_from_dataset(sonarDataSet)
 sonarSom.save_to_file("sonar-200passes-20x15.cod")
