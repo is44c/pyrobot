4d683fc8968031d95cc7d80b0a1564d20da89ad5
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-10-11 00:36:39 +0000

    Stereo cameras for B21R

---
+++ trunk/plugins/Attic/services/Stereo.py
@@ -0,0 +1,17 @@
+""" A simple loader for a Video for Linux (V4L) frame grabber """
+
+from pyro.camera.v4l import *
+
+def INIT(robot):
+    if robot.get("self", "name") == "Aria":
+        # Pioneers. You may have to set channel by hand to one that works
+        ch = 0 # channel
+    else:
+        # For framegrabbers:
+        # Channel -  0: television; 1: composite; 2: S-Video
+        ch = 1 # channel
+    return {"video0" : V4LGrabber( 160, 120, device = "/dev/video0",
+                                  channel = ch),
+	    "video1" : V4LGrabber( 160, 120, device = "/dev/video1",
+                                  channel = ch)}	
+    #384, 240, channel = ch)}
