120ad7f2f1c1b29afc7af26aa7f4648dd814f08c
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-10-12 06:30:32 +0000

    New vision organization

--- plugins/brains/SimpleBrain.py
+++ plugins/brains/SimpleBrain.py
@@ -7,19 +7,18 @@ class SimpleBrain(Brain):
 
    def setup(self, **args):
       print "Loading arg: '%s'" % args.get('my_arg')
+      self.camera = self.getRobot().startService("FakeCamera")[0]
       # initialize your vars here!
       
    # Only method you have to define is the step method:
 
    def step(self):
-      robot = self.getRobot()
-      if robot.get('self','stall'):
-         print "stuck--reversing"
-         robot.move(-0.5, 0)
-         sleep(0.5)
-      else:
-         robot.move(0.5, 0)
-      #self.quit()
+      self.camera.sleepTime=2.0  #sleep for 2 seconds after filters 
+      self.camera.sleepFlag = 1  #turn sleep flag on 
+      self.camera.lockCamera = 1 #lock the buffer 
+      self.camera.superColor("red",0) 
+      self.camera.maxBlobs(0, 1, 255, "mass") 
+      self.camera.lockCamera = 0 #unlock the buffer 
 
 # -------------------------------------------------------
 # This is the interface for calling from the gui engine.
