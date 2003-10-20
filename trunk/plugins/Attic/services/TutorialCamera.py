c9d30268089c2d6d8acefa137ede81419b6f9cde
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-10-20 00:41:49 +0000

    Hardcoded path, for now

--- trunk/plugins/Attic/services/TutorialCamera.py
+++ trunk/plugins/Attic/services/TutorialCamera.py
@@ -2,4 +2,4 @@ from pyro.camera.fake import FakeCamera
 from pyro.vision.cvision import VisionSystem
 
 def INIT(robot):
-    return {"FakeCamera": FakeCamera(pattern = "vision/tutorial/test-?.ppm", limit = 10, interval = 3, visionSystem = VisionSystem())}
+    return {"FakeCamera": FakeCamera(pattern = "/usr/local/pyro/vision/tutorial/test-?.ppm", limit = 10, interval = 3, visionSystem = VisionSystem())}
