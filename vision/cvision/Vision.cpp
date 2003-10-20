ca3207b5fff731ceeb2a934a736bffe7ebfc4d7b
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-10-20 21:05:43 +0000

    Fixed bug in reading PPM files (\n in scanf eats following whitespace, like \t); added medianBlur and scale. Last ones to do: histogram and train.

--- vision/cvision/Vision.cpp
+++ vision/cvision/Vision.cpp
@@ -24,14 +24,24 @@ PyObject *Vision::registerCameraDevice(void *dev) {
 		    device->getRGB()[0],device->getRGB()[1],device->getRGB()[2]);
 }
 
-PyObject *Vision::initialize(int wi, int he, int de, int r, int g, int b) {
-  width = wi;
-  height = he;
-  depth = de;
+PyObject *Vision::getRGB() {
+  return Py_BuildValue("iii", rgb[0], rgb[1], rgb[2] );
+}
+
+
+PyObject *Vision::setRGB(int r, int g, int b) {
   int rgb_order[MAXDEPTH] = {r, g, b};
   for (int d = 0; d < depth; d++)
     // set offsets for RGB
     rgb[d] = rgb_order[d];
+  return getRGB();
+}
+
+PyObject *Vision::initialize(int wi, int he, int de, int r, int g, int b) {
+  width = wi;
+  height = he;
+  depth = de;
+  setRGB(r, g, b);
   //workspace = new unsigned char [width * height * depth];
   //original = new unsigned char [width * height * depth];
   //memset(original, 0, width * height * depth);
@@ -100,6 +110,19 @@ PyObject *Vision::setPlane(int d, int value) {
   return Py_BuildValue("i", value);
 }
 
+PyObject *Vision::scale(float r, float g, float b) {
+  for (int w=0; w<width; w++) {
+    for (int h=0; h<height; h++) {
+      for (int d=0; d<depth; d++) {
+	Image[(h * width + w) * depth + rgb[0]] = (int)(MIN(MAX(r * Image[(h * width + w) * depth + rgb[0]],0),255));
+	Image[(h * width + w) * depth + rgb[1]] = (int)(MIN(MAX(g * Image[(h * width + w) * depth + rgb[1]],0),255));
+	Image[(h * width + w) * depth + rgb[2]] = (int)(MIN(MAX(b * Image[(h * width + w) * depth + rgb[2]],0),255));
+      }
+    }
+  }
+  return PyInt_FromLong(0L);
+}
+
 PyObject *Vision::getMMap() {
   PyObject *buffer;
   buffer = PyBuffer_FromMemory(Image, 
@@ -150,10 +173,10 @@ PyObject *Vision::superColor(float w1, float w2, float w3,
 // match() - match pixels by tolerance
 
 PyObject *Vision::match(int r, int g, int b, int tolerance, 
-			int outChannel, int mode) {
+			int outChannel) {
   return matchRange( r - tolerance, g - tolerance, b - tolerance,
 		     r + tolerance, g + tolerance, b + tolerance,
-		     outChannel, mode);
+		     outChannel);
 }
 
 // match() - match pixels by range
@@ -162,7 +185,7 @@ PyObject *Vision::match(int r, int g, int b, int tolerance,
 
 PyObject *Vision::matchRange(int lr, int lg, int lb, 
 			     int hr, int hg, int hb,
-			     int outChannel, int mode ) {
+			     int outChannel) {
   int matches;
   for(int h=0;h<height;h++) {
     for(int w=0;w<width;w++) {
@@ -253,49 +276,6 @@ PyObject *Vision::colorHistogram() {
 
 PyObject *Vision::trainColor() {
   return Py_BuildValue("");
-}
-
-PyObject *Vision::gaussianBlur() {
-  int offset,m1,m2;
-  int x,y,temp;
-  unsigned char *out;
-  
-  /******************************
-    here it says Image[(x-1)+(y+1)*width] 
-    if it is changed to Image[(x-depth+offset)+(y+depth)*width]
-    it should work for rgb (or bgr as the case may be)
-    -----------------------------------------------------
-     this means we will probably need another for loop for
-     offset, as we probably want to blur for all channels
-     ---------------------------------------------------
-       Get each of the surrounding pixels weighted value
-       top, left, right, and bottom weighing 2x
-       diagonals weighing 1x
-       pixel itself weighing 4x
-       and take the average
-       --------------------------------------------
-       I think *out is just the output buffer, which
-       in this case we want to be *Image, so I think all
-       those parts can be omitted
-  ******************************************/
-
-  for (y=1;y<height-1;y++)
-    for (x=1;x<width-1;x++)
-      for(offset=0;offset<depth;offset++)
-      {
-	temp=Image[(x-depth+offset+y*width)*depth+offset];
-	temp+=2*Image[(x-depth+y*width)*depth+offset];
-	temp+=Image[(x-depth+(y+1)*width)*depth+offset];
-	temp+=2*Image[(x+(y-1)*width)*depth+offset];
-	temp+=4*Image[(x+y*width)*depth+offset];
-	temp+=2*Image[(x+(y+1)*width)*depth+offset];
-	temp+=Image[(x+depth+offset+(y-1)*width)*depth+offset];
-	temp+=2*Image[(x+depth+offset+y*width)*depth+offset];
-	temp+=Image[(x+depth+offset+(y+1)*width)*depth+offset];
-	temp/=16;
-	Image[(x+offset+y*width)*depth+offset] = temp;
-      }
-  return PyInt_FromLong(0L);
 }
 
 PyObject *Vision::grayScale() {
@@ -410,7 +390,85 @@ PyObject *Vision::sobel(int thresh) {
 }
 
 PyObject *Vision::medianBlur(int kernel) {
+  int mid;
+  int w,h,i,j,x, moveVal, offset, temp=0;
+  int median[4][400] = {{-1}};  /*enough for 20x20(huge) Kernel */
+  int intensity = 3;
+  unsigned char *ptr, *outptr, *imagePtr;
+  static unsigned char *out = new unsigned char[width * height * depth];
+
+  if(kernel <= 0)
+    kernel = 1;
+  else if(kernel % 2 == 0) 
+    kernel--;
+  
+  x=(int)kernel/2;
+  
+  moveVal = x*width+x;
+
+  imagePtr = Image+(moveVal)*3;
+  outptr = out+(moveVal)*3;
+  
+  for (j=0;j<height*width*depth;j++)
+    out[j]=0;
+
+  offset = 0;
+
+  for(h=x;h<height-x;h++)
+    {
+      for(w=x;w<width-x;w++,imagePtr+=3,outptr+=3)
+	{
+	  ptr=imagePtr-(moveVal)*3;
+	  temp=0;
+	 
+	  /* find middle color of surrouding pixels */
+	  for(i=0;i<kernel;i++)
+	    {
+	      for(j=0;j<kernel;j++,ptr+=3,temp++)
+		{
+		  median[BLUE][temp]  = *(ptr);
+		  median[GREEN][temp] = *(ptr+1);
+		  median[RED][temp]  = *(ptr+2);
+		  median[intensity][temp]=(int) (0.3*(*(ptr+2)) + 0.59*(*(ptr+1)) + 0.11*(*(ptr)));
+		}
+	      /* bring ptr to next row */
+	      ptr=ptr-(kernel*3);
+	      ptr=ptr+width*3; /* bring ptr to next row */
+	    }
+	  
+	  /* get median intensity index */
+	  mid=getMiddleIndex(median, kernel);
+
+	  *outptr = median[BLUE][mid];
+	  *(outptr+1) = median[GREEN][mid];
+	  *(outptr+2) = median[RED][mid];
+
+	}
+    }
+
+  imagePtr = Image;
+  outptr = out;
+  for (j=0;j<height;j++)
+    for (i=0;i<width;i++)
+      for(offset=0;offset<depth;offset++)
+	Image[(i+j*width)*depth+offset] = outptr[(i+j*width)*depth+offset] ;
+
   return PyInt_FromLong(0L);
+}
+
+int Vision::getMiddleIndex(int median[4][400], int kernel) {
+  int i,j;
+  int rankTable[400];
+  
+  for(i=0; i<kernel*kernel; i++)
+    rankTable[i] = i;
+  
+  for(i=0; i < kernel*kernel; i++)
+    for(j=i+1; j< kernel*kernel; j++)
+      if(median[3][rankTable[i]] > median[3][rankTable[j]])
+	SWAP(rankTable[i],rankTable[j]);
+  
+  return(rankTable[(int)((kernel*kernel)/2)]);
 }
 
 PyObject *Vision::meanBlur(int kernel) {
@@ -881,6 +939,55 @@ PyObject *Vision::addFilter(PyObject *newFilter) {
   return Py_BuildValue("");
 }
 
+PyObject *Vision::gaussianBlur() {
+  int offset,m1,m2;
+  int x,y,temp;
+
+  /******************************
+    here it says image[(x-1)+(y+1)*width] 
+    if it is changed to image[(x-depth+offset)+(y+depth)*width]
+    it should work for rgb (or bgr as the case may be)
+    -----------------------------------------------------
+     this means we will probably need another for loop for
+     offset, as we probably want to blur for all channels
+     ---------------------------------------------------
+       Get each of the surrounding pixels weighted value
+       top, left, right, and bottom weighing 2x
+       diagonals weighing 1x
+       pixel itself weighing 4x
+       and take the average
+       --------------------------------------------
+       I think *out is just the output buffer, which
+       in this case we want to be *image, so I think all
+       those parts can be omitted
+  ******************************************/
+  static unsigned char *out = new unsigned char [width * height * depth];
+  for (y=1;y<height-1;y++)
+    for (x=1;x<width-1;x++)
+      for(offset=0;offset<depth;offset++)
+      {
+	temp=Image[(x-depth+offset+y*width)*depth+offset];
+	temp+=2*Image[(x-depth+y*width)*depth+offset];
+	temp+=Image[(x-depth+(y+1)*width)*depth+offset];
+	temp+=2*Image[(x+(y-1)*width)*depth+offset];
+	temp+=4*Image[(x+y*width)*depth+offset];
+	temp+=2*Image[(x+(y+1)*width)*depth+offset];
+	temp+=Image[(x+depth+offset+(y-1)*width)*depth+offset];
+	temp+=2*Image[(x+depth+offset+y*width)*depth+offset];
+	temp+=Image[(x+depth+offset+(y+1)*width)*depth+offset];
+	temp/=16;
+	out[(x+offset+y*width)*depth+offset] = temp;
+      }
+
+  for (y=1;y<height-1;y++)
+    for (x=1;x<width-1;x++)
+      for(offset=0;offset<depth;offset++)
+      	Image[(x+y*width)*depth+offset] = out[(x+y*width)*depth+offset] ;
+  
+  return PyInt_FromLong(0L);
+
+} 
+
 PyObject *Vision::applyFilterList() {
   return applyFilters(filterList);
 }
@@ -919,8 +1026,8 @@ PyObject *Vision::copyOriginal() {
 }
 
 PyObject *Vision::applyFilters(PyObject *newList) {
-  int i1, i2, i3, i4, i5, i6;
-  float f1, f2, f3, f4, f5, f6;
+  int i1, i2, i3, i4, i5, i6, i7;
+  float f1, f2, f3, f4, f5, f6, f7;
   PyObject *command;
   PyObject *filter, *list;
 
@@ -943,6 +1050,13 @@ PyObject *Vision::applyFilters(PyObject *newList) {
 	return NULL;
       }
       PyList_SetItem(retvals, i, superColor(f1, f2, f3, i1));
+    } else if (strcmp((char *)command, "scale") == 0) {
+      f1 = 1.0, f2 = 1.0, f3 = 1.0;
+      if (!PyArg_ParseTuple(list, "|fff", &f1, &f2, &f3)) {
+	PyErr_SetString(PyExc_TypeError, "Invalid applyFilters: scale()");
+	return NULL;
+      }
+      PyList_SetItem(retvals, i, scale(f1, f2, f3));
     } else if (strcmp((char *)command, "meanBlur") == 0) {
       i1 = 3;
       if (!PyArg_ParseTuple(list, "|i", &i1)) {
@@ -950,6 +1064,15 @@ PyObject *Vision::applyFilters(PyObject *newList) {
 	return NULL;
       }
       PyList_SetItem(retvals, i, meanBlur(i1));
+    } else if (strcmp((char *)command, "medianBlur") == 0) {
+      i1 = 3;
+      if (!PyArg_ParseTuple(list, "|i", &i1)) {
+	PyErr_SetString(PyExc_TypeError, "Invalid applyFilters: medianBlur");
+	return NULL;
+      }
+      PyList_SetItem(retvals, i, medianBlur(i1));
+    } else if (strcmp((char *)command, "gaussianBlur") == 0) {
+      PyList_SetItem(retvals, i, gaussianBlur());
     } else if (strcmp((char *)command, "sobel") == 0) {
       i1 = 1;
       if (!PyArg_ParseTuple(list, "|i", &i1)) {
@@ -972,12 +1095,19 @@ PyObject *Vision::applyFilters(PyObject *newList) {
       }
       PyList_SetItem(retvals, i, drawRect(i1, i2, i3, i4, i5, i6));
     } else if (strcmp((char *)command, "match") == 0) {
-      i1 = 0; i2 = 0; i3 = 0; i4 = 30; i5 = 0; i6 = ACCUM;
-      if (!PyArg_ParseTuple(list, "|iiiiii", &i1, &i2, &i3, &i4, &i5, &i6)) {
+      i1 = 0; i2 = 0; i3 = 0; i4 = 30; i5 = 0;
+      if (!PyArg_ParseTuple(list, "|iiiii", &i1, &i2, &i3, &i4, &i5)) {
 	PyErr_SetString(PyExc_TypeError, "Invalid applyFilters: match()");
 	return NULL;
       }
-      PyList_SetItem(retvals, i, match(i1, i2, i3, i4, i5, i6));
+      PyList_SetItem(retvals, i, match(i1, i2, i3, i4, i5));
+    } else if (strcmp((char *)command, "matchRange") == 0) {
+      i1 = 0; i2 = 0; i3 = 0; i4 = 255; i5 = 255; i6 = 255, i7 = 0;
+      if (!PyArg_ParseTuple(list, "|iiiiiii", &i1, &i2, &i3, &i4,&i5,&i6,&i7)) {
+	PyErr_SetString(PyExc_TypeError, "Invalid applyFilters: matchRange()");
+	return NULL;
+      }
+      PyList_SetItem(retvals, i, matchRange(i1, i2, i3, i4, i5, i6, i7));
     } else if (strcmp((char *)command, "grayScale") == 0) {
       PyList_SetItem(retvals, i, grayScale());
     } else if (strcmp((char *)command, "threshold") == 0) {
