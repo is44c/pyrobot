e62dfdd152f3aef15bb81143788386ab4c2f8e7c
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-10-30 02:53:55 +0000

    added real arrows, threshold around 0

--- tools/joystick.py
+++ tools/joystick.py
@@ -3,12 +3,21 @@ from Tkinter import *
 class Joystick:
 
    def __init__(self):
+      self.debug = 0
       self.app = Tk()
       self.app.withdraw()
       self.win = Toplevel()
       self.win.wm_title('Joystick')
       self.win.protocol('WM_DELETE_WINDOW',self.destroy)
       self.frame = Frame(self.win)
+      label = Label(self.frame, text = "Forward")
+      label.pack(side = "top")
+      label = Label(self.frame, text = "Reverse")
+      label.pack(side = "bottom")
+      label = Label(self.frame, text = "Turn\nLeft")
+      label.pack(side = "left")
+      label = Label(self.frame, text = "Turn\nRight")
+      label.pack(side = "right")
       self.canvas = Canvas(self.frame,
                            width = 220,
                            height = 220,
@@ -24,54 +33,59 @@ class Joystick:
       self.canvas.create_oval(105, 105, 115, 115, fill='black')
 
       self.frame.pack()
-      self.translate = 0
-      self.rotate = 0
+      self.translate = 0.0
+      self.rotate = 0.0
+      self.threshold = 0.10
 
    def getValue(self, event = None):
       return self.translate, self.rotate
 
    def move(self, translate, rotate):
       self.translate = translate
+      if self.translate < 0.0:
+         self.translate += self.threshold
+      elif self.translate > 0.0:
+         self.translate -= self.threshold
       self.rotate = rotate
+      if self.rotate < 0.0:
+         self.rotate += self.threshold
+      elif self.rotate > 0.0:
+         self.rotate -= self.threshold
+      if self.debug:
+         print self.translate, self.rotate
 
    def destroy(self):
       self.win.destroy()
 
    def canvas_clicked_up(self, event):
       self.canvas.delete("lines")
-      self.move(0, 0)
+      self.move(0.0, 0.0)
 
-   def drawArrows(self, x, y):
-      if y < 110:
-         yoffset = -10
+   def drawArrows(self, x, y, trans, rotate):
+      if trans == 0:
+         self.canvas.create_line(110, 110, 110, y, width=3, fill="blue", tag="lines")
       else:
-         yoffset = 10
-      if x < 110:
-         xoffset = -10
+         self.canvas.create_line(110, 110, 110, y, width=3, fill="blue", tag="lines", arrowshape = (10, 10, 3), arrow = "last")
+      if rotate == 0:
+         self.canvas.create_line(110, 110, x, 110, width=3, fill="red", tag="lines")
       else:
-         xoffset = 10
-      self.canvas.create_line(110, 110, 110, y, width=3, fill="blue", tag="lines")
-      self.canvas.create_line(110, y, 110 - 10, y - yoffset, width=3, fill="blue", tag="lines")
-      self.canvas.create_line(110, y, 110 + 10, y - yoffset, width=3, fill="blue", tag="lines")
-      self.canvas.create_line(110, 110, x, 110, width=3, fill="red", tag="lines")
-      self.canvas.create_line(x, 110, x - xoffset, 110 - 10, width=3, fill="red", tag="lines")
-      self.canvas.create_line(x, 110, x - xoffset, 110 + 10, width=3, fill="red", tag="lines")
+         self.canvas.create_line(110, 110, x, 110, width=3, fill="red", tag="lines", arrowshape = (10, 10, 3), arrow = "last")
 
    def canvas_clicked_down(self, event):
       if self.in_circle(event.x, event.y):
-         self.drawArrows(event.x, event.y)
          trans, rotate = self.calc_tr(event.x, event.y)
+         self.drawArrows(event.x, event.y, trans, rotate)
          self.move(trans, rotate)
 
    def canvas_moved(self, event):
       if self.in_circle(event.x, event.y):
          self.canvas.delete("lines")
-         self.drawArrows(event.x, event.y)         
          trans, rotate = self.calc_tr(event.x, event.y)
+         self.drawArrows(event.x, event.y, trans, rotate)         
          self.move(trans, rotate)
 
    def stop(self):
-      self.move(0, 0)
+      self.move(0.0, 0.0)
 
    def in_circle(self, x, y):
       r2 = ((self.circle_dim[2] - self.circle_dim[0])/2)**2
@@ -91,6 +105,10 @@ class Joystick:
                 (self.circle_dim[3] + self.circle_dim[1])/2)
       rot = float(center[0] - x) / float(center[0] - self.circle_dim[0])
       trans = float(center[1] - y) / float(center[1] - self.circle_dim[1])
+      if abs(rot) < self.threshold:
+         rot = 0.0
+      if abs(trans) < self.threshold:
+         trans = 0.0
       return (trans, rot)
 
 if __name__ == '__main__':
