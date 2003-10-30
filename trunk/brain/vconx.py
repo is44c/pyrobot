c69ebd73ce05b26f8bb11607ab8c481746098238
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-10-30 04:55:07 +0000

    header to wrap VisConx files nicely

---
+++ trunk/brain/vconx.py
@@ -0,0 +1,8 @@
+from pyro.brain.VisConx import VisRobotConx
+from pyro.brain.VisConx import VisSweepConx
+
+VNetwork = VisSweepConx.VisSweepNetwork
+VINetwork = VisRobotConx.VisRobotNetwork
+
+VSRN = VisSweepConx.VisSweepSRN
+VISRN = VisRobotConx.VisRobotSRN
