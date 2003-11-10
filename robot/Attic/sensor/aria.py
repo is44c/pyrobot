26bd9a8ac2a17c9b1a1a8babe892db7c5cd154f9
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2003-11-10 03:21:24 +0000

    Working on new sensor classes

--- robot/Attic/sensor/aria.py
+++ robot/Attic/sensor/aria.py
@@ -3,42 +3,28 @@ from pyro.robot.sensor import *
 
 class AriaRangeSensor(RangeSensor):
     """ For general Aria-sensors """
-
-class AriaLaser(AriaRangeSensor):
-    """ A Laser sensor for the Aria-class robots """
-
+    def __init__(self):
+        RangeSensor.__init__(self)
     def getType(self):
         return "range"
+
+class AriaLaserRangeSensor(AriaRangeSensor):
+    """ A Laser sensor for the Aria-class robots """
+    def __init__(self):
+        AriaRangeSensor.__init__(self)
     def getCount(self):
         return 180
-    def getMaxvalue(self):
-        return self.mmToUnits(15.0 * 1000, 'ROBOTS')
-
-	self.senses['laser'] = {}
-	self.senses['laser']['count'] = lambda self: 180
-	self.senses['laser']['type'] = lambda self: 'range'
-        self.senses['laser']['maxvalue'] = self.getLaserMaxRange
-        self.senses['laser']['units'] = lambda self: "ROBOTS"
-        self.senses['laser']['all'] =   self.getLaserRangeAll
-
-        # location of sensors' hits:
-        self.senses['laser']['x'] = self.getLaserXCoord
-	self.senses['laser']['y'] = self.getLaserYCoord
-	self.senses['laser']['z'] = lambda self, pos: 0.03
-	self.senses['laser']['value'] = self.getLaserRange
-	self.senses['laser']['flag'] = self.getLaserFlag
-
-	# location of origin of sensors:
-        self.senses['laser']['ox'] = self.laser_ox
-	self.senses['laser']['oy'] = self.laser_oy
-	self.senses['laser']['oz'] = lambda self, pos: 0.03 # meters
-	self.senses['laser']['th'] = self.laser_th
-        # in radians:
-        self.senses['laser']['arc'] = lambda self, pos : 1
-
-
+    def getMaxValue(self):
+        return 15.0
+    def update(self):
+        pass # handled by robot
 
 class AriaSonar(AriaRangeSensor):
     """ A Sonar sensor for the Aria-class robots """
 
     
+if __name__ == "__main__":
+    s = AriaLaserRangeSensor()
+    s.setRobotDiameter(10)
+    s.setMaxValue(25)
+    s.check()
