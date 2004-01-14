6bb1e2d957096a402085882fc7ffd591be375461
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-01-14 16:01:20 +0000

    Finished PTZ for Sony and Canon

--- trunk/plugins/Attic/services/V4LCamera.py
+++ trunk/plugins/Attic/services/V4LCamera.py
@@ -1,15 +1,6 @@
-""" A simple loader for a Video for Linux (V4L) frame grabber """
-
 from pyro.camera.v4l import V4LGrabber
 from pyro.vision.cvision import VisionSystem
 
 def INIT(robot):
-    if robot.get("self", "name") == "Aria":
-        # Pioneers. You may have to set channel by hand to one that works
-        ch = 0 # channel
-    else:
-        # For framegrabbers:
-        # Channel -  0: television; 1: composite; 2: S-Video
-        ch = 1 # channel
-    return {"V4LCamera" : V4LGrabber( 160, 120, channel = ch,
+    return {"V4LCamera" : V4LGrabber( 384, 240, channel = 0,
                                       visionSystem = VisionSystem())}
