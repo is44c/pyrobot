3569186612869a0d26837ef80650196ceb050ea3
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-03-14 21:54:37 +0000

    Had initializers in wrong order

--- trunk/camera/fake/__init__.py
+++ trunk/camera/fake/__init__.py
@@ -9,14 +9,14 @@ class FakeCamera(Camera):
    accessing the hardware, it will load a series of images from file.
    """
    def __init__(self, pattern = None,
-                start = 0, limit = 19, char = "?",
-                interval = 1.0, visionSystem = None):
+                start = 0, stop = 19, char = "?",
+                interval = 1.0, visionSystem = None, verbose = 0):
       """
       pattern is a filename with indicators on where to put digits for the
       sequence.  Absolute or relative filenames can be used.
 
       For example, 'image???-.ppm' would start at 'image000.ppm'
-      and continue up to limit.
+      and continue up to stop.
       
       char is the character that should be replaced in the pattern.
 
@@ -26,10 +26,11 @@ class FakeCamera(Camera):
       FakeCamera('vision/snaps/som-?.ppm', 0, 19)
       """
       if pattern == None:
-         pattern = "/vision/snaps/som-?.ppm"
+         pattern = "vision/snaps/som-?.ppm"
       self.pattern = pattern
-      self.limit = limit
+      self.stop = stop
       self.interval = interval
+      self.verbose = verbose
       self.lastUpdate = 0
       #create a matchdata object that we will store
       self.match = re.search(re.escape(char) + "+", pattern)
@@ -47,6 +48,8 @@ class FakeCamera(Camera):
          self.path = os.getenv('PYRO') + "/"
       else:
          raise "file not found:" + currname
+      if self.verbose:
+         print "info: readings file '%s'..." % (self.path + currname)
       self.cameraDevice = Fake(self.path + currname)
       # connect vision system: --------------------------
       self.vision = visionSystem
@@ -56,14 +59,14 @@ class FakeCamera(Camera):
       self.depth = self.vision.getDepth()
       self.cbuf = self.vision.getMMap()
       # -------------------------------------------------
-      self.data = CBuffer(self.cbuf)
       self.rgb = (0, 1, 2) # offsets to RGB
       self.format = "RGB"
       Camera.__init__(self, self.width, self.height, self.depth,
                       "Fake Camera View")
+      self.data = CBuffer(self.cbuf)
       self.optionsInitialized = 0
       self.oldStart = None
-      self.oldLimit = None
+      self.oldStop = None
 
    def makeWindow(self):
       Camera.makeWindow(self)
@@ -85,24 +88,26 @@ class FakeCamera(Camera):
    def freezeFrame(self):
       if self.oldStart == None:
          self.oldStart = self.start
-         self.oldLimit = self.limit
-         self.limit = self.current
+         self.oldStop = self.stop
+         self.stop = self.current
          self.start = max(self.current - 1, 0)
 
    def unFreezeFrame(self):
       if self.oldStart != None:
-         self.limit = self.oldLimit
+         self.stop = self.oldStop
          self.start = self.oldStart
          self.oldStart = None
-         self.oldLimit = None
+         self.oldStop = None
          
    def _update(self):
-      if (self.current < self.limit):
+      if (self.current < self.stop):
          currentTime = time.time()
          if currentTime - self.lastUpdate > self.interval:
             currname = self.pattern[:self.match.start()] + \
                        self.fstring % self.current + \
                        self.pattern[self.match.end():]
+            if self.verbose:
+               print "info: readings file '%s'..." % (self.path + currname)
             self.cameraDevice.updateMMap(self.path + currname)
             self.processAll()
             self.current += 1
