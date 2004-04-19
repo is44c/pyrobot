abe6f67f473e0df925c3fa85e5970db24569c1de
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-04-19 03:10:37 +0000

    Changed plot/simple to scroll of a sort; added Front-all view

--- trunk/gui/plot/simple.py
+++ trunk/gui/plot/simple.py
@@ -9,12 +9,14 @@ class SimplePlot(Tkinter.Tk):
     COLORS = ['blue', 'red', 'tan', 'yellow', 'orange', 'black',
               'azure', 'beige', 'brown', 'coral', 'gold', 'ivory',
               'moccasin', 'navy', 'salmon', 'tan', 'ivory']
-    def __init__(self, robot, what):
+    def __init__(self, robot, what, width = 400, height = 120):
         Tkinter.Tk.__init__(self)
+        self.width = width
+        self.height = height
         self.what = what
         self.robot = robot
         self.title("%s Sensors" % what)
-        self.canvas = Tkinter.Canvas(self,width=400,height=120)
+        self.canvas = Tkinter.Canvas(self,width=width,height=height)
         self.canvas.pack()
         self.dataMin = 0
         self.dataMax = robot.get('range', 'maxvalue')
@@ -34,17 +36,23 @@ class SimplePlot(Tkinter.Tk):
     def redraw(self, options = {}):
         # do something to draw yourself
         if self.dataCount > self.dataWindowSize:
-            self.canvas.delete('data')
-            self.dataCount = 0
+            self.canvas.delete('data1')
+            self.canvas.move("data2", -self.width/2, 0)
+            self.canvas.itemconfigure("data2", tag = "data1")
+            self.dataCount = self.dataWindowSize / 2
         else:
             self.dataCount += 1
         for sensor in self.robot.sensorSet[self.what]:
             dist = self.robot.get('range', 'value', sensor)
+            if self.dataCount < self.dataWindowSize/2:
+                tag = "data1"
+            else:
+                tag = "data2"
             self.canvas.create_line(self.dataCount - 1,
                                     self.dataHist[sensor],
                                     self.dataCount,
                                     int(float(dist)/self.dataMax * 100.0),
-                                    tags = 'data',
+                                    tags = tag,
                                     width = 2,
                                     fill = SimplePlot.COLORS[sensor])
             self.dataHist[sensor] = int(float(dist)/self.dataMax * 100.0 - 1)
@@ -55,6 +63,14 @@ if __name__ == '__main__':
         def __init__(self):
             self.sensorSet = {'all': (0,1)}
         def get(self, t1 = None, t2 = None, t3 = None):
-            return int(random.random() * 10)
+            if t1 == "range" and t2 == "count":
+                return 2
+            elif t1 == "range" and t2 == "maxvalue":
+                return 10.0
+            else:
+                return int(random.random() * 10)
     plot = SimplePlot(Robot(), 'all')
+    for i in range(2500):
+        plot.redraw()
+    print "Done!"
     plot.mainloop()
--- trunk/plugins/brains/NNPredict.py
+++ trunk/plugins/brains/NNPredict.py
@@ -1,8 +1,6 @@
 # A Neural Network Brain
 # D.S. Blank
 
-# Not a true predictor but useful as a platform for testing the new Conx step method.
-
 from pyro.brain import Brain
 from pyro.brain.conx import *
 
@@ -12,27 +10,14 @@ class predict(Brain):
       self.sensorCount = self.getRobot().get('range', 'count')
       self.net = Network()
       self.net.addThreeLayers(self.sensorCount + 2, 5, 2)
-      #self.net.add(Layer('input', self.sensorCount+2))
-      #self.net.addContext(Layer('context', 2), 'hidden')
-      #self.net.add(Layer('hidden', 2))
-      #self.net.add(Layer('prediction', self.sensorCount))
-      #self.net.add(Layer('motorOutput', 2))
-      #self.net.connect('input', 'hidden')
-      #self.net.connect('context', 'hidden')
-      #self.net.connect('hidden', 'prediction')
-      #self.net.connect('hidden', 'motorOutput')
-      
       self.net.initialize()
       self.net.setVerbosity(0)
-      #self.net.setInitContext(0)
       self.net.setEpsilon(0.5)
       self.net.setMomentum(.1)
       self.net.setLearning(1)
-
       self.trans = 0
       self.rotate = 0
       self.counter = 0
-      #self.getRobot().set('range', 'units', 'METERS')
       self.maxvalue = self.getRobot().get('range', 'maxvalue')
       self.new = map(self.scale, self.getRobot().get('range', 'value', 'all'))
 
@@ -51,24 +36,6 @@ class predict(Brain):
       right = self.getRobot().get('range', 'value', 'front-right', 'minval')
       if left < .5 or right < .5:
          target_trans = 0.5
-         target_rotate = 0.0 
-      elif left < 0.8:
-         target_trans = 0.75
-         target_rotate = 0.0
-      elif right < 0.8:
-         target_trans = 0.75
-         target_rotate = 1.0
-      return [target_trans, target_rotate]
-
-   def slow(self):
-      # set targets   
-      target_trans  = 1.0
-      target_rotate = 0.5
-      # left and right and front:
-      left = self.getRobot().get('range', 'value', 'front-left', 'minval')
-      right = self.getRobot().get('range', 'value', 'front-right', 'minval')
-      if left < .5 or right < .5:
-         target_trans = 0.5
       elif left < 0.8:
          target_trans = 0.75
       elif right < 0.8:
@@ -76,7 +43,7 @@ class predict(Brain):
       return [target_trans, target_rotate]
 
    def step(self):
-      target = self.slow()
+      target = self.avoid()
 
       old = self.new + [self.trans, self.rotate] #trans and rotate
       self.new = map(self.scale, self.getRobot().get('range', 'value', 'all'))
