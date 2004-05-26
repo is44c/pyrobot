f54e4f4ac2d88e21569078011b2dad5954696dfb
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-05-26 18:43:01 +0000

    Added LPV service

---
+++ trunk/plugins/Attic/services/LocalPerceptualView.py
@@ -0,0 +1,40 @@
+""" This service loads a Local Perceptual View """
+
+from pyro.map.lps import LPS
+from pyro.robot.service import Service
+import pyro.system.share as share
+
+class LPSService(LPS, Service):
+    def __init__(self, robot, *args, **kwargs):
+        self.robot = robot
+        Service.__init__(self, "LPS Service", 1) # visible = 1
+        LPS.__init__(self, *args, **kwargs)
+    
+    def makeWindow(self):
+        pass
+
+    def updateWindow(self):
+        self.redraw()
+
+    def getServiceData(self):
+        return self.grid
+
+    def getServiceState(self):
+        return self.state
+
+    def updateService(self):
+        self.reset()
+        self.sensorHits(self.robot, 'range')
+
+def INIT(robot):
+    if "LocalPerceptualView" in robot.getServices():
+        print "Service is already running: LocalPerceptualView"
+        return {}
+    units = robot.get('range', 'units')
+    robot.set('range', 'units', 'MM')
+    rangeMaxMM = robot.get('range', 'maxvalue')
+    sizeMM = rangeMaxMM * 3 + robot.get('self', 'radius')
+    # Reset our unit of measure
+    robot.set('range', 'units', units)
+    lps = LPSService(robot, share.gui, 20, 20, widthMM = sizeMM, heightMM = sizeMM)
+    return {"LocalPerceptualView": lps}
