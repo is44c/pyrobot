f7a4f10f35ab97481f7ae71a5fdd87820d37265d
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-07-02 00:57:44 +0000

    Finished camera; devData

--- trunk/plugins/Attic/services/Test.py
+++ trunk/plugins/Attic/services/Test.py
@@ -4,8 +4,12 @@ from pyro.robot.service import Service
 
 class Test(Service):
 
+    def seeData(self, *args):
+        return args
+
     def setup(self):
         self.devData['specialvalue'] = 42
+        self.subDataFunc['subdata'] = self.seeData
 
     def makeWindow(self):
         self.visible = 1
