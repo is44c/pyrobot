fa3ac8cb687e4cf49621001abe034c646b5fd942
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-07-08 03:09:19 +0000

    New brain

---
+++ trunk/plugins/brains/Vehicle2a.py
@@ -0,0 +1,30 @@
+"""
+Braitenberg Vehicle2a for the Khepera
+D.S. Blank
+"""
+
+from pyro.brain import Brain, avg
+
+class Vehicle(Brain):
+   # Only method you have to define for a brain is the step method:
+
+   def setup(self):
+      self.set('/devices/light0/units', "RAW")
+      self.maxvalue = self.get('/devices/light0/maxvalue')
+
+   def convert(self, raw):
+      return (self.maxvalue - raw) / self.maxvalue
+      
+   def step(self):
+      leftSensorValue  = self.get('/devices/light0/1/value') # left lights
+      rightSensorValue = self.get('/devices/light0/4/value') # right lights
+      leftSpeed  = self.convert(leftSensorValue)
+      rightSpeed = self.convert(rightSensorValue)
+      self.robot.motors(leftSpeed,  rightSpeed) # to the left
+
+def INIT(engine):
+   if engine.robot.get("robot/type") != 'Khepera':
+      raise "Robot should have light sensors!"
+   print "OK: robot has light sensors."
+   return Vehicle('Braitenberg2a', engine)
+      
