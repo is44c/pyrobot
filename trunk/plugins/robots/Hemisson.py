cd43f494c7f1ee2fec5482b4a26cc5146b2a38d6
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-07-26 05:20:52 +0000

    New for Version 3.0.1

--- trunk/plugins/robots/Hemisson.py
+++ trunk/plugins/robots/Hemisson.py
@@ -4,5 +4,10 @@ import termios
 from pyro.robot.khepera import *
 
 def INIT():
+    # For serial connected Hemisson:
+    #return KheperaRobot(port = "/dev/ttyS0", # "/dev/ttyUB0",
+    #                    rate = termios.B115200,
+    #                    subtype = "Hemisson")
     return KheperaRobot(port = "/dev/ttyUB0",
-                        rate = termios.B115200)
+                        rate = termios.B115200,
+                        subtype = "Hemisson")
