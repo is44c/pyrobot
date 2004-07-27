309d63e9462a65a4dc421433decb5ab0cbc5a784
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-07-27 09:51:30 +0000

    New test of devices

--- trunk/plugins/devices/Test.py
+++ trunk/plugins/devices/Test.py
@@ -1,8 +1,8 @@
-# A Simple Service
+# A Simple Device
 
-from pyro.robot.service import Service
+from pyro.robot.device import Device
 
-class Test(Service):
+class Test(Device):
 
     def seeData(self, *args):
         return args
@@ -12,11 +12,15 @@ class Test(Service):
         self.subDataFunc['subdata'] = self.seeData
 
     def makeWindow(self):
-        self.visible = 1
-        print "made window!"
+        self.devData[".visible"] = 1
+        print "[[[[ made window! ]]]]"
 
     def updateWindow(self):
-        print "update window!"
+        print "update window!" # when visible
+
+    def updateDevice(self):
+        #print "------------- update device!" 10 times a second!
+        pass
 
 def INIT(robot):
     return {"test": Test()}
