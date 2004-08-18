9d08dc51e212b91edd78e9342761278dc7f3b35a
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-08-18 19:37:27 +0000

    Updated with new K-Team serial interface

--- plugins/brains/Vehicle3.py
+++ plugins/brains/Vehicle3.py
@@ -9,8 +9,8 @@ class Vehicle(Brain):
    # Only method you have to define for a brain is the step method:
 
    def step(self):
-       lpos, left = self.robot.get('light', 'value', (0, 1, 2), 'min') # light
-       rpos, right = self.robot.get('light', 'value', (3, 4, 5), 'min')# light
+       left = min(self.robot.get('robot/light', (0, 1, 2), 'value')) 
+       right = min(self.robot.get('robot/light', (3, 4, 5), 'value'))
        forward = .2
        if left < right: # lower means more light
           self.robot.move(forward,  0.3) # to the left
@@ -18,7 +18,7 @@ class Vehicle(Brain):
           self.robot.move(forward, -0.3) # to the right
 
 def INIT(engine):
-   if engine.robot.type != 'khepera':
+   if engine.robot.get("robot/type") != 'K-Team':
       raise "Robot should have light sensors!"
    print "OK: robot has light sensors."
    return Vehicle('Braitenberg', engine)
