41ddb105df8e03396f0bc770cdead31e683c31f5
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-09-10 17:25:27 +0000

    Set values for Hemisson

--- plugins/robots/Hemisson.py
+++ plugins/robots/Hemisson.py
@@ -5,9 +5,6 @@ from pyro.robot.khepera import *
 
 def INIT():
     # For serial connected Hemisson:
-    #return KheperaRobot(port = "/dev/ttyS0", # "/dev/ttyUB0",
-    #                    rate = termios.B115200,
-    #                    subtype = "Hemisson")
     return KheperaRobot(port = "/dev/ttyUB0",
-                        rate = 115200, # 38400
+                        rate = 115200,
                         subtype = "Hemisson")
