aef0133bc6716691de47f306057c258f4cfc65cb
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-09-12 01:55:57 +0000

    Robocup, version 0.1

--- plugins/robots/RobocupTeamB.py
+++ plugins/robots/RobocupTeamB.py
@@ -3,4 +3,13 @@
 from pyro.robot.robocup import *
 
 def INIT():
-    return RobocupRobot(name = "TeamB", teamMode = 1)
+    # Make a team of 11 robots:
+    list = [0] * 11
+    for x in range(11):
+        list[x] = RobocupRobot(name = "TeamB", goalie = (x == 0))
+    # store the list on the first one
+    list[0].team = list
+    # put the goalie in the box
+    list[0].set("devices/truth0/pose", (-50, 0))
+    # return the first one
+    return list[0]
