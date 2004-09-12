4462fdd3ca882cf0045d42f6913f85dbce5ce778
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-09-12 01:55:57 +0000

    Robocup, version 0.1

--- trunk/robot/robocup.py
+++ trunk/robot/robocup.py
@@ -1,18 +1,17 @@
 from socket import *
 from pyro.robot import Robot
 from pyro.robot.device import Device
+from random import random
 
-class TruthDevice(Device):
-	def __init__(self, dev, type = "truth"):
-		Device.__init__(self, type)
-		self.dev = dev
-		self.devData["pose"] = (0, 0)
-	def postSet(self, item):
-		if item == "pose":
-			self.dev.sendMsg("(move %f %f)" % (self.devData["pose"][0], self.devData["pose"][1] ))
-			
+def makeDict(pairs):
+	""" Turns list of [name, value] pairs into a dict {name: value}"""
+	dict = {}
+	for item in pairs:
+		dict[item[0]] = item[1]
+	return dict
 
 def lex(str):
+	""" Simple lexical analizer for Lisp-like parser."""
 	retval = []
 	currentword = ""
 	for ch in str:
@@ -39,6 +38,7 @@ def lex(str):
 	return retval
 
 def parse(str):
+	""" Lisp-like parser. Takes str, returns Python list. """
 	lexed = lex(str)
 	stack = []
 	currentlist = []
@@ -48,7 +48,8 @@ def parse(str):
 			currentlist = []
 		elif symbol == ")":
 			if len(stack) == 0:
-				raise AttributeError, "too many closing parens"
+				print "too many closing parens:", str
+				return []
 			currentlist, temp = stack.pop(), currentlist
 			currentlist.append( temp )
 		else:
@@ -59,23 +60,38 @@ def parse(str):
 			else:
 				try:
 					# doesn't get "drop_ball" above
+					# in the isalpha test
 					currentlist.append( float(symbol) )
 				except:
+					if len(symbol) > 1 and symbol[0] == '"' and symbol[-1] == '"':
+						symbol = symbol[1:-1]
 					currentlist.append( symbol )
 	if len(stack) != 0:
-		raise AttributeError, "missing ending paren"
+		print "missing ending paren:", str
+		return []
 	return currentlist[0]
 
+class TruthDevice(Device):
+	""" A Truth Device for the Robocup robot"""
+	def __init__(self, dev, type = "truth"):
+		Device.__init__(self, type)
+		self.dev = dev
+		self.devData["pose"] = (0, 0)
+	def postSet(self, item):
+		if item == "pose":
+			self.dev.sendMsg("(move %f %f)" % (self.devData["pose"][0], self.devData["pose"][1] ))
+			
+
 class RobocupRobot(Robot):
-	BUF = 1024
-	def __init__(self, name = "TeamPyro", host = "localhost",
-		     port = 6000, teamMode = 0):
+	""" A robot to interface with the Robocup simulator. """
+	BUF = 10000
+	def __init__(self, name="TeamPyro", host="localhost", port=6000,
+		     goalie = 0):
 		Robot.__init__(self)
 		self.lastTranslate = 0
 		self.lastRotate = 0
 		self.updateNumber = 0L
 		self.historyNumber = 0L
-		self.teamMode = teamMode
 		self.lastHistory = 0
 		self.historySize = 100
 		self.history = [0] * self.historySize
@@ -83,14 +99,22 @@ class RobocupRobot(Robot):
 		self.devData["name"] = name
 		self.devData["host"] = host
 		self.devData["port"] = port
+		self.devData["continuous"] = 1
+		self.devData["goalie"] = goalie
 		self.address = (self.devData["host"], self.devData["port"])
 		self.socket = socket(AF_INET, SOCK_DGRAM)
-		self.socket.sendto("(init %s)" % self.devData["name"], self.address)
+		msg = "(init %s (version 9.0)" % self.devData["name"]
+		if goalie:
+			msg += "(goalie)"
+		msg += ")" 
+		self.socket.sendto(msg, self.address)
+		for x in range(10):
+			# get the real address
+			self.update()
 		self.devData["builtinDevices"] = ["truth"]
 		self.startDevice("truth")
-		self.set("/devices/truth0/pose", (-25, 0))
-		if self.teamMode:
-			self.makeTeam()
+		self.set("/devices/truth0/pose", (random() * 100 - 50,
+						  random() * 20 - 10))
 		
 	def startDeviceBuiltin(self, item):
 		if item == "truth":
@@ -98,25 +122,56 @@ class RobocupRobot(Robot):
 		else:
 			raise AttributeError, "robocup robot does not support device '%s'" % item
 		
-	def sendMsg(self, msg):
-		self.socket.sendto(msg, self.address)
+	def sendMsg(self, msg, address = None):
+		if address == None:
+			address = self.address
+		self.socket.sendto(msg + chr(0), address)
 
 	def getMsg(self):
 		data, addr = self.socket.recvfrom(self.BUF)
-		return parse(data)
+		return parse(data), addr
 
 	def disconnect(self):
 		self.stop()
 		self.socket.close()
 
 	def update(self):
+		self._update()
 		self.lastHistory = self.historyNumber % self.historySize
-		msg = self.getMsg()
+		msg, addr = self.getMsg()
 		if len(msg):
 			self.history[self.lastHistory] = msg
 			self.historyNumber += 1
-		self._update()
-		self.keepGoing()
+			if msg[0] == "init":
+				self.devData[msg[0]] = msg[1:]
+				self.address = addr
+			elif msg[0] == "server_param":
+				# next is list of pairs
+				self.devData[msg[0]] = makeDict(msg[1:])
+			elif msg[0] == "player_param":
+				# next is list of pairs
+				self.devData[msg[0]] = makeDict(msg[1:])
+			elif msg[0] == "player_type": # types
+				# next is list of ["id" num], pairs...
+				id = "%s:%d" % (msg[0], msg[1][1])
+				self.devData[id] = makeDict(msg[2:])
+			elif msg[0] == "sense_body": # time pairs...
+				self.devData[msg[0]] = makeDict(msg[2:])
+				self.devData["sense_body:time"] = msg[1]
+			elif msg[0] == "see": # time tuples...
+				self.devData[msg[0]] = msg[2:]
+				self.devData["%s:time" % msg[0]] = msg[1]
+			elif msg[0] == "error":
+				print "Robocup error:", msg[1]
+			elif msg[0] == "warning":
+				print "Robocup warning:", msg[1]
+			elif msg[0] == "hear": # hear time who what
+				self.devData[msg[0]] = msg[2:]
+				self.devData["%s:time" % msg[0]] = msg[1]
+			else:
+				print "need to handle '%s'" % msg[0], msg
+		if self.devData["continuous"]:
+			self.keepGoing()
 		self.updateNumber += 1
 
 	def keepGoing(self):
@@ -147,28 +202,6 @@ class RobocupRobot(Robot):
 		self.sendMsg("(turn %f)" % val)
 
 	def move(self, translate_velocity, rotate_velocity):
-		# only one per cycle!
 		self.translate(translate_velocity)
 		self.rotate(rotate_velocity)
 
-	def makeTeam(self):
-		self.teamMode = 1
-		self.team = [0] * 11
-		# main robot:
-		self.team[0] = self.socket
-		# goalie
-		# TODO: make this be a (goalie)
-		self.team[1] = socket(AF_INET, SOCK_DGRAM)
-		self.team[1].sendto("(init %s)" % self.devData["name"],
-				    self.address)
-		self.team[1].sendto("(move 25 25)", self.address)
-		# create the other team mates:
-		for x in range(2, 11):
-			self.team[x] = socket(AF_INET, SOCK_DGRAM)
-			self.team[x].sendto("(init %s)" % self.devData["name"],
-					    self.address)
-			# put in random place
-			self.team[x].sendto("(move 25 25)", self.address)
-
-	def teamMsg(self, number, msg):
-		self.team[number].sendto(msg, self.address)
