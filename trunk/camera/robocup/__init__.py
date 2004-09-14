b286de348c59febe4d5fd22b5d673218729e3466
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-09-14 17:55:39 +0000

    Working camera for robocup

--- trunk/camera/robocup/__init__.py
+++ trunk/camera/robocup/__init__.py
@@ -8,16 +8,13 @@ PIOVER180 = pi / 180.0
 class RobocupCamera(Camera):
    """
    """
-   def __init__(self, robot, width=40, height=30, depth = 3, interval = 1.0,
-                visionSystem = None):
+   def __init__(self, robot, width=80, height=60, depth = 3, visionSystem = None):
       """
       """
       self.robot = robot
       self.width = width
       self.height = height
       self.depth = depth
-      self.interval = interval
-      self.lastUpdate = 0
       self.cameraDevice = Robocup(self.width, self.height,
                                   self.depth)
       # connect vision system: --------------------------
@@ -31,6 +28,7 @@ class RobocupCamera(Camera):
       # outer boundaries: Top, Left, Right, Bottom
       # inner lines: top, left, right, bottom
       # pentalty box: 1pleft, 2pright
+      # from page 31 of the Robocup Soccer Simulator manual:
       self.lines = {"top": ["lt", "ct", "rt"],
                     "left": ["lt", "glt", "gl", "glb", "lb"],
                     "bottom": ["lb", "cb", "rb"],
@@ -45,7 +43,11 @@ class RobocupCamera(Camera):
                               "rb30", "rb20", "rb10"],
                     "center": ["t0", "ct", "c", "cb", "b0"],
                     "1pleft": ["plt", "plc", "plb"],  
-                    "2pright": ["prt", "prc", "prb"],  
+                    "2pright": ["prt", "prc", "prb"],
+                    "a": ["plt", "lt20"], # draw pentalty box sides
+                    "A": ["plb", "lb20"], # draw pentalty box sides
+                    "z": ["prt", "rt20"], # draw pentalty box sides
+                    "Z": ["prb", "rb20"], # draw pentalty box sides
                     }
       self.data = CBuffer(self.cbuf)
       self.rgb = (0, 1, 2) # offsets to RGB
@@ -74,40 +76,41 @@ class RobocupCamera(Camera):
       return retval
 
    def _update(self):
-      currentTime = time.time()
-      if currentTime - self.lastUpdate > self.interval:
-         try:
-            see = self.robot.get("robot/see")
-         except:
-            print "waiting for Robocup camera to come online..."
-            return # not ready yet
-         see.sort(lambda x,y: cmp(y[1],x[1]))
-         objlist = []
-         linePoints = {}
-         for s in self.lines:
-            linePoints[s] = []
-         for item in see:
-            # item is something like: [['f', 'c'], 14, 36, 0, 0]
-            if len(item) >= 3: # otherwise, can't do much without direction
-               if item[0][0] == "f" or item[0][0] == "g": # it's a flag or goal
-                  flagName = ""
-                  for ch in item[0][1:]:
-                     flagName += "%s" % ch
-                  onLines = self.lookupLines( flagName )
-                  for onLine in onLines:
-                     # distance, direction
-                     x, y = self.getPoint( item[1], item[2])
-                     if x != None and y != None:
-                        linePoints[onLine].append( (x, y) )
-               elif item[0][0] == "p" or \
-                    item[0][0] == "b" or \
-                    item[0][0] == "g":
-                  itemName = ""
-                  for ch in item[0]:
-                     itemName += "%s" % ch
+      try:
+         see = self.robot.get("robot/see")
+      except:
+         print "waiting for Robocup camera to come online..."
+         return # not ready yet
+      see.sort(lambda x,y: cmp(y[1],x[1]))
+      objlist = []
+      linePoints = {}
+      for s in self.lines:
+         linePoints[s] = []
+      for item in see:
+         # item is something like: [['f', 'c'], 14, 36, 0, 0]
+         if len(item) >= 3: # otherwise, can't do much without direction
+            if item[0][0] == "f" or item[0][0] == "g": # it's a flag or goal
+               flagName = ""
+               for ch in item[0][1:]:
+                  flagName += "%s" % ch
+               onLines = self.lookupLines( flagName )
+               for onLine in onLines:
+                  # distance, direction
+                  x, y = self.getPoint( item[1], item[2])
                   if x != None and y != None:
-                     retval = self.getPoint(item[1],item[2])
-                     objlist.append( (itemName, retval[0], retval[1]) )
-         self.cameraDevice.updateMMap((linePoints, objlist))
-         self.processAll() # filters
-         self.lastUpdate = currentTime
+                     linePoints[onLine].append( (x, y) )
+            if item[0][0] == "p" or \
+                   item[0][0] == "b" or \
+                   item[0][0] == "g" or \
+                   (item[0][0] == "f" and item[0][1] == "g"):
+               itemName = ""
+               for ch in item[0]:
+                  itemName += "%s" % ch
+               if x != None and y != None:
+                  retval = self.getPoint(item[1],item[2])
+                  objlist.append( (itemName, retval[0], retval[1]) )
+      try:
+         self.cameraDevice.updateMMap(linePoints, objlist)
+      except TypeError:
+         print "error in RobocupCamera data format: ignored"
+      self.processAll() # filters
