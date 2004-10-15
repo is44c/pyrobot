43ba61c01658691cf7f0a672e9ed0d9de65f3a2a
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-10-15 08:12:13 +0000

    Added robots for the Symbloic Sim

---
+++ plugins/robots/WumpusHunter.py
@@ -0,0 +1,48 @@
+import socket
+from pyro.robot import Robot
+
+class TCPRobot(Robot):
+	"""
+	A simple TCP-based socket robot for talking to SymbolicSimulator.
+	"""
+	BUFSIZE = 1024
+	def __init__(self, host, port):
+		Robot.__init__(self)
+		# Set the socket parameters
+		self.devData["host"] = host
+		self.devData["port"] = port
+		self.devData["Location"] = None
+		self.devData["Status"] = None
+		self.notSetables.extend( ["Status", "Location"] )
+		self.addr = (host, port)
+		# Create socket
+		self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.socket.settimeout(1)
+		self.socket.connect( self.addr )
+
+	def localize(self, x = 0, y = 0, th = 0):
+		pass
+		
+	def update(self):
+		self.devData["Location"] = self.move("Location")
+		self.devData["Direction"] = self.move("Direction")
+		self._update()
+
+	def move(self, message, other = None):
+		if other != None: return # rotate,translate command ignored
+		if (self.socket.sendto(message, self.addr)):
+			try:
+				retval, addr = self.socket.recvfrom(self.BUFSIZE)
+			except:
+				retval = ""
+			retval = retval.strip()
+		return retval
+
+	def disconnect(self):
+		# Close socket
+		self.socket.close()
+
+def INIT():
+	robot = TCPRobot("localhost", 6000)
+	return robot
+
