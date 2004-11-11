71142340930101340f6f733dc5007d946a139ea1
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-11-11 22:17:07 +0000

    Basic socket functions

--- trunk/camera/aibo/Aibo.h
+++ trunk/camera/aibo/Aibo.h
@@ -1,24 +1,21 @@
 #ifndef __AIBO_H__
 #define __AIBO_H__
 
-#define SWAP(a,b) { int itemp=(a);(a)=(b);(b)=itemp;}
-#define MAX(a,b) ((a)>(b)?(a):(b))
-#define MIN(a,b) ((a)<(b)?(a):(b))
-
+#include <sys/types.h> 
+#include <netinet/in.h> 
+#include <netdb.h>
+#include <sys/socket.h> 
 #include "Device.h"
 
 class Aibo : public Device {
  public:
-  Aibo(int w, int h, int d);
-  PyObject *updateMMap(PyObject *points, PyObject *lines);
-  void clear();
-  void setPixel(int x, int y, int r, int g, int b);
-  void drawLine(long int x1, long int y1, 
-		long int x2, long int y2, 
-		long int r, long int g, long int b);
-  void drawRect(long int x1, long int y1, 
-		long int x2, long int y2, 
-		long int r, long int g, long int b);
+  Aibo(char *hostname);
+  PyObject *updateMMap();
+  char *host;
+  int sockfd;
+ private:
+  struct sockaddr_in server_addr;
+  struct hostent *he;
 };
 
 #endif
