a149e7de03a0a7fcdeb4f29e2b11dd66150df7aa
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2004-11-13 20:07:28 +0000

    New JPEG decompress from PLayer/Stage packet code

--- trunk/camera/aibo/Aibo.cpp
+++ trunk/camera/aibo/Aibo.cpp
@@ -1,6 +1,7 @@
 #include "Aibo.h"
 
-#include "jpeglib.h"
+#include <jpeglib.h>
+#include "jpeg.h"
 
 long convert(char *buff) {
   long retval = 0;
@@ -9,144 +10,6 @@ long convert(char *buff) {
   retval += (buff[2] & 0xFF) << 16;
   retval += (buff[3] & 0xFF) << 24;
   return retval;
-}
-
-static void init_source (j_decompress_ptr cinfo) {
-  // nothing
-}
-static boolean fill_input_buffer (j_decompress_ptr cinfo) {
-  struct jpeg_source_mgr *src = cinfo->src;
-  // WARNMS(cinfo, JWRN_JPEG_EOF);
-  /* Insert a fake EOI marker */
-  char buffer[2] = { (JOCTET) 0xFF, (JOCTET) JPEG_EOI };
-  // The next two lines were READ ONLY?
-  //src->next_input_byte[0] = (JOCTET) 0xFF;
-  //src->next_input_byte[1] = (JOCTET) JPEG_EOI;
-  memcpy((void *)src->next_input_byte, buffer, 2);
-  src->bytes_in_buffer = 2;
-  return TRUE;
-}
-static void skip_input_data (j_decompress_ptr cinfo, long num_bytes) {
-  struct jpeg_source_mgr *src = cinfo->src;
-  if (num_bytes > 0) {
-    while (num_bytes > (long) src->bytes_in_buffer) {
-      num_bytes -= (long) src->bytes_in_buffer;
-      (void) fill_input_buffer(cinfo);
-      /* note we assume that fill_input_buffer will never return FALSE,
-       * so suspension need not be handled.
-       */
-    }
-    src->next_input_byte += (size_t) num_bytes;
-    src->bytes_in_buffer -= (size_t) num_bytes;
-  }
-}
-static void resync_to_restart (j_decompress_ptr cinfo, int desired) {
-  // nothing
-}
-static void term_source (j_decompress_ptr cinfo) {
-  // nothing
-}
-static void jpeg_memory_src (j_decompress_ptr cinfo, 
-			     void* buffer, unsigned int nbyte) {
-  struct jpeg_source_mgr *src;
-  if (cinfo->src == NULL) {/* first time for this JPEG object? */
-    cinfo->src = (struct jpeg_source_mgr *)
-      (*cinfo->mem->alloc_small) ((j_common_ptr) cinfo, JPOOL_PERMANENT,
-  				  sizeof(struct jpeg_source_mgr));
-  }
-  src = cinfo->src;
-  src->next_input_byte = (JOCTET *)
-    (*cinfo->mem->alloc_small) ((j_common_ptr) cinfo, JPOOL_PERMANENT,
-  				nbyte * sizeof(JOCTET));
-  src->init_source = init_source;
-  src->fill_input_buffer = fill_input_buffer;
-  src->skip_input_data = skip_input_data;
-  src->resync_to_restart = jpeg_resync_to_restart; /* use default method */
-  src->term_source = term_source;
-  src->bytes_in_buffer = nbyte; /* forces fill_input_buffer on first read */
-  memcpy((void *)src->next_input_byte, buffer, nbyte);
-}
-
-int jpeg_decompress(char *image_buffer, unsigned int size) {
-  // URL: http://courses.cs.deu.edu.tr/cse566/newpage2.htm
-  /* This struct contains the JPEG decompression parameters and pointers to
-   * working space (which is allocated as needed by the JPEG library).
-   */
-  struct jpeg_decompress_struct cinfo;
-  JSAMPARRAY buffer;/* Output row buffer */
-  int row_stride;/* physical row width in output buffer */
-
-  /* Step 1: allocate and initialize JPEG decompression object */
-  jpeg_create_decompress(&cinfo);
-
-  /* Step 2: specify data source (eg, a file) */
-  // jpeg_stdio_src(&cinfo, infile); // file
-  jpeg_memory_src(&cinfo, image_buffer, size); // memory
-
-  /* Step 3: read file parameters with jpeg_read_header() */
-  (void) jpeg_read_header(&cinfo, TRUE);
-  /* We can ignore the return value from jpeg_read_header since
-   *   (a) suspension is not possible with the stdio data source, and
-   *   (b) we passed TRUE to reject a tables-only JPEG file as an error.
-   * See libjpeg.doc for more info.
-   */
-
-  /* Step 4: set parameters for decompression */
-
-  /* In this example, we don't need to change any of the defaults set by
-   * jpeg_read_header(), so we do nothing here.
-   */
-
-  /* Step 5: Start decompressor */
-
-  (void) jpeg_start_decompress(&cinfo);
-  /* We can ignore the return value since suspension is not possible
-   * with the stdio data source.
-   */
-
-  /* We may need to do some setup of our own at this point before reading
-   * the data.  After jpeg_start_decompress() we have the correct scaled
-   * output image dimensions available, as well as the output colormap
-   * if we asked for color quantization.
-   * In this example, we need to make an output work buffer of the right size.
-   */ 
-  /* JSAMPLEs per row in output buffer */
-  row_stride = cinfo.output_width * cinfo.output_components;
-  /* Make a one-row-high sample array that will go away when done with image */
-  buffer = (*cinfo.mem->alloc_sarray)
-    ((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
-
-  /* Step 6: while (scan lines remain to be read) */
-  /*           jpeg_read_scanlines(...); */
-
-  /* Here we use the library's state variable cinfo.output_scanline as the
-   * loop counter, so that we don't have to keep track ourselves.
-   */
-  while (cinfo.output_scanline < cinfo.output_height) {
-    /* jpeg_read_scanlines expects an array of pointers to scanlines.
-     * Here the array is only one element long, but you could ask for
-     * more than one scanline at a time if that's more convenient.
-     */
-    (void) jpeg_read_scanlines(&cinfo, buffer, 1);
-    /* Assume put_scanline_someplace wants a pointer and sample count. */
-    //put_scanline_someplace(buffer[0], row_stride); FIX
-    // MAYBE THIS IS WHERE WE CAN GET X,Y
-    // buffer[0] should be rgb * width, I think
-  }
-
-  /* Step 7: Finish decompression */
-
-  (void) jpeg_finish_decompress(&cinfo);
-  /* We can ignore the return value since suspension is not possible
-   * with the stdio data source.
-   */
-
-  /* Step 8: Release JPEG decompression object */
-
-  /* This is an important step since it will release a good deal of memory. */
-
-  jpeg_destroy_decompress(&cinfo);
-  return 1;
 }
 
 PyObject *Aibo::updateMMap() {
@@ -209,7 +72,8 @@ PyObject *Aibo::updateMMap() {
     return PyInt_FromLong(0);
   }
   if (size > 0 && size < 10000) {
-    jpeg_decompress(image_buffer, (unsigned int) size);
+    jpeg_decompress((unsigned char *)image, (width * height * depth), 
+		    (unsigned char *)image_buffer, (int) size);
     return PyInt_FromLong(size);
   } else {
     printf("Aibo camera bad JPEG size: %ld\n", size);
@@ -227,52 +91,3 @@ Aibo::Aibo(char *hostname) {
   updateMMap(); // this will set height and width automatically
   initialize(width, height, depth, 0, 1, 2); // create some space
 }
-
-/*
-From: /usr/share/doc/libjpeg-devel-6b/libjpeg.doc
-A data source manager provides five methods:
-
-init_source (j_decompress_ptr cinfo)
-        Initialize source.  This is called by jpeg_read_header() before any
-        data is actually read.  Unlike init_destination(), it may leave
-        bytes_in_buffer set to 0 (in which case a fill_input_buffer() call
-        will occur immediately).
-
-fill_input_buffer (j_decompress_ptr cinfo)
-        This is called whenever bytes_in_buffer has reached zero and more
-        data is wanted.  In typical applications, it should read fresh data
-        into the buffer (ignoring the current state of next_input_byte and
-        bytes_in_buffer), reset the pointer & count to the start of the
-        buffer, and return TRUE indicating that the buffer has been reloaded.
-        It is not necessary to fill the buffer entirely, only to obtain at
-        least one more byte.  bytes_in_buffer MUST be set to a positive value
-        if TRUE is returned.  A FALSE return should only be used when I/O
-        suspension is desired (this mode is discussed in the next section).
-
-skip_input_data (j_decompress_ptr cinfo, long num_bytes)
-        Skip num_bytes worth of data.  The buffer pointer and count should
-        be advanced over num_bytes input bytes, refilling the buffer as
-        needed.  This is used to skip over a potentially large amount of
-        uninteresting data (such as an APPn marker).  In some applications
-        it may be possible to optimize away the reading of the skipped data,
-        but it's not clear that being smart is worth much trouble; large
-        skips are uncommon.  bytes_in_buffer may be zero on return.
-        A zero or negative skip count should be treated as a no-op.
-
-resync_to_restart (j_decompress_ptr cinfo, int desired)
-        This routine is called only when the decompressor has failed to find
-        a restart (RSTn) marker where one is expected.  Its mission is to
-        find a suitable point for resuming decompression.  For most
-        applications, we recommend that you just use the default resync
-        procedure, jpeg_resync_to_restart().  However, if you are able to back
-        up in the input data stream, or if you have a-priori knowledge about
-        the likely location of restart markers, you may be able to do better.
-        Read the read_restart_marker() and jpeg_resync_to_restart() routines
-        in jdmarker.c if you think you'd like to implement your own resync
-        procedure.
-
-term_source (j_decompress_ptr cinfo)
-        Terminate source --- called by jpeg_finish_decompress() after all
-        data has been read.  Often a no-op.
-
-*/
