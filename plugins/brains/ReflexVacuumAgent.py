33b982aa8574c24ee087b421534f3046114a10fe
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2005-01-13 19:53:23 +0000

    Sample for Vacuum Cleaner World

---
+++ plugins/brains/ReflexVacuumAgent.py
@@ -0,0 +1,25 @@
+# A Pyro brain to clean up two rooms using a
+# simple reflex brain.After Russell and Norvig
+# (AIMA, 2003) page 46.
+
+from pyro.brain import Brain
+
+class SimpleBrain(Brain):
+   def ReflexVaccumAgent(self, location, status):
+      if status == "dirty": return "suck"
+      elif location == "A": return "right"
+      elif location == "B": return "left"
+      
+   def step(self):
+      # ask the robot for perceptions:
+      location = self.get("robot/location")
+      status = self.get("robot/status")
+      # call the agent program
+      action = self.ReflexVaccumAgent(location, status)
+      # make the move:
+      self.robot.move(action)
+
+# a way of returning the brain instance:
+def INIT(engine):
+   return SimpleBrain('My Vaccum Robot Brain', engine)
+      
