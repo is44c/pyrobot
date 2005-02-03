a8418252b387e4f12e52264e6eac5b22c66ed46f
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2005-02-03 17:59:49 +0000

    Removed file; fixed test

--- plugins/brains/Test.py
+++ plugins/brains/Test.py
@@ -1,23 +1,17 @@
 # A bare brain
 
 from pyro.brain import Brain
-from pyro.robot.playerpuck import *
 
 class SimpleBrain(Brain):
    # Only method you have to define is the step method:
 
    def setup(self):
       # create any vars you need here
-      self.puck = PlayerPuck("puck1", 8000)
+      pass
 
    def step(self):
       #self.robot.move(0, -.2) # negative is to the right!
-      print "IR : ----------------------------"
-      print "Min distance:", self.robot.getMin().distance
-      print "Max distance:", self.robot.getMax().distance
-
-      print "Min angle:", self.robot.getMin().angle
-      print "Max angle:", self.robot.getMax().angle
+      pass
 
 # -------------------------------------------------------
 # This is the interface for calling from the gui engine.
