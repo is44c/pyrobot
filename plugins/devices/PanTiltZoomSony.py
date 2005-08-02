54b95efcd9da38cde137adaacfdfd23d71cc2306
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2005-08-02 16:32:13 +0000

    bad files must go

--- plugins/devices/PanTiltZoomSony.py
+++
@@ -1,7 +0,0 @@
-""" This device signals the robot to load its PTZ """
-
-from pyrobot.robot.aria import AriaPTZDevice
-
-def INIT(robot):
-    ptz = AriaPTZDevice(robot.dev, "sony")
-    return {"ptz": ptz}
