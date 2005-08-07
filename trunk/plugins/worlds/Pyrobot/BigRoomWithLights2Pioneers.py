a679b65b2e6f119b5ffa2e12eaf61e4a926433d1
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2005-08-07 15:14:10 +0000

    Changed the colors of the lights to test rgb on light sensor

--- trunk/plugins/worlds/Pyrobot/BigRoomWithLights2Pioneers.py
+++ trunk/plugins/worlds/Pyrobot/BigRoomWithLights2Pioneers.py
@@ -5,26 +5,28 @@ two lights.
 (c) 2005, PyroRobotics.org. Licensed under the GNU GPL.
 """
 
-from pyrobot.simulators.pysim import TkSimulator, TkPioneer, \
-     PioneerFrontSonars, PioneerFrontLightSensors
+from pyrobot.simulators.pysim import *
 
 def INIT():
     # (width, height) pixels, (offset x, offset y) pixels, scale:
     sim = TkSimulator((600,600), (30, 586), 13.7) 
     # x1, y1, x2, y2 in meters:
     sim.addBox(0, 0, 40, 40)
-    # (x, y) meters, brightness usually 1 (1 meter radius), color (default "yellow")":
-    sim.addLight(5, 5, 1, "blue")
+    # (x, y) meters, brightness usually 1 (1 meter radius),
+    #    color (default "yellow")":
+    sim.addLight(5, 5, 1, "purple")
     # (x, y) meters, brightness usually 1 (1 meter radius):
-    sim.addLight(5, 30, 1)
+    sim.addLight(5, 30, 1, "yellow")
     # x1, y1, x2, y2 in meters:
     sim.addWall(0, 20, 10, 10)  
-    # port, name, x, y, th, bounding Xs, bounding Ys, color (optional TK color name):
+    # port, name, x, y, th, bounding Xs, bounding Ys, color
+    #    (optional TK color name):
     sim.addRobot(60000, TkPioneer("RedPioneer",
                                   15, 30, 0.0,
                                   ((.38, .38, -.38, -.38),
                                    (.25, -.25, -.25, .25))))
-    # port, name, x, y, th, bounding Xs, bounding Ys, color (optional TK color name):
+    # port, name, x, y, th, bounding Xs, bounding Ys, color
+    #    (optional TK color name):
     sim.addRobot(60001, TkPioneer("BluePioneer",
                                   30, 35, 1.5,
                                   ((.38, .38, -.38, -.38),
