f095027b711a794764fd51d9c7baa15ba716c1a3
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2005-11-02 01:46:41 +0000

    New worlds; working on a generic way to run python from windows

---
+++ trunk/plugins/worlds/Pyrobot/Room.py
@@ -0,0 +1,24 @@
+"""
+A PyrobotSimulator world. A large room with two robots and
+two lights.
+
+(c) 2005, PyroRobotics.org. Licensed under the GNU GPL.
+"""
+
+from pyrobot.simulators.pysim import *
+
+def INIT():
+    # (width, height), (offset x, offset y), scale:
+    sim = TkSimulator((465,506), (44,434), 77.338746)
+    # x1, y1, x2, y2 in meters:
+    sim.addBox(0, 0, 5, 5)
+    # port, name, x, y, th, bounding Xs, bounding Ys, color
+    # (optional TK color name):
+    sim.addRobot(60000, TkPioneer("RedPioneer",
+                                  2.48, 0.82, 6.28,
+                                  ((.225, .225, -.225, -.225),
+                                   (.175, -.175, -.175, .175)),
+                                  "red"))
+    # add some sensors:
+    sim.robots[0].addDevice(PioneerFrontSonars())
+    return sim
