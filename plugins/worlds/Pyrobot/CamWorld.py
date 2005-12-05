cab0325016aa860e5e143e83d21c74ec949f4899
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2005-12-05 05:10:06 +0000

    Slowed down vx, increased angle variance

--- plugins/worlds/Pyrobot/CamWorld.py
+++ plugins/worlds/Pyrobot/CamWorld.py
@@ -9,13 +9,13 @@ from pyrobot.simulators.pysim import *
 
 def INIT():
     # (width, height), (offset x, offset y), scale:
-    sim = TkSimulator((441,434),(22,420), 40.357554)  
+    sim = TkSimulator((443,466),(-677,1135),151.170842)
     # x1, y1, x2, y2 in meters:
     #sim.addBox(0, 0, 10, 10)
     # port, name, x, y, th, bounding Xs, bounding Ys, color
     # (optional TK color name):
     sim.addRobot(60000, TkPioneer("Pioneer1",
-                                  1, 1, 5.42,
+                                  5.60, 5.59, 5.53,
                                   ((.225, .225, -.225, -.225),
                                    (.175, -.175, -.175, .175)),
                                   "red"))
@@ -24,7 +24,8 @@ def INIT():
     sim.robots[0].addDevice(PioneerFrontSonars())
     sim.robots[0].addDevice(Camera(60, 40, 60 * math.pi/180, -60 * math.pi/180, 0, 0, 0))
 
-    sim.addRobot(None, TkPuck("Puck1", 5, 5, 0, ((.05, .05, -.05, -.05), (.05, -.05, -.05, .05)), "purple"))
-    sim.addRobot(None, TkPuck("Puck2", 6, 6, 0, ((.05, .05, -.05, -.05), (.05, -.05, -.05, .05)), "blue"))
+    sim.addRobot(None, TkPuck("Puck1", 6.28, 6.34, 0, ((.05, .05, -.05, -.05), (.05, -.05, -.05, .05)), "purple"))
+    sim.addRobot(None, TkPuck("Puck2", 6.47, 6.47, 0, ((.05, .05, -.05, -.05), (.05, -.05, -.05, .05)), "blue"))
+    sim.addRobot(None, TkPuck("Puck3", 7.0, 7.0, 0, ((.05, .05, -.05, -.05), (.05, -.05, -.05, .05)), "red"))
         
     return sim
