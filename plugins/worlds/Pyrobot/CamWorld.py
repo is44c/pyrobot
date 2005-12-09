759661740ccf7d98fb3d544b63b639837268f5b9
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2005-12-09 04:14:28 +0000

    Added gripper breakbeam, drawing rep, and .close() method

--- plugins/worlds/Pyrobot/CamWorld.py
+++ plugins/worlds/Pyrobot/CamWorld.py
@@ -9,21 +9,22 @@ from pyrobot.simulators.pysim import *
 
 def INIT():
     # (width, height), (offset x, offset y), scale:
-    sim = TkSimulator((443,466),(-677,1135),151.170842)
+    sim = TkSimulator((445,496),(26,426),39.911318)
     # x1, y1, x2, y2 in meters:
-    #sim.addBox(0, 0, 10, 10)
+    sim.addBox(0, 0, 10, 10)
     # port, name, x, y, th, bounding Xs, bounding Ys, color
     # (optional TK color name):
     sim.addRobot(60000, TkPioneer("Pioneer1",
                                   5.60, 5.59, 5.53,
                                   ((.225, .225, -.225, -.225),
                                    (.175, -.175, -.175, .175)),
-                                  "red"))
+                                  "green"))
     # add some sensors:
     # x, y relative to body center (beyond bounding box):
     sim.robots[0].addDevice(PioneerFrontSonars())
     sim.robots[0].addDevice(Camera(60, 40, 60 * math.pi/180, -60 * math.pi/180, 0, 0, 0))
-    #sim.robots[0].addDevice(Gripper(((.225, .5, .225, .5), (.20, .20, -.20, .20))))
+    sim.robots[0].addDevice(Gripper(((.225, .40, .225, .40),
+                                     (.12, .12, -.12, -.12))))
 
     sim.addRobot(None, TkPuck("Puck1", 6.28, 6.34, 0, ((.05, .05, -.05, -.05), (.05, -.05, -.05, .05)), "purple"))
     sim.addRobot(None, TkPuck("Puck2", 6.47, 6.47, 0, ((.05, .05, -.05, -.05), (.05, -.05, -.05, .05)), "blue"))
