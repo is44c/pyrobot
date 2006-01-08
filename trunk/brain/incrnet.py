a703f23cf743ee9d08bfc0e6ac9dcadb439d6d07
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2006-01-08 17:08:19 +0000

    A working version of Incremental Network. changed: - train() now takes resetEpoch, cont - added net.complete which shows if learning is complete - added incrnet.py to test XOR

--- trunk/brain/incrnet.py
+++ trunk/brain/incrnet.py
@@ -1,8 +1,17 @@
 from pyrobot.brain.conx import *
 
-net = IncrementalNetwork()
-net.addLayers(2, 1)
-net.addCandidateLayer(1)
+net = IncrementalNetwork("cascade") # "parallel" or "cascade"
+net.addLayers(2, 1) # sizes
+net.addCandidateLayer(8) # size
 net.setInputs( [[0, 0], [0, 1], [1, 0], [1, 1]])
 net.setTargets([[0], [1], [1], [0]])
 net.tolerance = .1
+net.train(100)
+sweeps = 300
+while not net.complete:
+    net.recruitBest()
+    net.train(sweeps, cont=1)
+net["candidate"].active = 0
+net.displayConnections()
+net.interactive = 1
+net.sweep()
