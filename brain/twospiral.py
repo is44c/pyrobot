b74bcbee2103c59d3fae1575f7a27d469464f85a
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2006-01-09 22:32:29 +0000

    Working on a sum + RL twospiral solution

--- brain/twospiral.py
+++ brain/twospiral.py
@@ -6,15 +6,17 @@ def test(net, resolution = 30):
         net["candidate"].active = 0
     for x in range(resolution):
         row = ""
-        for y in range(resolution):
-            input = (x/float(resolution), y/float(resolution))
-            results = net.propagate(input = input)
-            c = round(results[0], 1)
-            if c == 1.0:
-                c = "#"
-            else:
-                c = str(c * 10)[0]
-            row += "%s" % c
+        for i in range(net["output"].size):
+            for y in range(resolution):
+                input = (x/float(resolution), y/float(resolution))
+                results = net.propagate(input = input)
+                c = round(results[i], 1)
+                if c == 1.0:
+                    c = "#"
+                else:
+                    c = str(c * 10)[0]
+                row += "%s" % c
+            row += "   "
         print row
     if "candidate" in [layer.name for layer in net.layers]:
         net["candidate"].active = 1
@@ -33,14 +35,14 @@ for line in fp:
     inputs.append( data[:2] )
     targets.append( data[2:] )
 
-net = IncrementalNetwork()
-net.addLayers(2, 10, 1)
-net.setInputs( inputs )
-net.setTargets( targets)
-net.tolerance = 0.4
-net.addCandidateLayer(4)
-net.reportRate = 1
-#net.train(10)
+net0 = IncrementalNetwork()
+net0.addLayers(2, 10, 1)
+net0.setInputs( inputs )
+net0.setTargets( targets)
+net0.tolerance = 0.4
+net0.addCandidateLayer(4)
+net0.reportRate = 1
+#net0.train(10)
 
 net2 = GovernorNetwork(5, 2.1, 0.01, 5, 0.2) # 5, 2.1, 0.3, 5, 0.2
 net2.reportHistograms = 1
@@ -60,8 +62,37 @@ net3.setTargets( targets)
 net3.tolerance = 0.4
 net3.reportRate = 10
 
+class MyNetwork(Network):
+    def getData(self, i):
+        patterns = {1.0: [1.0, 0.0], 0.0: [0.0, 1.0]}
+        data = {}
+        data["input"] = self.inputs[i]
+        data["output"] = patterns[self.targets[i][0]]
+        return data
+
+class MyNetworkSummation(Network):
+    def getData(self, i):
+        self.propagate(input = self.inputs[i])
+        data = {}
+        data["input"] = self.inputs[i]
+        sum = reduce(operator.add, self["output"].activation) / self["output"].size
+        if abs(sum - self.targets[i]) < self.tolerance:
+            data["output"] = [n for n in self["output"].activation]
+        else:
+            data["output"] = [1 - n for n in self["output"].activation]
+        return data
+
+net4 = MyNetwork()
+net4.addLayers(2, 10, 10, 2)
+net4.setInputs( inputs )
+net4.setTargets( targets)
+net4.tolerance = 0.4
+net4.reportRate = 10
+
+net = net4
+
 cont = 0
-while not net3.complete:
-    net3.train(50, cont=cont)
-    test(net3)
+while not net.complete:
+    net.train(50, cont=cont)
+    test(net)
     cont = 1
