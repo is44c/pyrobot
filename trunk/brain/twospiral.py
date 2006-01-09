19a6ee456d730b5f6d207956389e61437280453f
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2006-01-09 02:31:03 +0000

    Working on testing with GovernorNetwork too

--- trunk/brain/twospiral.py
+++ trunk/brain/twospiral.py
@@ -1,7 +1,9 @@
 from pyrobot.brain.conx import *
+from pyrobot.brain.governor import *
 
 def test(net, resolution = 30):
-    net["candidate"].active = 0
+    if "candidate" in [layer.name for layer in net.layers]:
+        net["candidate"].active = 0
     for x in range(resolution):
         row = ""
         for y in range(resolution):
@@ -9,7 +11,8 @@ def test(net, resolution = 30):
             results = net.propagate(input = input)
             row += "%d" % int(round(results[0]))
         print row
-    net["candidate"].active = 1
+    if "candidate" in [layer.name for layer in net.layers]:
+        net["candidate"].active = 1
 
 fp = open("two-spiral.dat", "r")
 inputs = []
@@ -27,6 +30,13 @@ net.tolerance = 0.4
 net.addCandidateLayer(4)
 net.reportRate = 1
 #net.train(10)
+
+net2 = GovernorNetwork()
+net2.addThreeLayers(2, 10, 1)
+net2.setInputs( inputs )
+net2.setTargets( targets)
+net2.tolerance = 0.4
+net2.reportRate = 1
 
 def train(net, sweeps = 30):
     net["candidate"].active = 1
