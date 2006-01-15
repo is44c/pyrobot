f60bdec0001bec1eed7dcc3eff045d8e2918a6b3
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2006-01-15 19:33:32 +0000

    Working on getting some of the Pyrobot Simulator code in C++

--- geometry/cgeometry/Geometry.h
+++ geometry/cgeometry/Geometry.h
@@ -5,22 +5,34 @@
 #include <stdlib.h>
 #include <math.h>
 
-class Geometry {
+class World {
  public:
-  Geometry();
+  World();
+};
+
+class Point {
+ public:
+  double x, y;
+  Point(double x, double y);
+  Point();
 };
 
 class Segment {
  public:
-  int x1, y1, x2, y2;
+  double x1, y1, x2, y2;
+  PyObject *start, *end;
   int id;
-  Segment(int x1, int y1, int x2, int y2, int id);
+  Segment(double x1, double y1, double x2, double y2, int id = 0);
+  Segment(PyObject *start, PyObject *end, int id = 0);
+  ~Segment();
+  double angle();
   double length();
-  bool in_bbox(int x, int y);
-  bool parallel(Segment other);
-  bool vertical(Segment other);
-  bool intersection(Segment other);
-  PyObject *intersects(Segment other);
+  bool in_bbox(double x, double y);
+  bool parallel(Segment *other);
+  bool vertical();
+  double slope();
+  Point *intersection(Segment *other);
+  PyObject *intersects(Segment *other);
   double yintercept();
 };
 
