88510fa01daadeecde0c6bf6dbf5e8c173d25349
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2006-03-16 04:47:06 +0000

    Replaced old world

--- trunk/plugins/worlds/Pyrobot/Tutorial.py
+++ trunk/plugins/worlds/Pyrobot/Tutorial.py
@@ -9,20 +9,26 @@ from pyrobot.simulators.pysim import *
 
 def INIT():
     # (width, height), (offset x, offset y), scale:
-    sim = TkSimulator((585,596), (33,545), 173.983100)
+    sim = TkSimulator((446,491),(21,451),80.517190)
     # x1, y1, x2, y2 in meters:
-    sim.addBox(0, 0, 3, 3)
-    sim.addRobot(60000, TkMyro("GreenMyro",
-                               0.37, 0.33, 5.21,
-                               ((.20, .20, -.10, -.10),
-                                (.18, -.18, -.18, .18)), "green"))
-    sim.addRobot(60001, TkMyro("BlueMyro",
-                               2.69, 2.68, 2.03,
-                               ((.20, .20, -.10, -.10),
-                                (.18, -.18, -.18, .18)), "blue"))
-    for i in range(2):
-        sim.robots[i].addDevice(BulbDevice(-.10, 0))
-        sim.robots[i].addDevice(MyroIR()) # infrared
-        sim.robots[i].addDevice(MyroBumper()) # bumpers
-        sim.robots[i].addDevice(MyroLightSensors()) # bumpers
+    sim.addBox(0, 0, 5, 5)
+    sim.addBox(0, 4, 1, 5, "blue", wallcolor="blue")
+    sim.addBox(2.5, 0, 2.6, 2.5, "green", wallcolor="green")
+    sim.addBox(2.5, 2.5, 3.9, 2.6, "green", wallcolor="green")
+    # port, name, x, y, th, bounding Xs, bounding Ys, color
+    # (optional TK color name):
+    sim.addRobot(60000, TkPioneer("RedPioneer",
+                                  .5, 2.5, 0.00,
+                                  ((.225, .225, -.225, -.225),
+                                   (.175, -.175, -.175, .175))))
+    # add some sensors:
+    # sim.robots[0].addDevice(PioneerFrontSonars()) # for 8 front sonar
+    sim.robots[0].addDevice(Pioneer16Sonars()) # for full 360 sonar
+    sim.robots[0].addDevice(PioneerFrontLightSensors())
+    # x, y relative to body center (beyond bounding box):
+    sim.robots[0].addDevice(BulbDevice(0.226, 0)) # pose x, pose y
+    # width, height, startAngle, stopAngle, pose x, pose y, pose thr:
+    cam = Camera(60, 40, 0, 120, 0, 0, 0)
+    sim.robots[0].addDevice(cam)
+    sim.robots[0].addDevice( PTZ(cam) )
     return sim
