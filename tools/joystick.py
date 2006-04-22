e8f7dd9fac6847a2fb88ec3887ccde05c0f8702f
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2006-04-22 20:39:06 +0000

    Added stop, spring-back

--- tools/joystick.py
+++ tools/joystick.py
@@ -7,8 +7,21 @@ class Joystick(Tkinter.Toplevel):
       self.debug = 0
       self.wm_title('Joystick')
       self.protocol('WM_DELETE_WINDOW',self.destroy)
-      if hasZ: # has a 3rd dimension, height
-         self.topFrame = Tkinter.Frame(self)
+      self.springBack = 0
+      self.hasZ = hasZ
+      self.mBar = Tkinter.Frame(self, relief=Tkinter.RAISED, borderwidth=2)
+      self.mBar.pack(fill=Tkinter.X)
+      self.goButtons = {}
+      self.menuButtons = {}
+      self.heightScaleValue = 0
+      menu = [('Options',[['Toggle spring-back to center', self.toggleSpringBack],
+                          ]),
+              ]
+      for entry in menu:
+         self.mBar.tk_menuBar(self.makeMenu(self.mBar, entry[0], entry[1]))
+      if self.hasZ: # has a 3rd dimension, height
+         self.mainFrame = Tkinter.Frame(self)
+         self.topFrame = Tkinter.Frame(self.mainFrame)
          self.rightFrame = Tkinter.Frame(self.topFrame)
          ticks = 10
          resolution = .1
@@ -16,7 +29,7 @@ class Joystick(Tkinter.Toplevel):
          label.pack(side = "top")
          self.heightScale = Tkinter.Scale(self.rightFrame, orient=Tkinter.VERTICAL,
                                           length = 220, from_=1, to=-1,
-                                          tickinterval=ticks, command = self.getHeightScale,
+                                          tickinterval=ticks, command = self.setHeightScale,
                                           resolution = resolution)
          self.heightScale.set(0)
          self.heightScale.pack(side = 'bottom', expand = "yes", anchor = "e", fill = 'y')
@@ -24,7 +37,8 @@ class Joystick(Tkinter.Toplevel):
          self.topFrame.pack()
          self.frame = Tkinter.Frame(self.topFrame)
       else:
-         self.frame = Tkinter.Frame(self)
+         self.mainFrame = Tkinter.Frame(self)
+         self.frame = Tkinter.Frame(self.mainFrame)
       label = Tkinter.Label(self.frame, text = "Forward")
       label.pack(side = "top")
       label = Tkinter.Label(self.frame, text = "Reverse")
@@ -39,18 +53,35 @@ class Joystick(Tkinter.Toplevel):
                                    bg = 'white')
       self.initHandlers()
       self.canvas.pack(side=Tkinter.BOTTOM)
-
       self.circle_dim = (10, 10, 210, 210) #x0, y0, x1, y1
       self.circle = self.canvas.create_oval(self.circle_dim, fill = 'white')
       self.canvas.create_oval(105, 105, 115, 115, fill='black')
-
       self.frame.pack(side='left')
+      self.goButtons["Stop"] = Tkinter.Button(self,text="Stop",command=self.stop)
+      self.goButtons["Stop"].pack(side=Tkinter.BOTTOM,padx=2,pady=2,fill=Tkinter.X, expand = "yes", anchor="s")
+      self.mainFrame.pack()
       self.translate = 0.0
       self.rotate = 0.0
       self.threshold = 0.10
+   def toggleSpringBack(self):
+      self.springBack = not self.springBack
+   def makeMenu(self, bar, name, commands):
+      """ Assumes self.menuButtons exists """
+      menu = Tkinter.Menubutton(bar,text=name,underline=0)
+      self.menuButtons[name] = menu
+      menu.pack(side=Tkinter.LEFT,padx="2m")
+      menu.filemenu = Tkinter.Menu(menu)
+      for cmd in commands:
+         if cmd:
+            menu.filemenu.add_command(label=cmd[0],command=cmd[1])
+         else:
+            menu.filemenu.add_separator()
+      menu['menu'] = menu.filemenu
+      return menu
 
-   def getHeightScale(self, event = None):
-      return self.heightScale.get()
+   def setHeightScale(self, event = None):
+      self.heightScaleValue = self.heightScale.get()
+      self.move(self.translate, self.rotate, self.heightScaleValue)
 
    def initHandlers(self):
       self.canvas.bind("<ButtonRelease-1>", self.canvas_clicked_up)
@@ -60,23 +91,19 @@ class Joystick(Tkinter.Toplevel):
    def getValue(self, event = None):
       return self.translate, self.rotate
 
-   def move(self, translate, rotate):
+   def _move(self, translate, rotate):
       self.translate = translate
-      if self.translate < 0.0:
-         self.translate += self.threshold
-      elif self.translate > 0.0:
-         self.translate -= self.threshold
       self.rotate = rotate
-      if self.rotate < 0.0:
-         self.rotate += self.threshold
-      elif self.rotate > 0.0:
-         self.rotate -= self.threshold
+      self.move(self.translate, self.rotate, self.heightScaleValue)
+
+   def move(self, x, y, z = 0):
       if self.debug:
-         print self.translate, self.rotate
+         print x, y, z
 
    def canvas_clicked_up(self, event):
-      self.canvas.delete("lines")
-      self.move(0.0, 0.0)
+      if not self.springBack:
+         self.canvas.delete("lines")
+         self._move(0.0, 0.0)
 
    def drawArrows(self, x, y, trans, rotate):
       if trans == 0:
@@ -90,19 +117,23 @@ class Joystick(Tkinter.Toplevel):
 
    def canvas_clicked_down(self, event):
       if self.in_circle(event.x, event.y):
+         self.canvas.delete("lines")
          trans, rotate = self.calc_tr(event.x, event.y)
          self.drawArrows(event.x, event.y, trans, rotate)
-         self.move(trans, rotate)
+         self._move(trans, rotate)
 
    def canvas_moved(self, event):
       if self.in_circle(event.x, event.y):
          self.canvas.delete("lines")
          trans, rotate = self.calc_tr(event.x, event.y)
          self.drawArrows(event.x, event.y, trans, rotate)         
-         self.move(trans, rotate)
+         self._move(trans, rotate)
 
-   def stop(self):
-      self.move(0.0, 0.0)
+   def stop(self, event = None):
+      if self.hasZ:
+         self.heightScale.set(0)
+      self.canvas.delete("lines")
+      self._move(0.0, 0.0)
 
    def in_circle(self, x, y):
       return 1
@@ -130,7 +161,7 @@ class Joystick(Tkinter.Toplevel):
       return (trans, rot)
 
 if __name__ == '__main__':
-   app = Tk()
+   app = Tkinter.Tk()
    app.withdraw()
-   joystick = Joystick(parent = app)
-   app.mainloop()
+   joystick = Joystick(parent = app, hasZ = 1)
+   #app.mainloop()
