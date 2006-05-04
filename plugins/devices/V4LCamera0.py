ba99be1ff01d317f0b0f63d609f91b888813c711
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2006-05-04 23:45:29 +0000

    V4LCamera0 now defaults to 640 x 480; added a recorder of images for blimp

--- plugins/devices/V4LCamera0.py
+++ plugins/devices/V4LCamera0.py
@@ -2,5 +2,5 @@ from pyrobot.camera.v4l import V4LCamera
 from pyrobot.vision.cvision import VisionSystem
 
 def INIT(robot):
-    return {"camera" : V4LCamera( 160, 120, channel = 0, 
+    return {"camera" : V4LCamera( 640, 480, channel = 0, 
                                   visionSystem = VisionSystem())}
