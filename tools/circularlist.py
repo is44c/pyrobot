c38162e71859fdd0cebb3e1422833506ce408c40
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2006-05-19 13:13:15 +0000

    Fixed tests as noted by Willi Richert; add __iter__ and next methods

--- tools/circularlist.py
+++ tools/circularlist.py
@@ -6,12 +6,13 @@ class CircularList:
     insert an item into the list.  It will automatically remove the
     oldest item in the list if it has reached the maxSize.  Use the
     nextItem method to retrieve the oldest visited item. Use the
-    getItem method to retrieve an item at a particular index.
+    __getitem__ method (object[item]) to retrieve an item at a
+    particular index.
     
     """
     def __init__(self, size=-1):
         self.maxSize = size
-        self.next = 0
+        self.nextPos = 0
         self.contents = []
         self.names = []
         self.nextID = 0
@@ -22,7 +23,7 @@ class CircularList:
             return output + "Empty"
         output += "| "
         for i in range(len(self.contents)):
-            if i == self.next:
+            if i == self.nextPos:
                 output += " ^ "
             output += (str(self.contents[i]) + " ")
         return output + " |"
@@ -35,6 +36,10 @@ class CircularList:
             return self.contents[index]
         raise StopIteration, "no item at index %d in CircularList" % index
 
+    def __iter__(self):
+        while 1:
+            yield self.nextItem()
+
     def addItem(self, item):
         if self.maxSize == 0: return
         if len(self.contents) == self.maxSize:
@@ -44,26 +49,29 @@ class CircularList:
         self.names.append(self.nextID)
         self.nextID += 1
 
+    def next(self): return self.nextItem()
+
     def nextItem(self):
         if len(self.contents) == 0:
             raise AttributeError, "no nextItem in empty CircularList"
-        item = self.contents[self.next]
-        self.next = (self.next + 1) % len(self.contents)
+        item = self.contents[self.nextPos]
+        self.nextPos = (self.nextPos + 1) % len(self.contents)
         return item
 
-if __name__ == "__main__":
+def main():
     print "-------------------------------------------------"
     print "TEST: try to get the next item from an empty list"
     emptyList = CircularList()
     print emptyList
     try:
-        emptyList.nextItem()
+        emptyList.next()
+        print "ERROR: did not catch the error"
     except:
         print "successfully caught the error"
     print "-------------------------------------------------"
     print "TEST: try to get a particular item from an empty list"
     try:
-        emptyList.getItem(4)
+        emptyList[4]
     except:
         print "successfully caught the error"
     print "-------------------------------------------------"
@@ -73,12 +81,12 @@ if __name__ == "__main__":
         clist.addItem(i)
     print clist
     for i in range(len(clist)):
-        print clist.nextItem()
+        print clist.next()
         print clist
     for i in range(3):
         clist.addItem(i*10)
     print clist
-    print "Getting item at index two: ", clist.getItem(2)
+    print "Getting item at index two: ", clist[2]
     print "-------------------------------------------------"
     print "TEST: create a list with no maximum size"
     clist = CircularList()
@@ -86,7 +94,7 @@ if __name__ == "__main__":
         clist.addItem(i)
     print clist
     for i in range(len(clist)):
-        print clist.nextItem()
+        print clist.next()
         print clist
     for i in range(3):
         clist.addItem(i*10)
@@ -95,16 +103,30 @@ if __name__ == "__main__":
     print "TEST: create a circular list of cicular lists"
     clist = CircularList()
     clist.addItem(CircularList(3))
-    clist.getItem(0).addItem('a')
-    clist.getItem(0).addItem('b')
+    clist[0].addItem('a')
+    clist[0].addItem('b')
     clist.addItem(CircularList(3))
-    clist.getItem(1).addItem('c')
+    clist[1].addItem('c')
     clist.addItem(CircularList(3))
-    clist.getItem(2).addItem('d')
-    clist.getItem(2).addItem('e')
-    clist.getItem(2).addItem('f')
+    clist[2].addItem('d')
+    clist[2].addItem('e')
+    clist[2].addItem('f')
     for i in range(12):
         print clist
-        print clist.nextItem().nextItem()
+        print "Next:", clist.next().next()
+    print clist
+    print "-------------------------------------------------"
+    print "TEST: test circular list as an iterator"
+    n = 0
+    print clist
+    for i in clist:
+        for j in i:
+            print "Next:", j, n
+            n += 1
+            if n >= 100: break
+        if n >= 100: break
+    print clist
+    return clist
 
-    
+if __name__ == "__main__":
+    main()
