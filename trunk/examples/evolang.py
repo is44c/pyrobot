2ac19b49c48b71cf51e122f079311a851f8665fa
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2006-06-10 15:28:00 +0000

    Should be working...

--- trunk/examples/evolang.py
+++ trunk/examples/evolang.py
@@ -2,6 +2,10 @@
 Replicating ALife Nolfi 2006.
 """
 
+############################################################
+# First, let's build a simulated world:
+############################################################
+
 from pyrobot.simulators.pysim import *
 from pyrobot.geometry import distance
 import time, random, math
@@ -14,7 +18,6 @@ sim.addBox(0, 0, 10, 10)
 sim.addLight(2, 2, 1)
 sim.addLight(7, 7, 1)
 # port, name, x, y, th, bounding Xs, bounding Ys, color
-# (optional TK color name):
 sim.addRobot(60000, TkPioneer("RedPioneer",
                               1, 1, -0.86,
                               ((.225, .225, -.225, -.225),
@@ -39,6 +42,10 @@ sim.addRobot(60003, TkPioneer("YellowPioneer",
 for robot in sim.robots:
     robot.addDevice(PioneerFrontSonars())
     robot.addDevice(PioneerFrontLightSensors())
+
+############################################################
+# Now, make some client-side connections to the sim robots
+############################################################
 
 # client side:
 from pyrobot.robot.symbolic import Simbot
@@ -74,32 +81,67 @@ sys.exitfunc = myquit
 
 sim.redraw()
 
+############################################################
+# Now, let's set up the GA:
+############################################################
+
 from pyrobot.geometry import Polar
-def quadSound(myLoc, q, lastS, location):
+def quadNum(myangle, angle):
     """
-    Computes the sound heard for quad[q].
+    Given angle, return quad number
       |0|
     |3| |1|
       |2|
+    """
+    diff = angle - myangle
+    if diff >= 0:
+        if diff < math.pi/4:
+            return 0
+        elif diff < math.pi/4 + math.pi/2:
+            return 3
+        elif diff < math.pi:
+            return 2
+        else:
+            return 1
+    else:
+        if diff > -math.pi/4:
+            return 0
+        elif diff > -math.pi/4 - math.pi/2:
+            return 1
+        elif diff > -math.pi:
+            return 2
+        else:
+            return 3
+def quadTest(robot = 0):
+    location = [0] * 4
+    for n in range(4):
+        location[n] = engines[0].robot.simulation[0].getPose(n)
+    myLoc = location[robot]
+    return quadSound(myLoc, range(4), location)
+def quadSound(myLoc, lastS, location):
+    """
+    Computes the sound heard for all quads.
     myLoc:    (x, y, t) of current robot; t where 0 is up
-    q:        which quad?
     lastS:    last sound made by robots
     location: (x, y, t) of robots; t where 0 is up
     """
-    angleRange = [(0, 1), (0, 1), (0, 1), (0, 1)] # FIX: need angles of quads
-    closest = (10000, 0) # dist, freq
+    closest = [(10000, 0), (10000, 0), (10000, 0), (10000, 0)] # dist, freq
     for n in range(len(location)):
         loc = location[n]
         if loc != myLoc:
             # distance between robots:
             dist = distance(myLoc[0], myLoc[1], loc[0], loc[1])
             # global angle from one robot to another:
-            angle = Polar(loc[0] - myLoc[0], loc[1] - myLoc[1], bIsPolar=0) # 0 to right (geometry)
-            # FIX: compute angle from myLoc[2]
-            if angleRange[0] < angle < angleRange[1]: # if in right quad
-                if dist < closest[0]: # if shorter than previous
-                    closest = dist, lastS[n] # new closest
-    return closest[1]
+            angle = Polar(loc[0] - myLoc[0], loc[1] - myLoc[1], bIsPolar=0) # 0 to right, neg down (geometry-style)
+            angle = angle.t # get theta
+            if angle < 0:
+                angle = math.pi + (math.pi + angle) # 0 to 2pi
+            angle = (angle - math.pi/2) % (math.pi * 2)
+            q = quadNum(myLoc[2], angle) 
+            #print n, myLoc[2], angle, q
+            if dist < closest[q][0]: # if shorter than previous
+                closest[q] = dist, lastS[n] # new closest
+    return [v[1] for v in closest] # return the sounds
 
 from pyrobot.brain.ga import *
 import operator
@@ -109,32 +151,36 @@ class NNGA(GA):
         for n in range(len(engines)):
             engine = engines[n]
             engine.brain.net.unArrayify(self.pop.individuals[genePos].genotype)
-            # fix to make sure they don't overlap
+            # Put each robot in a random location:
             x, y, t = 1 + random.random() * 7, 1 + random.random() * 7, random.random() * math.pi
             engine.robot.simulation[0].setPose(n, x, y, t)
         fitness = [0.0] * 4
-        seconds = 10 # how much simulated real time to run?
-        s = [0] * 4
-        lastS = [0] * 4
-        location = [(0, 0, 0) for v in range(4)]
+        seconds = 2 # how much simulated real time to run, in sim seconds
+        s = [0] * 4 # each robot's sound
+        lastS = [0] * 4 # previous sound
+        location = [(0, 0, 0) for v in range(4)] # each robot's location
         for i in range(seconds * (1000/sim.timeslice)): # simulated seconds (10/sec)
-            # move the robots
+            # get the locations
+            for n in range(4): # number of robots
+                location[n] = engine.robot.simulation[0].getPose(n)
+            # compute the move for each robot
             for n in range(4): # number of robots
                 engine = engines[n]
                 engine.robot.update()
                 # compute quad for this robot
-                quad = [0] * 4
-                myLoc = engine.robot.simulation[0].getPose(n)
-                for q in range(4):
-                    quad[q] = quadSound(myLoc, q, lastS, location)
+                myLoc = location[n]
+                quad = quadSound(myLoc, lastS, location)
                 # compute output for each robot
                 oTrans, oRotate, s[n] = engine.brain.propagate(quad)
-                location[n] = engine.robot.simulation[0].getPose(n)
-                # then move it
+                # then set the move velocities:
                 engine.brain.step(oTrans, oRotate)
+            # save the sounds
+            for n in range(4): # number of robots
                 lastS = [v for v in s]
+            # make the move:
             sim.step(run=0)
             sim.update_idletasks()
+            # compute fitness
             closeTo = [0, 0] # how many robots are close to which lights?
             for n in range(len(engines)):
                 engine = engines[n]
@@ -150,6 +196,7 @@ class NNGA(GA):
                     else:
                         closeTo[1] += 1
             for n in range(len(engines)):
+                if engines[n].robot.stall: continue
                 for total in closeTo:
                     if total <= 2:
                         fitness[n] += .25 * total
@@ -168,3 +215,5 @@ ga = NNGA(Population(3, Gene, size=len(g), verbose=1,
           maxGeneration=2, verbose=1)
 ga.evolve()
 
+for n in range(4):
+    engines[n].robot.stop()
