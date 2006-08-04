45c7e023439f338df88af20d467c94ef0ab8f603
dblank <dblank@21022541-8d97-4203-aabd-96d1d42fbb12>
2006-08-04 12:41:24 +0000

    Turned off psyco and added print seed

--- trunk/examples/twospy.py
+++ trunk/examples/twospy.py
@@ -1,6 +1,6 @@
 # to turn off psyco uncomment these:
-#import pyrobot.system.share
-#pyrobot.system.share.debug = 1
+import pyrobot.system.share
+pyrobot.system.share.debug = 1
 
 from pyrobot.brain.conx import *
 from pyrobot.brain.cascor import *
